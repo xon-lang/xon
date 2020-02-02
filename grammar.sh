6a1dc3f856dc22267ce03734e616ae237c010cad
nizami <nizami.dev@gmail.com>
2020-02-02 08:57:49 +0300

    Update grammar generating

--- grammar.sh
+++ grammar.sh
@@ -1,14 +1,27 @@
+generate_grammar()
+{
+    cd src/grammar
+    npx rimraf .antlr
+    npx antlr4ts *.g4 -o .antlr
+    cd .antlr
+    cp XonLexer.ts ../xon-lexer.ts
+    cp XonParser.ts ../xon-parser.ts
+    npx rimraf .antlr
+}
+
+
 lexer_date=$(date -r ./src/grammar/XonLexer.g4 "+%Y-%m-%d %H:%M:%S")
 lexer_ts_date=$(date -r ./src/grammar/.antlr/XonLexer.ts "+%Y-%m-%d %H:%M:%S")
 parser_date=$(date -r ./src/grammar/XonParser.g4 "+%Y-%m-%d %H:%M:%S")
 parser_ts_date=$(date -r ./src/grammar/.antlr/XonParser.ts "+%Y-%m-%d %H:%M:%S")
 
+
 if [[ "$lexer_date" > "$lexer_ts_date" ]] ;
 then
-    npm run grammar-force
+    generate_grammar
 fi
 
 if [[ "$parser_date" > "$parser_ts_date" ]] ;
 then
-    npm run grammar-force
+    generate_grammar
 fi
