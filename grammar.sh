f55a5eeb4e650f2ba28744252c51264da87eb511
nizami <nizami.dev@gmail.com>
2020-02-02 09:03:06 +0300

    Update grammar.sh

--- grammar.sh
+++ grammar.sh
@@ -1,19 +1,19 @@
 generate_grammar()
 {
-    cd src/grammar
     npx rimraf .antlr
     npx antlr4ts *.g4 -o .antlr
     cd .antlr
     cp XonLexer.ts ../xon-lexer.ts
     cp XonParser.ts ../xon-parser.ts
-    npx rimraf .antlr
 }
 
+cd src/grammar
 
-lexer_date=$(date -r ./src/grammar/XonLexer.g4 "+%Y-%m-%d %H:%M:%S")
-lexer_ts_date=$(date -r ./src/grammar/.antlr/XonLexer.ts "+%Y-%m-%d %H:%M:%S")
-parser_date=$(date -r ./src/grammar/XonParser.g4 "+%Y-%m-%d %H:%M:%S")
-parser_ts_date=$(date -r ./src/grammar/.antlr/XonParser.ts "+%Y-%m-%d %H:%M:%S")
+lexer_date=$(date -r XonLexer.g4 "+%Y-%m-%d %H:%M:%S")
+lexer_ts_date=$(date -r xon-lexer.ts "+%Y-%m-%d %H:%M:%S")
+
+parser_date=$(date -r XonParser.g4 "+%Y-%m-%d %H:%M:%S")
+parser_ts_date=$(date -r xon-parser.ts "+%Y-%m-%d %H:%M:%S")
 
 
 if [[ "$lexer_date" > "$lexer_ts_date" ]] ;
