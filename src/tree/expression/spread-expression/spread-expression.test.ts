23b93e2ed4867a013f69ef0ce4975f52066cc01b
nizami <nizami.dev@gmail.com>
2020-04-13 12:16:41 +0300

    Update Spread test

--- src/tree/expression/spread-expression/spread-expression.test.ts
+++ src/tree/expression/spread-expression/spread-expression.test.ts
@@ -1,8 +1,11 @@
 import { parseCode } from '../../../test-helper';
+import { ArrayExpressionTree } from '../array-expression/array-expression.tree';
 import { SpreadExpressionTree } from './spread-expression.tree';
 
 test('spread expression', () => {
-    const code = '...some.object.propName';
-    const tree = parseCode(code, SpreadExpressionTree);
-    expect(tree).toBeInstanceOf(SpreadExpressionTree);
+    const code = '[...[1,2,3]]';
+    const tree = parseCode(code, ArrayExpressionTree);
+    const item = tree.items[0] as SpreadExpressionTree;
+    expect(item).toBeInstanceOf(SpreadExpressionTree);
+    expect(item.value).toBeInstanceOf(ArrayExpressionTree);
 });
