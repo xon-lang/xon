e5930172d3bc7ce27dbc2481fc1588d7bd9de2ea
nizami <nizami.dev@gmail.com>
2020-08-01 12:32:56 +0300

    Test pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,12 +1,12 @@
 # { git diff --name-only ; git diff --name-only --staged ; } | grep '.ts$' \
 # && npm test || echo 'Tests skipped'
 
-if [[ $(git diff --name-only --staged) ]]; then
-    git diff --name-only --staged | grep '.ts$' &&
-        npm test ||
-        echo 'Tests skipped. No changed ts files in staged'
-else
-    git diff --name-only | grep '.ts$' &&
-        npm test ||
-        echo 'Tests skipped. No changed ts files'
-fi
+# if [[ $(git diff --name-only --staged) ]]; then
+#     git diff --name-only --staged | grep '.ts$' &&
+#         npm test ||
+#         echo 'Tests skipped. No changed ts files in staged'
+# else
+#     git diff --name-only | grep '.ts$' &&
+#         npm test ||
+#         echo 'Tests skipped. No changed ts files'
+# fi
