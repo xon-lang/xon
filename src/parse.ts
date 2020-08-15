5be2b05a85b6d1ed4d315f751a59be7c475e2800
nizami <nizami.dev@gmail.com>
2020-08-15 21:53:23 +0300

    Refactoring parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -1,27 +1,67 @@
-import { ParserRuleContext } from 'antlr4ts';
-import { Parser } from './parser';
+import { ANTLRInputStream, CommonTokenStream, ParserRuleContext } from 'antlr4ts';
+import * as fs from 'fs';
+import path from 'path';
+import { XonLexer } from './grammar/xon-lexer';
+import { XonParser } from './grammar/xon-parser';
 import { BaseTree } from './tree/base.tree';
+import { getExpressionTree } from './tree/expression/expression-helper';
 import { ExpressionTree } from './tree/expression/expression.tree';
+import { getStatementTree } from './tree/statement/statement-helper';
 
 export function parse(code: string) {
-    return new Parser().parse(code);
+    const inputStream = new ANTLRInputStream(code);
+    const lexer = new XonLexer(inputStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new XonParser(tokenStream);
+    return parser;
 }
 
 export function parseExpression(code: string): ExpressionTree {
-    return new Parser().parseExpression(code);
+    const parser = this.parse(code);
+    const ctx = parser.expression();
+    return getExpressionTree(ctx);
 }
 
 export function parseStatement(code: string) {
-    return new Parser().parseStatement(code);
+    return getStatementTree(this.parse(code).statement());
 }
 
 export function parseCode<T extends BaseTree>(
     code: string,
     type: new (ctx: ParserRuleContext) => T
 ) {
-    return new Parser().parseCode(code, type);
+    const parser = this.parse(code);
+    if (type.name.endsWith('LiteralTree')) {
+        return new type(parser.literal());
+    }
+    if (type.name.endsWith('ExpressionTree')) {
+        return new type(parser.expression());
+    }
+    if (type.name.endsWith('StatementTree')) {
+        return new type(parser.statement());
+    }
+    if (type.name.endsWith('DefinitionTree')) {
+        return new type(parser.definition());
+    }
+
+    const methodName = camelize(type.name.replace(/Tree$/, ''));
+    if (methodName in parser) {
+        return new type((parser as any)[methodName]());
+    }
+
+    throw new Error('No ' + methodName + ' for ' + type.name);
 }
 
 export function parseFile<T extends BaseTree>(filePath: string, type: new (ctx) => T) {
-    return new Parser().parseFile(filePath, type);
+    filePath = path.join(process.cwd(), 'src', filePath);
+    const code = fs.readFileSync(filePath, 'utf8');
+    return this.parseCode(code, type);
+}
+
+function camelize(str: string) {
+    return str
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+            return index == 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, '');
 }
