4f737970045920e5c47ab14d0edf81797ecd017f
nizami <nizami.dev@gmail.com>
2020-09-12 23:02:46 +0300

    Update Select tests

--- src/tree/expression/select-expression/select-expression.test.ts
+++ src/tree/expression/select-expression/select-expression.test.ts
@@ -42,3 +42,29 @@ test('select with two cases', () => {
     const func = expr.object as IdExpressionTree;
     expect(func.name).toBe('log');
 });
+
+test('select with value equality', () => {
+    const code = `select expression:
+    true:
+        5 + 5
+        trueLog()
+    3.14:
+        log('pi')
+        anotherMethod()`;
+    const tree = parseExpression<SelectExpressionTree>(code);
+    expect(tree).toBeInstanceOf(SelectExpressionTree);
+    expect(tree.value.as<IdExpressionTree>()).toBeInstanceOf(IdExpressionTree);
+    expect(tree.cases.length).toBe(2);
+
+    expect(evalExpression(tree.cases[0].value)).toBe(true);
+    expect(tree.cases[0].statements.length).toBe(2);
+    const statements = tree.cases[0].statements[0] as ExpressionStatementTree;
+    expect(evalExpression(statements.value)).toBe(5 + 5);
+
+    expect(evalExpression(tree.cases[1].value)).toBe(3.14);
+    expect(tree.cases[1].statements.length).toBe(2);
+    const body2 = tree.cases[1].statements[0] as ExpressionStatementTree;
+    const expr = body2.value as FunctionExpressionTree;
+    const func = expr.object as IdExpressionTree;
+    expect(func.name).toBe('log');
+});
