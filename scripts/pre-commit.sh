c467e528be32a53f7799eb39c519ac0055b19fff
nizami <nizami.dev@gmail.com>
2021-01-31 04:06:21 +0300

    Check linting

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,10 +2,10 @@
 
 if [[ $(git diff --name-only --staged) ]]; then
     if [[ $(git diff --name-only --staged | grep '.ts$') ]]; then
-        npm run lint && npm run test
+        npm run lint
     fi
 else
     if [[ $(git diff --name-only | grep '.ts$') ]]; then
-        npm run lint && npm run test
+        npm run lint
     fi
 fi
