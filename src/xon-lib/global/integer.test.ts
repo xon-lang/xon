984f7971db1c3c3fc980af16455d5e2158d28259
nizami <nizami.dev@gmail.com>
2021-02-07 02:59:37 +0300

    Add integer and float types

--- src/xon-lib/global/integer.test.ts
+++ src/xon-lib/global/integer.test.ts
@@ -1,8 +1,8 @@
 import { parseExpression } from '../../parse';
 import { AddExpressionTree } from '../../tree/expression/add-expression/add-expression.tree';
 
-test('num + num', () => {
-  const code = '1.1+1';
+test('integer + integer', () => {
+  const code = '1+1';
   const tree = parseExpression<AddExpressionTree>(code);
   expect(tree.getType().name).toBe('Number');
 });
