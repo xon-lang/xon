71c7c9119a9c6a31810514435d934d6c1e803836
nizami <nizami.dev@gmail.com>
2021-02-12 05:59:58 +0300

    Update method-member rule. Add tests.

--- src/tree/definition/member/method-member/method-member.test.ts
+++ src/tree/definition/member/method-member/method-member.test.ts
@@ -6,6 +6,8 @@ test('method member', () => {
   const tree = parseMember<MethodMemberTree>(code);
 
   expect(tree.name).toBe('func_name');
+  expect(tree.isAbstract).toBe(false);
+  expect(tree.returnType?.name).toBe('String');
   expect(tree.args.length).toBe(3);
 
   expect(tree.args[0].name).toBe('arg1');
@@ -16,4 +18,30 @@ test('method member', () => {
 
   expect(tree.args[2].name).toBe('arg3');
   expect(tree.args[2].type.name).toBe('String');
+});
+
+test('abstract method member', () => {
+  const code = 'func_name(a Integer) String';
+  const tree = parseMember<MethodMemberTree>(code);
+
+  expect(tree.name).toBe('func_name');
+  expect(tree.isAbstract).toBe(true);
+  expect(tree.returnType?.name).toBe('String');
+
+  expect(tree.args.length).toBe(1);
+  expect(tree.args[0].name).toBe('a');
+  expect(tree.args[0].type.name).toBe('Integer');
+});
+
+test('no return type', () => {
+  const code = 'func_name(a Integer)';
+  const tree = parseMember<MethodMemberTree>(code);
+
+  expect(tree.name).toBe('func_name');
+  expect(tree.isAbstract).toBe(true);
+  expect(tree.returnType).toBeFalsy();
+
+  expect(tree.args.length).toBe(1);
+  expect(tree.args[0].name).toBe('a');
+  expect(tree.args[0].type.name).toBe('Integer');
 });
