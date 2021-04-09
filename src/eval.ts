cf21dd5eba42aec58eba8b58fcb70db6792b00c5
nizami <nizami.dev@gmail.com>
2021-04-10 01:19:41 +0300

    Refactoring

--- src/eval.ts
+++ src/eval.ts
@@ -4,13 +4,20 @@ import { LiteralExpressionTree } from './tree/expression/literal-expression/lite
 import { OperatorExpressionTree } from './tree/expression/operator-expression/operator-expression.tree';
 import { ParenthesizedExpressionTree } from './tree/expression/parenthesized-expression/parenthesized-expression.tree';
 
+const escapeIfString = (s: unknown) => (typeof s === 'string' ? `\`${s}\`` : s);
+
 export const evalExpression = (tree: ExpressionTree, argsMap = {}): unknown => {
   if (tree === undefined) return undefined;
 
   if (tree instanceof LiteralExpressionTree) return tree.literal.value;
   if (tree instanceof ParenthesizedExpressionTree) return evalExpression(tree.value);
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  if (tree instanceof OperatorExpressionTree) return evalOperatorExpression(tree, argsMap);
+  if (tree instanceof OperatorExpressionTree) {
+    const a = evalExpression(tree.left, argsMap);
+    const b = evalExpression(tree.right, argsMap);
+    const o = tree.operator === '^' ? '**' : tree.operator;
+    // eslint-disable-next-line no-eval
+    return eval(`${escapeIfString(a)} ${o} ${escapeIfString(b)}`);
+  }
 
   if (tree instanceof IdExpressionTree) {
     if (tree.name in argsMap) return argsMap[tree.name];
@@ -18,14 +25,4 @@ export const evalExpression = (tree: ExpressionTree, argsMap = {}): unknown => {
   }
 
   throw new Error('Unsupported operation');
-};
-
-const escapeIfString = (s: unknown) => (typeof s === 'string' ? `\`${s}\`` : s);
-
-export const evalOperatorExpression = (tree: OperatorExpressionTree, argsMap = {}): unknown => {
-  const a = evalExpression(tree.left, argsMap);
-  const b = evalExpression(tree.right, argsMap);
-  const o = tree.operator === '^' ? '**' : tree.operator;
-  // eslint-disable-next-line no-eval
-  return eval(`${escapeIfString(a)} ${o} ${escapeIfString(b)}`);
 };
