8936d61d6900e7804d64ec3b4a14b589e5954dc6
nizami <nizami.dev@gmail.com>
2021-04-11 20:22:14 +0300

    Add dataType to all tree

--- src/tree/expression/expression.tree.ts
+++ src/tree/expression/expression.tree.ts
@@ -1,11 +1,8 @@
 import { ExpressionContext } from '../../grammar/xon-parser';
 import { BaseTree } from '../base.tree';
-import { TypeTree } from '../type/type.tree';
 
 export abstract class ExpressionTree extends BaseTree {
   public ctx?: ExpressionContext;
-
-  public dataType: TypeTree;
 
   public metaType: string = this.constructor.name.replace(/ExpressionTree/, '');
 }
