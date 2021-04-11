f96cd71971f016cd26099c0a88ef0e2487101298
nizami <nizami.dev@gmail.com>
2021-04-11 20:43:00 +0300

    Update typing util methods

--- src/processing/typing.ts
+++ src/processing/typing.ts
@@ -1,6 +1,6 @@
-import { TypeTree } from '../tree/type/type.tree';
+import { DataType } from './data-type';
 
-const scopes: { name: string; dataType: TypeTree }[][] = [];
+const scopes: { name: string; dataType: DataType }[][] = [];
 
 export const pushScope = (): void => {
   scopes.push([]);
@@ -10,13 +10,13 @@ export const popScope = (): void => {
   scopes.pop();
 };
 
-export const findInScopes = (name: string): { name: string; dataType: TypeTree } => {
+export const findInScopes = (name: string): { name: string; dataType: DataType } => {
   const scope = scopes.reverse().find((x) => x.some((z) => z.name === name));
   if (!scope) return null;
   return scope.find((x) => x.name === name);
 };
 
-export const addToScope = (name: string, dataType: TypeTree): void => {
+export const addToScope = (name: string, dataType: DataType): void => {
   if (findInScopes(name)) throw new Error(`"${name}" already exists in the scope`);
   scopes[scopes.length - 1].push({ dataType, name });
 };
