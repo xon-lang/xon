f461ca550b831074e161fb9b75afc1b0cd36659e
nizami <nizami.dev@gmail.com>
2021-04-13 01:35:13 +0300

    Update definition-storage

---
+++ src/type/definition-storage.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import { glob } from 'glob';
+import { DefinitionTree } from '../tree/definition/definition.tree';
+import { parseProgram } from '../tree/parse';
+import { ProgramTree } from '../tree/program/program.tree';
+
+const definitionCache = new Map<string, DefinitionTree>();
+
+function loadProgramByPath(filePath: string): ProgramTree {
+  if (!fs.existsSync(filePath)) throw new Error(`File "${filePath}" not found`);
+
+  const code = fs.readFileSync(filePath).toString();
+  return parseProgram(code);
+}
+
+// load libraries
+glob
+  .sync('src/xon-lib/**/*.xon')
+  .map(loadProgramByPath)
+  .map((x) => x.definitions)
+  .forEach((x) =>
+    x.forEach((z) => {
+      definitionCache.set(z.name, z);
+    }),
+  );
+
+export function findDefinition(name: string): DefinitionTree {
+  return definitionCache.get(name);
+}
+
+// function getTypeGenerics(type: TypeTree): TypeTree[] {
+//   if (type instanceof ArrayTypeTree) {
+//     return [type.itemType];
+//   }
+//   if (type instanceof GenericTypeTree) {
+//     return type.generics;
+//   }
+//   return [];
+// }
+
+// export function getTypeDefinition(type: TypeTree): DefinitionTree {
+//   let name = '';
+//   const generics: Map<string, TypeTree> = new Map();
+//   if (type instanceof PlainTypeTree) name = type.name;
+//   if (type instanceof GenericTypeTree) name = type.mainType.name;
+//   if (type instanceof ArrayTypeTree) name = 'Array';
+
+//   if (name) {
+//     if (definitionCache.has(name)) return definitionCache.get(name);
+
+//     if (!fs.existsSync(libTypePaths[name]))
+//       throw new Error(`File "${libTypePaths[name]}" doesn't exists`);
+
+//     const code = fs.readFileSync(libTypePaths[name]).toString();
+//     const definition = parseDefinition(code);
+//     getTypeGenerics(type).forEach((x, i) => generics.set(definition.generics[i], x));
+//     definition.useGenerics(generics);
+//     definition.body();
+
+//     definitionCache.set(name, definition);
+//     return definition;
+//   }
+
+//   throw new Error(`Wrong argument type ${type.toJson()}`);
+// }
