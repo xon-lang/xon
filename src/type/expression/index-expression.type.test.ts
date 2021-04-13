9dbaa5c4a6f779f5fb94e1ee579a2a7e1175f997
nizami <nizami.dev@gmail.com>
2021-04-14 02:10:15 +0300

    Add index and array expression types tests

--- src/type/expression/index-expression.type.test.ts
+++ src/type/expression/index-expression.type.test.ts
@@ -2,20 +2,20 @@ import { parseStatement } from '../../tree/parse';
 import { ExpressionStatementTree } from '../../tree/statement/expression-statement/expression-statement.tree';
 import { getExpressionType } from './expression.type';
 
-test('all array items are integer literals', () => {
-  const code = '[1, 2, 3, 4]';
+test('index type is Integer', () => {
+  const code = '[1, 2, 3][1]';
   const tree = parseStatement<ExpressionStatementTree>(code);
   expect(tree).toBeInstanceOf(ExpressionStatementTree);
 
   const type = getExpressionType(tree.value);
-  expect('Integer[]').toBe(type.toString());
+  expect('Integer').toBe(type.toString());
 });
 
-test('array items are float, string, integer and boolean literals', () => {
-  const code = '[1, 2, true, "Hi", 2.7, 4]';
+test('index type is Union', () => {
+  const code = '["1", 2, 3][1]';
   const tree = parseStatement<ExpressionStatementTree>(code);
   expect(tree).toBeInstanceOf(ExpressionStatementTree);
 
   const type = getExpressionType(tree.value);
-  expect('(Integer | Boolean | String | Float)[]').toBe(type.toString());
+  expect('String | Integer').toBe(type.toString());
 });
