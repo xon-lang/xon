2a5ca52b2218cce6f0c51e4f9b1f40d1db1799e4
nizami <nizami.dev@gmail.com>
2021-04-15 22:39:17 +0300

    Fill method member type

--- src/type/definition/member/member-type.helper.ts
+++ src/type/definition/member/member-type.helper.ts
@@ -1,9 +1,12 @@
 import { InitMemberTree } from '../../../tree/definition/member/init-member/init-member.tree';
 import { MemberTree } from '../../../tree/definition/member/member.tree';
+import { MethodMemberTree } from '../../../tree/definition/member/method-member/method-member.tree';
 import { GenericsMap } from '../../generics-map';
 import { InitMemberType } from './init-member/init-member.type';
+import { MethodMemberType } from './method-member/method-member.type';
 
 export function fillMemberTypes(tree: MemberTree, genericsMap: GenericsMap): void {
   if (tree instanceof InitMemberTree) new InitMemberType(tree, genericsMap).fillTypes();
+  else if (tree instanceof MethodMemberTree) new MethodMemberType(tree, genericsMap).fillTypes();
   else throw new Error('Member type not found');
 }
---
+++ src/type/definition/member/method-member/method-member.type.ts
@@ -0,0 +1,28 @@
+import { MethodMemberTree } from '../../../../tree/definition/member/method-member/method-member.tree';
+import { GenericsMap } from '../../../generics-map';
+import { addToScope, popScope, pushScope } from '../../../id-scope';
+import { fillStatementTypes } from '../../../statement/statement-type.helper';
+import { MemberType } from '../member.type';
+
+export class MethodMemberType extends MemberType {
+  public constructor(public tree: MethodMemberTree, public genericsMap: GenericsMap) {
+    super();
+  }
+
+  public fillTypes(): void {
+    this.tree.parameters.forEach((x) => {
+      // eslint-disable-next-line no-param-reassign
+      x.type = x.type.useGenericsMap(this.genericsMap);
+      addToScope(x.name, x.type);
+    });
+
+    // if(this.tree.returnType)
+    // addToScope(this.tree.name, createFunctionType(this.tree.parameters.map(x=>x.type), this.tree.returnType));
+    // else
+    // addToScope(this.tree.name, createActionType(this.tree.parameters.map(x=>x.type)));
+
+    pushScope();
+    this.tree.body.forEach((x) => fillStatementTypes(x, this.genericsMap));
+    popScope();
+  }
+}
