7286d145e9a8f7dd08ac6aa9669677d40ae84657
nizami <nizami.dev@gmail.com>
2021-04-15 22:56:52 +0300

    Fill property member types

--- src/type/definition/member/member-type.helper.ts
+++ src/type/definition/member/member-type.helper.ts
@@ -2,15 +2,19 @@ import { InitMemberTree } from '../../../tree/definition/member/init-member/init
 import { MemberTree } from '../../../tree/definition/member/member.tree';
 import { MethodMemberTree } from '../../../tree/definition/member/method-member/method-member.tree';
 import { OperatorMemberTree } from '../../../tree/definition/member/operator-member/operator-member.tree';
+import { PropertyMemberTree } from '../../../tree/definition/member/property-member/property-member.tree';
 import { GenericsMap } from '../../generics-map';
 import { InitMemberType } from './init-member/init-member.type';
 import { MethodMemberType } from './method-member/method-member.type';
 import { OperatorMemberType } from './operator-member/operator-member.type';
+import { PropertyMemberType } from './property-member/property-member.type';
 
 export function fillMemberTypes(tree: MemberTree, genericsMap: GenericsMap): void {
   if (tree instanceof InitMemberTree) new InitMemberType(tree, genericsMap).fillTypes();
   else if (tree instanceof MethodMemberTree) new MethodMemberType(tree, genericsMap).fillTypes();
   else if (tree instanceof OperatorMemberTree)
     new OperatorMemberType(tree, genericsMap).fillTypes();
+  else if (tree instanceof PropertyMemberTree)
+    new PropertyMemberType(tree, genericsMap).fillTypes();
   else throw new Error('Member type not found');
 }
---
+++ src/type/definition/member/property-member/property-member.type.ts
@@ -0,0 +1,15 @@
+import { PropertyMemberTree } from '../../../../tree/definition/member/property-member/property-member.tree';
+import { fillExpressionTypes } from '../../../expression/expression-type.helper';
+import { GenericsMap } from '../../../generics-map';
+import { MemberType } from '../member.type';
+
+export class PropertyMemberType extends MemberType {
+  public constructor(public tree: PropertyMemberTree, public genericsMap: GenericsMap) {
+    super();
+  }
+
+  public fillTypes(): void {
+    this.tree.returnType = this.tree.returnType.useGenericsMap(this.genericsMap);
+    fillExpressionTypes(this.tree.value, this.genericsMap);
+  }
+}
