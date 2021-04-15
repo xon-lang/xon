cf97b698caa51a2e62c1c4e17319c208b0d71686
nizami <nizami.dev@gmail.com>
2021-04-15 22:15:38 +0300

    Fill types for init member

---
+++ src/type/definition/member/init-member/init-member.test.ts
@@ -0,0 +1,13 @@
+import { InitMemberTree } from '../../../../tree/definition/member/init-member/init-member.tree';
+import { parseMember } from '../../../../tree/parse';
+import { ExpressionStatementTree } from '../../../../tree/statement/expression-statement/expression-statement.tree';
+import { fillMemberTypes } from '../member-type.helper';
+
+test('has one statement', () => {
+  const code = 'init:\n    1+1';
+  const tree = parseMember<InitMemberTree>(code);
+  expect(tree).toBeInstanceOf(InitMemberTree);
+
+  fillMemberTypes(tree, new Map());
+  expect((tree.body[0] as ExpressionStatementTree).value.type.toString()).toBe('Integer');
+});
--- src/type/definition/member/member-type.helper.ts
+++ src/type/definition/member/member-type.helper.ts
@@ -3,7 +3,7 @@ import { MemberTree } from '../../../tree/definition/member/member.tree';
 import { GenericsMap } from '../../generics-map';
 import { InitMemberType } from './init-member/init-member.type';
 
-export function fillStatementTypes(tree: MemberTree, genericsMap: GenericsMap): void {
+export function fillMemberTypes(tree: MemberTree, genericsMap: GenericsMap): void {
   if (tree instanceof InitMemberTree) new InitMemberType(tree, genericsMap).fillTypes();
   else throw new Error('Member type not found');
 }
