08eaad91520e494129afc1421124c1e27f8f2193
nizami <nizami.dev@gmail.com>
2021-10-08 03:44:36 +0300

    Update prefix expression

--- src/tree/expression/prefix-expression/prefix-expression.tree.ts
+++ src/tree/expression/prefix-expression/prefix-expression.tree.ts
@@ -1,13 +1,17 @@
-import { ExpressionContext } from '../../../grammar/xon-parser';
+import { PrefixExpressionContext } from '../../../grammar/xon-parser';
+import { getExpressionTree } from '../expression-tree.helper';
 import { ExpressionTree } from '../expression.tree';
 
 export class PrefixExpressionTree extends ExpressionTree {
-  public constructor(
-    public ctx: ExpressionContext,
-    public operator: string,
-    public value: ExpressionTree,
-  ) {
+  public operator: string;
+  public value: ExpressionTree;
+
+  public constructor(public ctx?: PrefixExpressionContext) {
     super();
+    if (!ctx) return;
+
+    this.operator = ctx.operator().text;
+    this.value = getExpressionTree(ctx.expression());
   }
 
   public toString(): string {
