912b7a9c5c2178a378905a696da97df21fd0bb8b
nizami <nizami.dev@gmail.com>
2021-10-13 00:27:46 +0300

    Revert "FIx infix declration test"
    This reverts commit 8e4061b88c99d8197f8379cba9904ca54ec50b9c.

--- src/tree/expression/expression.tree.test.ts
+++ src/tree/expression/expression.tree.test.ts
@@ -23,9 +23,9 @@ test('3', () => {
 });
 
 test('4', () => {
-  const code = '2>=1<=3';
+  const code = '2>=1<=3 or 4!=5 and not 6';
   const tree = parseExpression(code);
-  expect(tree.toString()).toBe('((2 > 1 || 2 == 1) < 3 || (2 > 1 || 2 == 1) == 3)');
+  expect(tree.toString()).toBe('(2 > 1 or 2 == 1) and (1 < 3 or 1 == 3) or not (4 == 5) and not 6');
   // expect(evalExpression(tree)).toBe((1 * (2 + (3 - 4))) / 5);
 });
 
@@ -33,7 +33,7 @@ test('5', () => {
   const code = '1<2<3>0<=1>=5';
   const tree = parseExpression(code);
   expect(tree.toString()).toBe(
-    '((1 < 2 < 3 > 0 < 1 || 1 < 2 < 3 > 0 == 1) > 5 || (1 < 2 < 3 > 0 < 1 || 1 < 2 < 3 > 0 == 1) == 5)',
+    '1 < 2 and 2 < 3 and 3 > 0 and (0 < 1 or 0 == 1) and (1 > 5 or 1 == 5)',
   );
   // expect(evalExpression(tree)).toBe((1 * (2 + (3 - 4))) / 5);
 });
