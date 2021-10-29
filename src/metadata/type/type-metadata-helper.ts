b860c79003ab5314fddad37ba8eda4aea4f94ccb
nizami <nizami.dev@gmail.com>
2021-10-29 16:39:28 +0300

    Fix type handler tests

--- src/metadata/type/type-metadata-helper.ts
+++ src/metadata/type/type-metadata-helper.ts
@@ -1,10 +1,9 @@
 import { ClassDefinitionTree } from '../../tree/definition/class-definition/class-definition-tree';
 import { DefinitionTree } from '../../tree/definition/definition-tree';
-import { TypeTree } from '../../tree/type/type.tree';
 import { ClassTypeMetadata } from './id-type/class-type/class-type-metadata';
-import { TypeMetadata } from './type-metadata';
+import { DefinitionTypeMetadata } from './id-type/definition-type-metadata';
 
-export const getDefinitionMetadata = (tree: DefinitionTree | TypeTree): TypeMetadata => {
+export const getDefinitionMetadata = (tree: DefinitionTree): DefinitionTypeMetadata => {
   if (tree === undefined) return undefined;
 
   if (tree instanceof ClassDefinitionTree) return new ClassTypeMetadata(tree);
