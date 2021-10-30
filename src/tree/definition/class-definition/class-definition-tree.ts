15b33a7a5e357c9fd71fa5a576c89b39e245f08e
nizami <nizami.dev@gmail.com>
2021-10-30 17:22:37 +0300

    Update class definition tree

--- src/tree/definition/class-definition/class-definition-tree.ts
+++ src/tree/definition/class-definition/class-definition-tree.ts
@@ -7,12 +7,20 @@ import { TypeTree } from '../../type/type.tree';
 import { DefinitionTree } from '../definition-tree';
 import { getClassMembersTrees } from './class-member/class-member-tree.helper';
 import { ClassMemberTree } from './class-member/class-member.tree';
+import { InitClassMemberTree } from './class-member/init-class-member/init-class-member-tree';
+import { MethodClassMemberTree } from './class-member/method-class-member/method-class-member-tree';
+import { OperatorClassMemberTree } from './class-member/operator-class-member/operator-class-member-tree';
+import { PropertyClassMemberTree } from './class-member/property-class-member/property-class-member-tree';
 
 export class ClassDefinitionTree extends DefinitionTree {
   public genericParameters: IdToken[] = [];
   public parameters: ParameterTree[];
   public baseType?: TypeTree;
   public members: ClassMemberTree[] = [];
+  public properties: PropertyClassMemberTree[] = [];
+  public inits: InitClassMemberTree[] = [];
+  public methods: MethodClassMemberTree[] = [];
+  public operators: OperatorClassMemberTree[] = [];
 
   public constructor(public ctx?: ClassDefinitionContext) {
     super();
@@ -24,5 +32,12 @@ export class ClassDefinitionTree extends DefinitionTree {
     this.parameters = getParametersTrees(ctx.parameters());
     this.baseType = getTypeTree(ctx.type());
     this.members = getClassMembersTrees(ctx.classMember());
+
+    for (const member of this.members) {
+      if (member instanceof PropertyClassMemberTree) this.properties.push(member);
+      if (member instanceof InitClassMemberTree) this.inits.push(member);
+      if (member instanceof MethodClassMemberTree) this.methods.push(member);
+      if (member instanceof OperatorClassMemberTree) this.operators.push(member);
+    }
   }
 }
