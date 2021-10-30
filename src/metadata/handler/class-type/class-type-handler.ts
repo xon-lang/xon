e1b72aebeea94044504cd7c3adacb7fccd2b192a
nizami <nizami.dev@gmail.com>
2021-10-30 07:04:07 +0300

    Refactoring

--- src/metadata/handler/class-type/class-type-handler.ts
+++ src/metadata/handler/class-type/class-type-handler.ts
@@ -1,9 +1,8 @@
 import { ClassDefinitionTree } from '../../../tree/definition/class-definition/class-definition-tree';
 import { PropertyClassMemberTree } from '../../../tree/definition/class-definition/class-member/property-class-member/property-class-member-tree';
-import { ExpressionHandler } from '../expression/expression-handler';
+import { DeclarationHandler } from '../declaration-handler/declaration-handler';
 import { HandlerScope } from '../handler-scope';
 import { MetadataHandler } from '../metadata-handler';
-import { TypeHandler } from '../type/type-handler';
 
 export class ClassTypeHandler extends MetadataHandler {
   public constructor(scope: HandlerScope) {
@@ -14,25 +13,13 @@ export class ClassTypeHandler extends MetadataHandler {
     this.scope.addDefinition(tree);
 
     this.handleProperties(
-      tree.members
-        .filter((x) => x instanceof PropertyClassMemberTree)
-        .map((x) => x as PropertyClassMemberTree),
+      tree.members.filter((x) => x instanceof PropertyClassMemberTree) as PropertyClassMemberTree[],
     );
   }
 
   handleProperties(properties: PropertyClassMemberTree[]) {
     for (const property of properties) {
-      property.id.declarationLink = property.id.sourceReference
-      property.typeMetadata
-
-      property.id.declarationLink = property.id.sourceReference;
-      if (property.type) new TypeHandler(this.scope).handle(property.type);
-      if (property.value) new ExpressionHandler(this.scope).handle(property.value);
-      property.typeMetadata = property.type?.typeMetadata || property.value.typeMetadata;
-      this.scope.addDeclaration(property);
-      return;
-
-      this.scope.addDeclaration(property);
+      new DeclarationHandler(this.scope).handle(property);
     }
   }
 }
