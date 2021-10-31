3021b23f50703ef235a8f08602616f96ae983fde
nizami <nizami.dev@gmail.com>
2021-10-31 14:25:13 +0300

    Refactoring

--- src/metadata/type/function-type/function-type-metadata.ts
+++ src/metadata/type/function-type/function-type-metadata.ts
@@ -1,13 +1,13 @@
-import { ParameterTree } from '../../../tree/parameter/parameter.tree';
 import { SourceReference } from '../../../tree/source-reference';
 import { FunctionTypeTree } from '../../../tree/type/function-type/function-type.tree';
+import { ParameterTypeMetadata } from '../parameter-type/parameter-type-metadata';
 import { TypeMetadata } from '../type-metadata';
 
 export class FunctionTypeMetadata extends TypeMetadata {
   sourceReference: SourceReference;
   name: string;
   genericArguments: TypeMetadata[] = [];
-  parameters: ParameterMetadata[] = [];
+  parameters: ParameterTypeMetadata[] = [];
   returnType?: TypeMetadata;
 
   constructor(tree: FunctionTypeTree, genericArguments: TypeMetadata[]) {
@@ -16,21 +16,7 @@ export class FunctionTypeMetadata extends TypeMetadata {
     this.sourceReference = tree.sourceReference;
     this.name = this.constructor.name.replace(TypeMetadata.name, '');
     this.genericArguments = genericArguments;
-    this.parameters = tree.parameters.map((x) => new ParameterMetadata(x));
+    this.parameters = tree.parameters.map((x) => new ParameterTypeMetadata(x));
     this.returnType = tree.returnType?.typeMetadata;
-  }
-}
-
-export class ParameterMetadata extends TypeMetadata {
-  sourceReference: SourceReference;
-  name: string;
-  type: TypeMetadata;
-
-  constructor(tree: ParameterTree) {
-    super();
-
-    this.sourceReference = tree.id.sourceReference;
-    this.name = tree.id.text;
-    this.type = tree.typeMetadata;
   }
 }
---
+++ src/metadata/type/parameter-type/parameter-type-metadata.ts
@@ -0,0 +1,17 @@
+import { ParameterTree } from '../../../tree/parameter/parameter.tree';
+import { SourceReference } from '../../../tree/source-reference';
+import { TypeMetadata } from '../type-metadata';
+
+export class ParameterTypeMetadata extends TypeMetadata {
+  sourceReference: SourceReference;
+  name: string;
+  type: TypeMetadata;
+
+  constructor(tree: ParameterTree) {
+    super();
+
+    this.sourceReference = tree.id.sourceReference;
+    this.name = tree.id.text;
+    this.type = tree.typeMetadata;
+  }
+}
