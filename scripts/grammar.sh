8de339c63f415bc9db65a231ae7f4fafbf0ecba9
nizami <nizami.dev@gmail.com>
2021-10-31 12:02:53 +0300

    Update grammar compilation script

--- scripts/grammar.sh
+++ scripts/grammar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-generate_grammar() {
-    npx -- antlr4ts -Werror -o .antlr -no-listener -no-visitor *.g4
+copy_ts_and_clean() {
     cp .antlr/XonLexer.ts xon-lexer.ts
-    cp .antlr/XonParser.ts xon-parser.ts && clear
+    cp .antlr/XonParser.ts xon-parser.ts
     npx rimraf .antlr
+    clear
+    echo "Grammar compiled successfully!"
 }
 
 cd src/grammar || exit
@@ -16,5 +17,8 @@ parser_date=$(date -r XonParser.g4 "+%Y-%m-%d %H:%M:%S")
 parser_ts_date=$(date -r xon-parser.ts "+%Y-%m-%d %H:%M:%S")
 
 if [[ "$lexer_date" > "$lexer_ts_date" || "$parser_date" > "$parser_ts_date" ]]; then
-    generate_grammar
+    npx -- antlr4ts -Werror -o .antlr -no-listener -no-visitor *.g4 && copy_ts_and_clean
+else
+    clear
+    echo "Grammar compiled files already up to date!"
 fi
