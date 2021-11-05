3220ea27e9cd22374c7f9c67ea15572348b790ff
nizami <nizami.dev@gmail.com>
2021-11-05 18:30:45 +0300

    Fix for statement tests

--- src/tree/statement/for-statement/for-statement.tree.ts
+++ src/tree/statement/for-statement/for-statement.tree.ts
@@ -1,14 +1,15 @@
 import { ForStatementContext } from '../../../grammar/xon-parser';
+import { BodyTree } from '../../body/body-tree';
+import { getBodyTree } from '../../body/body-tree.helper';
 import { getExpressionTree } from '../../expression/expression-tree.helper';
 import { ExpressionTree } from '../../expression/expression.tree';
-import { getStatements } from '../statement-tree.helper';
 import { StatementTree } from '../statement.tree';
 
 export class ForStatementTree extends StatementTree {
   public valueVarName?: string;
   public indexVarName?: string;
   public expression: ExpressionTree;
-  public body: StatementTree[];
+  public body: BodyTree;
 
   public constructor(public ctx?: ForStatementContext) {
     super();
@@ -17,6 +18,6 @@ export class ForStatementTree extends StatementTree {
     this.valueVarName = ctx._value?.text || null;
     this.indexVarName = ctx._index?.text || null;
     this.expression = getExpressionTree(ctx.expression());
-    this.body = getStatements(ctx.body());
+    this.body = getBodyTree(ctx.body());
   }
 }
