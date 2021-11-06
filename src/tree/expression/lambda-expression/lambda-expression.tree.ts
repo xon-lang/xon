fc7ab235f3d5a836d5eaf8515ebb555a834b1749
nizami <nizami.dev@gmail.com>
2021-11-06 03:56:20 +0300

    Fix lambda expression tests

--- src/tree/expression/lambda-expression/lambda-expression.tree.ts
+++ src/tree/expression/lambda-expression/lambda-expression.tree.ts
@@ -1,24 +1,22 @@
 import { LambdaExpressionContext } from '../../../grammar/xon-parser';
 import { getParametersTrees } from '../../parameter/parameter-tree.helper';
 import { ParameterTree } from '../../parameter/parameter.tree';
-import { getStatementsFromBody } from '../../statement/statement-tree.helper';
-import { StatementTree } from '../../statement/statement.tree';
+import { getExpressionTree } from '../expression-tree.helper';
 import { ExpressionTree } from '../expression.tree';
 
 export class LambdaExpressionTree extends ExpressionTree {
   parameters: ParameterTree[];
-  body: StatementTree[];
+  expression: ExpressionTree;
 
   constructor(public ctx?: LambdaExpressionContext) {
     super();
     if (!ctx) return;
 
-    this.parameters = getParametersTrees(ctx.parameters());
-    this.body = getStatementsFromBody(ctx.body());
-    if (this.body.length !== 1) throw new Error('Lambda should be with single expression');
+    this.parameters = getParametersTrees(ctx.parameter());
+    this.expression = getExpressionTree(ctx.expression());
   }
 
   toString(): string {
-    return `(${this.parameters.join(', ')}): ${this.body[0]}`;
+    return `(${this.parameters.join(', ')}): ${this.expression}`;
   }
 }
