0ae443331cb116b7baace117fef02053ccec2896
nizami <nizami.dev@gmail.com>
2021-11-18 10:28:44 +0300

    Update type parameter

--- src/tree/type-parameter/type-parameter.tree.test.ts
+++ src/tree/type-parameter/type-parameter.tree.test.ts
@@ -1,20 +1,35 @@
 import { parseTypeParameter } from '../parse';
 import { TypeParameterTree } from './type-parameter.tree';
 
-test('has data type', () => {
+test('parameter name', () => {
   const code = 'T';
   const tree = parseTypeParameter(code);
   expect(tree).toBeInstanceOf(TypeParameterTree);
 
-  expect(tree.id.text).toBe('T');
   expect(tree.hasSpread).toBe(false);
+  expect(tree.id.text).toBe('T');
+  expect(tree.typeRestrict.name).toBe(null);
+  expect(tree.metaRestrict).toBe(null);
 });
 
-test('has data and meta types', () => {
+test('spread and parameter name', () => {
   const code = '...T';
   const tree = parseTypeParameter(code);
   expect(tree).toBeInstanceOf(TypeParameterTree);
 
-  expect(tree.id.text).toBe('T');
   expect(tree.hasSpread).toBe(true);
+  expect(tree.id.text).toBe('T');
+  expect(tree.typeRestrict.name).toBe(null);
+  expect(tree.metaRestrict).toBe(null);
+});
+
+test('restricts', () => {
+  const code = '...T is String#StringLiteral';
+  const tree = parseTypeParameter(code);
+  expect(tree).toBeInstanceOf(TypeParameterTree);
+
+  expect(tree.hasSpread).toBe(true);
+  expect(tree.id.text).toBe('T');
+  expect(tree.typeRestrict.name).toBe('String');
+  expect(tree.metaRestrict).toBe('StringLiteral');
 });
--- src/tree/type-parameter/type-parameter.tree.ts
+++ src/tree/type-parameter/type-parameter.tree.ts
@@ -1,17 +1,23 @@
 import { TypeParameterContext } from '../../grammar/xon-parser';
 import { BaseTree } from '../base.tree';
 import { IdToken } from '../id-token';
+import { getTypeTree } from '../type/type-tree.helper';
+import { TypeTree } from '../type/type.tree';
 
 export class TypeParameterTree extends BaseTree {
-  id: IdToken;
   hasSpread: boolean;
+  id: IdToken;
+  typeRestrict: TypeTree;
+  metaRestrict: string;
 
   constructor(public ctx?: TypeParameterContext) {
     super();
     if (!ctx) return;
 
-    this.id = new IdToken(ctx._name);
     this.hasSpread = !!ctx.SPREAD();
+    this.id = new IdToken(ctx._name);
+    this.typeRestrict = getTypeTree(ctx.type()) || null;
+    this.metaRestrict = ctx._meta?.text || null;
   }
 
   toString(): string {
