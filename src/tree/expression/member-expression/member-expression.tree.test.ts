b77b8d71e16b7c9027fc426573d995207b93b05b
nizami <nizami.dev@gmail.com>
2021-11-18 08:02:09 +0300

    Update member-expression tests

--- src/tree/expression/member-expression/member-expression.tree.test.ts
+++ src/tree/expression/member-expression/member-expression.tree.test.ts
@@ -23,3 +23,17 @@ test('safe', () => {
   expect(tree.isSafe).toBe(true);
   expect(tree.id.text).toBe('def');
 });
+
+test('safe with generics', () => {
+  const code = 'abc?.def<String, Float>';
+  const tree = parseExpression<MemberExpressionTree>(code);
+  expect(tree).toBeInstanceOf(MemberExpressionTree);
+
+  expect(tree.instance).toBeInstanceOf(IdExpressionTree);
+  expect((tree.instance as IdExpressionTree).id.text).toBe('abc');
+  expect(tree.isSafe).toBe(true);
+  expect(tree.id.text).toBe('def');
+  expect(tree.genericArguments.length).toBe(2);
+  expect(tree.genericArguments[0].name).toBe('String');
+  expect(tree.genericArguments[1].name).toBe('Float');
+});
