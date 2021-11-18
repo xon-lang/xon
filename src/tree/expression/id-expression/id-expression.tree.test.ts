df848d4730ea71f96eb60a890ac540bd1ee14766
nizami <nizami.dev@gmail.com>
2021-11-18 07:58:38 +0300

    Refactoring id-expression

--- src/tree/expression/id-expression/id-expression.tree.test.ts
+++ src/tree/expression/id-expression/id-expression.tree.test.ts
@@ -1,10 +1,21 @@
 import { parseExpression } from '../../parse';
 import { IdExpressionTree } from './id-expression.tree';
 
-test('variable', () => {
+test('id', () => {
   const code = 'myVariable';
   const tree = parseExpression<IdExpressionTree>(code);
   expect(tree).toBeInstanceOf(IdExpressionTree);
 
-  expect(tree.id.text).toBe(code);
+  expect(tree.id.text).toBe('myVariable');
+});
+
+test('id with generics', () => {
+  const code = 'abc<String, Number>';
+  const tree = parseExpression<IdExpressionTree>(code);
+  expect(tree).toBeInstanceOf(IdExpressionTree);
+
+  expect(tree.id.text).toBe('abc');
+  expect(tree.genericArguments.length).toBe(2);
+  expect(tree.genericArguments[0].toString()).toBe('String');
+  expect(tree.genericArguments[1].toString()).toBe('Number');
 });
--- src/tree/expression/id-expression/id-expression.tree.ts
+++ src/tree/expression/id-expression/id-expression.tree.ts
@@ -1,18 +1,23 @@
 import { IdExpressionContext } from '../../../grammar/xon-parser';
 import { IdToken } from '../../id-token';
+import { getTypesTrees } from '../../type/type-tree.helper';
+import { TypeTree } from '../../type/type.tree';
 import { ExpressionTree } from '../expression.tree';
 
 export class IdExpressionTree extends ExpressionTree {
   id: IdToken;
+  genericArguments: TypeTree[];
 
   constructor(public ctx?: IdExpressionContext) {
     super();
     if (!ctx) return;
 
     this.id = new IdToken(ctx._name);
+    this.genericArguments = getTypesTrees(ctx.genericArguments()?.type());
   }
 
   toString(): string {
+    if (this.genericArguments) return `<${this.genericArguments.join(', ')}>${this.id}`;
     return this.id.toString();
   }
 }
