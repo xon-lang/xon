aa18cb92807111f6fa41be8ab304d73008a0c7fb
nizami <nizami.dev@gmail.com>
2021-11-18 10:55:37 +0300

    Update handler-scope

--- src/metadata/handler/handler-scope.ts
+++ src/metadata/handler/handler-scope.ts
@@ -1,23 +1,40 @@
 import { IdToken } from '../../tree/id-token';
+import { TypeParameterTree } from '../../tree/type-parameter/type-parameter.tree';
+import { TypeTree } from '../../tree/type/type.tree';
 import { TypeMetadata } from '../type/type-metadata';
 
 export class HandlerScope {
   parent?: HandlerScope;
-  // private definitions = new Map<string, DefinitionTree>();
   private declarations = new Map<string, TypeMetadata>();
 
   constructor(parent: HandlerScope = null) {
     this.parent = parent;
   }
 
-  addDeclaration(id: IdToken, typeMetadata: TypeMetadata) {
-    if (this.declarations.has(id.text)) throw new Error(`'${id.text}' already exists`);
-    this.declarations.set(id.text, typeMetadata);
+  addDeclaration({
+    id,
+    typeMetadata,
+    typeParameters = [],
+  }: {
+    id: IdToken;
+    typeMetadata: TypeMetadata;
+    typeParameters?: TypeParameterTree[];
+  }) {
+    const name = `${id.text}<${typeParameters.length}>`;
+    if (this.declarations.has(name)) throw new Error(`'${name}' already exists`);
+    this.declarations.set(name, typeMetadata);
   }
 
-  findDeclaration(name: string): TypeMetadata {
+  findDeclaration({
+    id,
+    typeArguments = [],
+  }: {
+    id: IdToken;
+    typeArguments?: TypeTree[];
+  }): TypeMetadata {
+    const name = `${id.text}<${typeArguments.length}>`;
     if (this.declarations.has(name)) return this.declarations.get(name);
-    if (this.parent) return this.parent.findDeclaration(name);
+    if (this.parent) return this.parent.findDeclaration({ id, typeArguments });
     throw new Error(`'${name}' not found`);
   }
 }
