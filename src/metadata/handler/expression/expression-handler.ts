684c928a804a4f28f05a8731cca7f5a14f99d74f
nizami <nizami.dev@gmail.com>
2021-11-19 00:25:49 +0300

    Refactoring

--- src/metadata/handler/expression/expression-handler.ts
+++ src/metadata/handler/expression/expression-handler.ts
@@ -1,27 +1,21 @@
 import { ExpressionTree } from '../../../tree/expression/expression.tree';
 import { IdExpressionTree } from '../../../tree/expression/id-expression/id-expression.tree';
-import { InstantiationExpressionTree } from '../../../tree/expression/instantiation-expression/instantiation-expression.tree';
 import { LiteralExpressionTree } from '../../../tree/expression/literal-expression/literal-expression.tree';
-import { getDefinitionMetadata } from '../../type/type-metadata-helper';
 import { LiteralHandler } from '../literal/literal-handler';
 import { MetadataHandler } from '../metadata-handler';
-import { TypeHandler } from '../type/type-handler';
 
 export class ExpressionHandler extends MetadataHandler {
   handle(tree: ExpressionTree) {
-    if (tree instanceof IdExpressionTree) {
-      const declarationMetadata = this.scope.findDeclaration(tree.id.text);
-      tree.typeMetadata = declarationMetadata.type;
-      tree.id.declarationLink = declarationMetadata.id.sourceReference;
-      return;
-    }
-
     if (tree instanceof LiteralExpressionTree) {
       new LiteralHandler(this.scope).handle(tree.literal);
       tree.typeMetadata = tree.literal.typeMetadata;
       return;
     }
-
+    if (tree instanceof IdExpressionTree) {
+      tree.typeMetadata = this.scope.findDeclaration(tree.id.text, tree.typeArguments.length);
+      tree.id.declarationLink = tree.typeMetadata.sourceReference;
+      return;
+    }
 
     throw new Error(`'${tree.constructor.name}' handler not found`);
   }
