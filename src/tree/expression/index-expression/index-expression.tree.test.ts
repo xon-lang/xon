2df7dfa2e8d3668654721946f8358ec9ba7fae5b
nizami <nizami.dev@gmail.com>
2021-11-18 07:36:22 +0300

    Update index-expression tests

--- src/tree/expression/index-expression/index-expression.tree.test.ts
+++ src/tree/expression/index-expression/index-expression.tree.test.ts
@@ -1,23 +1,49 @@
+import { IntegerLiteralTree } from '../../literal/integer-literal/integer-literal.tree';
+import { StringLiteralTree } from '../../literal/string-literal/string-literal.tree';
 import { parseExpression } from '../../parse';
 import { IdExpressionTree } from '../id-expression/id-expression.tree';
-import { InfixExpressionTree } from '../infix-expression/infix-expression.tree';
 import { LiteralExpressionTree } from '../literal-expression/literal-expression.tree';
+import { MemberExpressionTree } from '../member-expression/member-expression.tree';
 import { IndexExpressionTree } from './index-expression.tree';
 
-test('string expression index', () => {
-  const code = 'prop["ppp"]';
+test('function index', () => {
+  const code = 'f[3, "str"]';
   const tree = parseExpression<IndexExpressionTree>(code);
-  expect(tree.instance).toBeInstanceOf(IdExpressionTree);
+  expect(tree).toBeInstanceOf(IndexExpressionTree);
 
-  expect(tree.index).toBeInstanceOf(LiteralExpressionTree);
-  expect((tree.index as LiteralExpressionTree).literal.value).toBe('ppp');
+  expect(tree.arguments.length).toBe(2);
+  expect((tree.arguments[0].value as LiteralExpressionTree).literal).toBeInstanceOf(
+    IntegerLiteralTree,
+  );
+  expect((tree.arguments[0].value as LiteralExpressionTree).literal.value).toBe(3);
+  expect((tree.arguments[1].value as LiteralExpressionTree).literal).toBeInstanceOf(
+    StringLiteralTree,
+  );
+  expect((tree.arguments[1].value as LiteralExpressionTree).literal.value).toBe('str');
+  expect(tree.instance).toBeInstanceOf(IdExpressionTree);
 });
 
-test('integer expression index', () => {
-  const code = 'prop[12+33]';
+test('function on several lines', () => {
+  const code = `f[3,
+        "str", 123, 
+    415]`;
   const tree = parseExpression<IndexExpressionTree>(code);
-  expect(tree.instance).toBeInstanceOf(IdExpressionTree);
+  expect(tree).toBeInstanceOf(IndexExpressionTree);
 
-  expect(tree.index).toBeInstanceOf(InfixExpressionTree);
-  expect((tree.index as InfixExpressionTree).left).toBeInstanceOf(LiteralExpressionTree);
+  expect(tree.arguments.length).toBe(4);
+  const [arg1, arg2] = tree.arguments.map((x) => x.value as LiteralExpressionTree);
+  expect(arg1.literal).toBeInstanceOf(IntegerLiteralTree);
+  expect(arg2.literal).toBeInstanceOf(StringLiteralTree);
+  expect(tree.instance).toBeInstanceOf(IdExpressionTree);
+});
+
+test('can index with generics', () => {
+  const code = 'a.get<String > [1]';
+  const tree = parseExpression<IndexExpressionTree>(code);
+  expect(tree).toBeInstanceOf(IndexExpressionTree);
+
+  expect(tree.arguments.length).toBe(1);
+  const [arg] = tree.arguments.map((x) => x.value as LiteralExpressionTree);
+  expect(arg.literal).toBeInstanceOf(IntegerLiteralTree);
+  expect(tree.instance).toBeInstanceOf(MemberExpressionTree);
 });
