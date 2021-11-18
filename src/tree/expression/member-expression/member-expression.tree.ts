21b8c14ad2c42c698adb2227c827457df10a7ec6
nizami <nizami.dev@gmail.com>
2021-11-18 08:01:03 +0300

    Update member-expression

--- src/tree/expression/member-expression/member-expression.tree.ts
+++ src/tree/expression/member-expression/member-expression.tree.ts
@@ -1,5 +1,7 @@
 import { MemberExpressionContext } from '../../../grammar/xon-parser';
 import { IdToken } from '../../id-token';
+import { getTypesTrees } from '../../type/type-tree.helper';
+import { TypeTree } from '../../type/type.tree';
 import { getExpressionTree } from '../expression-tree.helper';
 import { ExpressionTree } from '../expression.tree';
 
@@ -7,6 +9,7 @@ export class MemberExpressionTree extends ExpressionTree {
   instance: ExpressionTree;
   isSafe: boolean;
   id: IdToken;
+  genericArguments: TypeTree[];
 
   constructor(public ctx?: MemberExpressionContext) {
     super();
@@ -15,9 +18,12 @@ export class MemberExpressionTree extends ExpressionTree {
     this.instance = getExpressionTree(ctx.expression());
     this.isSafe = !!ctx.COALESCING();
     this.id = new IdToken(ctx._name);
+    this.genericArguments = getTypesTrees(ctx.genericArguments()?.type());
   }
 
   toString(): string {
-    return `${this.instance}${this.id}`;
+    if (this.genericArguments)
+      return `${this.instance}.${this.id}<${this.genericArguments.join(', ')}>`;
+    return `${this.instance}.${this.id}`;
   }
 }
