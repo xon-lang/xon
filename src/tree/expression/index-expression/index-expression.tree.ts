e7c9f37975465de103b5cdbcad8fb915cdae0202
nizami <nizami.dev@gmail.com>
2021-11-18 07:06:14 +0300

    Refactoring

--- src/tree/expression/index-expression/index-expression.tree.ts
+++ src/tree/expression/index-expression/index-expression.tree.ts
@@ -1,21 +1,28 @@
 import { IndexExpressionContext } from '../../../grammar/xon-parser';
+import { getArgumentsTrees } from '../../argument/argument-tree.helper';
+import { ArgumentTree } from '../../argument/argument.tree';
+import { getTypesTrees } from '../../type/type-tree.helper';
+import { TypeTree } from '../../type/type.tree';
 import { getExpressionTree } from '../expression-tree.helper';
 import { ExpressionTree } from '../expression.tree';
 
 export class IndexExpressionTree extends ExpressionTree {
+  genericArguments: TypeTree[];
   instance: ExpressionTree;
+  arguments: ArgumentTree[];
 
-  index: ExpressionTree;
-
-  constructor(public ctx: IndexExpressionContext) {
+  constructor(public ctx?: IndexExpressionContext) {
     super();
     if (!ctx) return;
 
-    this.instance = getExpressionTree(ctx.expression(0));
-    this.index = getExpressionTree(ctx.expression(1));
+    this.genericArguments = getTypesTrees(ctx.genericArguments()?.type());
+    this.instance = getExpressionTree(ctx.expression());
+    this.arguments = getArgumentsTrees(ctx.indexArguments());
   }
 
   toString(): string {
-    return `${this.instance}[${this.index}]`;
+    const generics = this.genericArguments.length ? `<${this.genericArguments.join(', ')}>` : '';
+    const args = this.arguments.length ? `(${this.arguments.join(', ')})` : '';
+    return `${this.instance}${generics}${args}`;
   }
 }
