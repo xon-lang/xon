e836f4a784eb1ded6b8fabbcb69672e0cb205d83
nizami <nizami.dev@gmail.com>
2021-11-22 19:39:19 +0300

    Remove instance expression

--- src/tree/expression/instance-expression/instance-expression.tree.test.ts
+++
@@ -1,8 +0,0 @@
-import { parseExpression } from '../../parse';
-import { InstanceExpressionTree } from './instance-expression.tree';
-
-test('has this keyword', () => {
-  const code = '$';
-  const tree = parseExpression<InstanceExpressionTree>(code);
-  expect(tree).toBeInstanceOf(InstanceExpressionTree);
-});
