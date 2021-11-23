f7673ea73e8229bb12a9e3d75aa5c3fa4c91bc3a
nizami <nizami.dev@gmail.com>
2021-11-23 13:55:22 +0300

    Update source-tree

--- src/tree/source/source-tree.ts
+++ src/tree/source/source-tree.ts
@@ -1,24 +1,31 @@
-import { DefinitionSourceMemberContext, SourceContext } from '../../grammar/xon-parser';
+import {
+  AttributeSourceMemberContext,
+  DefinitionSourceMemberContext,
+  SourceContext,
+} from '../../grammar/xon-parser';
+import { AttributeTree } from '../attribute/attribute-tree';
+import { getAttributeTree } from '../attribute/attribute-tree.helper';
 import { BaseTree } from '../base.tree';
 import { DefinitionTree } from '../definition/definition-tree';
-import { getDefinitionsTrees } from '../definition/definition-tree-helper';
+import { getDefinitionTree } from '../definition/definition-tree-helper';
 import { getLibrariesTrees } from '../import/import-tree.helper';
 import { ImportTree } from '../import/import.tree';
 
 export class SourceTree extends BaseTree {
   imports: ImportTree[];
-  definitions: DefinitionTree[];
-  
+  definitions: DefinitionTree[] = [];
+  attributes: AttributeTree[] = [];
+
   constructor(public ctx?: SourceContext) {
     super();
     if (!ctx) return;
 
     this.imports = getLibrariesTrees(ctx.library());
-    this.definitions = getDefinitionsTrees(
-      ctx
-        .sourceMember()
-        .filter((x) => x instanceof DefinitionSourceMemberContext)
-        .map((x) => (x as DefinitionSourceMemberContext).definition()),
-    );
+    for (const member of ctx.sourceMember()) {
+      if (member instanceof DefinitionSourceMemberContext)
+        this.definitions.push(getDefinitionTree(member.definition()));
+      if (member instanceof AttributeSourceMemberContext)
+        this.attributes.push(getAttributeTree(member.attribute()));
+    }
   }
 }
