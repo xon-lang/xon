27e03e1bc8976e2090d70be11e49b59d06e82ba0
nizami <nizami.dev@gmail.com>
2021-11-24 17:05:36 +0300

    Update base type tree

--- src/tree/type/type.tree.ts
+++ src/tree/type/type.tree.ts
@@ -1,11 +1,9 @@
 import { TypeContext } from '../../grammar/xon-parser';
 import { TypeMetadata } from '../../metadata/type/type-metadata';
 import { BaseTree } from '../base.tree';
-import { ExpressionTree } from '../expression/expression.tree';
 
 export abstract class TypeTree extends BaseTree {
-  ctx?: TypeContext;
-  typeMetadata: TypeMetadata;
+  abstract ctx?: TypeContext;
   abstract name: string;
-  abstract condition: ExpressionTree;
+  typeMetadata: TypeMetadata;
 }
