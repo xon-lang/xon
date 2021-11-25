ec4b5fc8c53ca0a2aceb9eebbfa9344c60496f16
nizami <nizami.dev@gmail.com>
2021-11-26 00:04:39 +0300

    Refactoring

--- src/tree/definition/definition-tree.ts
+++ src/tree/definition/definition-tree.ts
@@ -1,10 +1,8 @@
 import { DefinitionContext } from '../../grammar/xon-parser';
-import { TypeInfo } from '../../metadata/type/type-info';
 import { BaseTree } from '../base.tree';
 import { IdToken } from '../id-token';
 
 export abstract class DefinitionTree extends BaseTree {
   abstract ctx?: DefinitionContext;
   id: IdToken;
-  typeMetadata: TypeInfo;
 }
