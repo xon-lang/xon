30f199b6a4bc0eaf040604ed451821af751a19de
nizami <nizami.dev@gmail.com>
2021-11-26 00:02:52 +0300

    Refactoring

--- src/tree/literal/literal.tree.ts
+++ src/tree/literal/literal.tree.ts
@@ -1,9 +1,7 @@
 import { LiteralContext } from '../../grammar/xon-parser';
-import { TypeInfo } from '../../metadata/type/type-info';
 import { BaseTree } from '../base.tree';
 
 export abstract class LiteralTree extends BaseTree {
   abstract ctx?: LiteralContext;
   abstract value: string | number;
-  typeMetadata: TypeInfo;
 }
