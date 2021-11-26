00e0411d1a0f5b6ff30c4859acdaa57b1e7d6d05
nizami <nizami.dev@gmail.com>
2021-11-26 18:26:05 +0300

    Refactoring

--- src/metadata/handler/expression/literal/literal-expression-metadata.ts
+++ src/metadata/handler/expression/literal/literal-expression-metadata.ts
@@ -1,14 +1,14 @@
 import { LiteralExpressionTree } from '../../../../tree/expression/literal-expression/literal-expression.tree';
-import { TypeMetadata } from '../../../type/type-metadata';
+import { IdTypeMetadata } from '../../../type/id/id-type-metadata';
 import { DeclarationScope } from '../../declaration-scope';
 import { ExpressionMetadata } from '../expression-metadata';
 
 export class LiteralExpressionMetadata extends ExpressionMetadata {
-  type: TypeMetadata;
+  type: IdTypeMetadata;
 
   constructor(tree: LiteralExpressionTree, scope: DeclarationScope) {
     super();
     const literalName = tree.literal.constructor.name.replace('LiteralTree', '');
-    this.type = scope.get(literalName).type;
+    this.type = scope.get(literalName).type as IdTypeMetadata;
   }
 }
