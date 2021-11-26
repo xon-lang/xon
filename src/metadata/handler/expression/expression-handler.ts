7b2096f98d94575a410078117653cc85b281e1d2
nizami <nizami.dev@gmail.com>
2021-11-26 17:46:33 +0300

    Update id-expression handler logic

--- src/metadata/handler/expression/expression-handler.ts
+++ src/metadata/handler/expression/expression-handler.ts
@@ -2,6 +2,8 @@ import { ExpressionTree } from '../../../tree/expression/expression.tree';
 import { IdExpressionTree } from '../../../tree/expression/id-expression/id-expression.tree';
 import { LiteralExpressionTree } from '../../../tree/expression/literal-expression/literal-expression.tree';
 import { IdToken } from '../../../tree/id-token';
+import { DeclarationMetadata } from '../../declaration-metadata';
+import { IdTypeMetadata } from '../../type/id/id-type-metadata';
 import { DeclarationScope } from '../declaration-scope';
 import { ExpressionMetadata } from './expression-metadata';
 
@@ -14,8 +16,16 @@ export function expressionHandler(
     tree.metadata = new ExpressionMetadata(scope.get(literalName).type);
   } else if (tree instanceof IdExpressionTree) {
     const declaration = scope.get(tree.id.text);
-    tree.metadata = new ExpressionMetadata(declaration.type);
-    tree.id.metadata = declaration;
+    // todo fix it later
+    if (
+      tree.id.text[0] == tree.id.text[0].toUpperCase() &&
+      declaration.type instanceof IdTypeMetadata
+    ) {
+      tree.metadata = new ExpressionMetadata(declaration.type.constructorType);
+    } else {
+      tree.metadata = new ExpressionMetadata(declaration.type);
+    }
+    tree.id.metadata = new DeclarationMetadata(declaration.id, tree.metadata.type);
   }
 
   if (!tree.metadata) throw new Error(`Metadata not found for '${tree.constructor.name}'`);
