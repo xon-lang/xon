b299a0cc6aee1a7a7caae113031a28a2f044a463
nizami <nizami.dev@gmail.com>
2021-11-26 23:49:16 +0300

    Fix id-expression-metadata test

--- src/metadata/handler/expression/id/id-expression-metadata.test.ts
+++ src/metadata/handler/expression/id/id-expression-metadata.test.ts
@@ -1,6 +1,5 @@
 import { parseExpression } from '../../../../tree/parse';
 import { TestDeclarationScope } from '../../test-declaration-scope';
-import { FunctionTypeMetadata } from '../../type/function/function-type-metadata';
 import { IdTypeMetadata } from '../../type/id/id-type-metadata';
 import { getExpressionMetadata } from '../expression-metadata-helper';
 
@@ -9,11 +8,9 @@ test('null', () => {
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.type).toBeInstanceOf(FunctionTypeMetadata);
-  expect((metadata.type as FunctionTypeMetadata).returnType).toBeInstanceOf(IdTypeMetadata);
-  expect(((metadata.type as FunctionTypeMetadata).returnType as IdTypeMetadata).name).toBe(
-    'String',
-  );
+  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
+  const type = metadata.type as IdTypeMetadata;
+  expect(type.name).toBe('Null');
 });
 
 test('true', () => {
@@ -21,9 +18,7 @@ test('true', () => {
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.type).toBeInstanceOf(FunctionTypeMetadata);
-  expect((metadata.type as FunctionTypeMetadata).returnType).toBeInstanceOf(IdTypeMetadata);
-  expect(((metadata.type as FunctionTypeMetadata).returnType as IdTypeMetadata).name).toBe(
-    'String',
-  );
+  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
+  const type = metadata.type as IdTypeMetadata;
+  expect(type.name).toBe('Boolean');
 });
