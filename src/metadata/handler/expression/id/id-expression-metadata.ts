b9b8e48dc287570068c5b7ce7305c911c1db9f40
nizami <nizami.dev@gmail.com>
2021-11-27 01:27:44 +0300

    Revert id-expression-metadata

--- src/metadata/handler/expression/id/id-expression-metadata.ts
+++ src/metadata/handler/expression/id/id-expression-metadata.ts
@@ -3,6 +3,7 @@ import { DeclarationScope } from '../../declaration-scope';
 import { AttributeDeclarationMetadata } from '../../declaration/attribute/attribute-declaration-metadata';
 import { ClassDeclarationMetadata } from '../../declaration/class/class-declaration-metadata';
 import { TypeMetadata } from '../../type/type-metadata';
+import { getTypeMetadata } from '../../type/type-metadata-helper';
 import { ExpressionMetadata } from '../expression-metadata';
 
 export class IdExpressionMetadata extends ExpressionMetadata {
@@ -12,11 +13,12 @@ export class IdExpressionMetadata extends ExpressionMetadata {
     super();
 
     const declaration = scope.get(tree.id.text);
+    const typeArguments = tree.typeArguments.map((x) => getTypeMetadata(x, scope));
     tree.id.metadata = declaration;
-    if(declaration instanceof AttributeDeclarationMetadata)
-    this.type = declaration.type;
-    else if(declaration instanceof ClassDeclarationMetadata){
-      this.type = 
+
+    if (declaration instanceof AttributeDeclarationMetadata) this.type = declaration.type;
+    else if (declaration instanceof ClassDeclarationMetadata) {
+      this.type = declaration.init(typeArguments, scope);
     }
   }
 }
