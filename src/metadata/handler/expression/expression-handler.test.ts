a1cc8da942974b955efbc2653e8d156aae61ca7d
nizami <nizami.dev@gmail.com>
2021-11-26 17:32:56 +0300

    Add id-expression metadata tests

--- src/metadata/handler/expression/expression-handler.test.ts
+++ src/metadata/handler/expression/expression-handler.test.ts
@@ -1,13 +1,26 @@
 import { parseExpression } from '../../../tree/parse';
+import { FunctionTypeMetadata } from '../../type/function/function-type-metadata';
 import { IdTypeMetadata } from '../../type/id/id-type-metadata';
-import { expressionHandler } from './expression-handler';
 import { TestHandlerScope } from '../test-handler-scope';
+import { expressionHandler } from './expression-handler';
 
-test('array property', () => {
+test('literal', () => {
   const code = '123';
   const tree = parseExpression(code);
-
   const metadata = expressionHandler(tree, new TestHandlerScope());
+
   expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
   expect((metadata.type as IdTypeMetadata).name).toBe('Integer');
+});
+
+test('id', () => {
+  const code = 'String';
+  const tree = parseExpression(code);
+  const metadata = expressionHandler(tree, new TestHandlerScope());
+
+  expect(metadata.type).toBeInstanceOf(FunctionTypeMetadata);
+  expect((metadata.type as FunctionTypeMetadata).resultType).toBeInstanceOf(IdTypeMetadata);
+  expect(
+    ((metadata.type as FunctionTypeMetadata).resultType as IdTypeMetadata).name,
+  ).toBeInstanceOf('String');
 });
