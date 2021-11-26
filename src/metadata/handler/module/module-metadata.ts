2540c0f8ba767b64f858a7d6e652a4d13ce898e0
nizami <nizami.dev@gmail.com>
2021-11-26 23:53:39 +0300

    Update module-metadata

--- src/metadata/handler/module/module-metadata.ts
+++ src/metadata/handler/module/module-metadata.ts
@@ -2,6 +2,7 @@ import * as glob from 'glob';
 import * as path from 'path';
 import { ClassDefinitionTree } from '../../../tree/definition/class-definition/class-definition-tree';
 import { parseSourceFile } from '../../../tree/parse';
+import { DeclarationScope } from '../declaration-scope';
 import { AttributeDeclarationMetadata } from '../declaration/attribute/attribute-declaration-metadata';
 import { ClassDeclarationMetadata } from '../declaration/class/class-declaration-metadata';
 import { DeclarationMetadata } from '../declaration/declaration-metadata';
@@ -10,8 +11,10 @@ export class ModuleMetadata {
   declarations: DeclarationMetadata[] = [];
 
   constructor(moduleDir: string, defaultModules: ModuleMetadata[] = []) {
+    const scope = new DeclarationScope();
+
     for (const declaration of defaultModules.flatMap((x) => x.declarations)) {
-      this.declarations.push(declaration);
+      scope.set(declaration);
     }
 
     const globPath = path.resolve(moduleDir, '**/*.xon');
@@ -21,12 +24,13 @@ export class ModuleMetadata {
       x.definitions.map((x) => x as ClassDefinitionTree),
     )) {
       const declaration = new ClassDeclarationMetadata(definition);
-      this.declarations.push(declaration);
+      scope.set(declaration);
     }
 
     for (const attribute of sources.flatMap((x) => x.attributes)) {
-      const declaration = new AttributeDeclarationMetadata(attribute);
-      this.declarations.push(declaration);
+      const declaration = new AttributeDeclarationMetadata(attribute, scope);
+      scope.set(declaration);
     }
+    this.declarations = [...scope.declarations.values()];
   }
 }
