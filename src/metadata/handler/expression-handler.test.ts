942ff8d8170f58d6f2776abad18be36b1acf74e5
nizami <nizami.dev@gmail.com>
2021-11-26 16:35:12 +0300

    Fix expression-handler test

--- src/metadata/handler/expression-handler.test.ts
+++ src/metadata/handler/expression-handler.test.ts
@@ -1,4 +1,5 @@
 import { parseExpression } from '../../tree/parse';
+import { IdTypeMetadata } from '../type/id/id-type-metadata';
 import { expressionHandler } from './expression-handler';
 import { TestHandlerScope } from './test-handler-scope';
 
@@ -6,5 +7,7 @@ test('array property', () => {
   const code = '123';
   const tree = parseExpression(code);
 
-  expect(expressionHandler(tree, new TestHandlerScope())).toBe(null);
+  const metadata = expressionHandler(tree, new TestHandlerScope());
+  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
+  expect((metadata.type as IdTypeMetadata).name).toBe('Integer');
 });
