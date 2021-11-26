8e721ab180a91095f65131bce017ced477dd036c
nizami <nizami.dev@gmail.com>
2021-11-26 23:53:51 +0300

    Update attribute-declaration-metadata

--- src/metadata/handler/declaration/attribute/attribute-declaration-metadata.ts
+++ src/metadata/handler/declaration/attribute/attribute-declaration-metadata.ts
@@ -1,15 +1,25 @@
 import { AttributeTree } from '../../../../tree/attribute/attribute-tree';
 import { IdToken } from '../../../../tree/id-token';
+import { ExpressionStatementTree } from '../../../../tree/statement/expression-statement/expression-statement.tree';
+import { DeclarationScope } from '../../declaration-scope';
+import { getExpressionMetadata } from '../../expression/expression-metadata-helper';
 import { TypeMetadata } from '../../type/type-metadata';
+import { getTypeMetadata } from '../../type/type-metadata-helper';
 import { DeclarationMetadata } from '../declaration-metadata';
 
 export class AttributeDeclarationMetadata extends DeclarationMetadata {
   id: IdToken;
   type: TypeMetadata;
 
-  constructor(private tree: AttributeTree) {
+  constructor(tree: AttributeTree, scope: DeclarationScope) {
     super();
 
     this.id = tree.id;
+    if (tree.type) this.type = getTypeMetadata(tree.type, scope);
+    else
+      this.type = getExpressionMetadata(
+        (tree.body[0] as ExpressionStatementTree).expression,
+        scope,
+      ).type;
   }
 }
