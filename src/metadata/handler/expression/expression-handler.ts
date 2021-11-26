05a653ae8877404d36ff614ee63104d3b3617110
nizami <nizami.dev@gmail.com>
2021-11-26 18:14:22 +0300

    Add id-expression-metadata

--- src/metadata/handler/expression/expression-handler.ts
+++ src/metadata/handler/expression/expression-handler.ts
@@ -1,12 +1,10 @@
 import { ExpressionTree } from '../../../tree/expression/expression.tree';
 import { IdExpressionTree } from '../../../tree/expression/id-expression/id-expression.tree';
 import { LiteralExpressionTree } from '../../../tree/expression/literal-expression/literal-expression.tree';
-import { IdToken } from '../../../tree/id-token';
-import { DeclarationMetadata } from '../../declaration-metadata';
-import { IdTypeMetadata } from '../../type/id/id-type-metadata';
 import { DeclarationScope } from '../declaration-scope';
 import { ExpressionMetadata } from './expression-metadata';
-import { LiteralExpressionMetadata } from './literal/literal-expression-handler';
+import { IdExpressionMetadata } from './id/id-expression-metadata';
+import { LiteralExpressionMetadata } from './literal/literal-expression-metadata';
 
 export function getExpressionMetadata(
   tree: ExpressionTree,
@@ -14,16 +12,6 @@ export function getExpressionMetadata(
 ): ExpressionMetadata {
   if (tree instanceof LiteralExpressionTree)
     return (tree.metadata = new LiteralExpressionMetadata(tree, scope));
-  else if (tree instanceof IdExpressionTree) {
-  
-  }
-
-  if (!tree.metadata) {
-    throw new Error(`Metadata not found for '${tree.constructor.name}'`);
-  }
-  if ('id' in tree && tree['id'] instanceof IdToken && !tree['id'].metadata) {
-    throw new Error(`Metadata not set for id token in '${tree.constructor.name}'`);
-  }
-
-  return tree.metadata;
+  if (tree instanceof IdExpressionTree)
+    return (tree.metadata = new IdExpressionMetadata(tree, scope));
 }
---
+++ src/metadata/handler/expression/id/id-expression-metadata.ts
@@ -0,0 +1,23 @@
+import { IdExpressionTree } from '../../../../tree/expression/id-expression/id-expression.tree';
+import { DeclarationMetadata } from '../../../declaration-metadata';
+import { IdTypeMetadata } from '../../../type/id/id-type-metadata';
+import { TypeMetadata } from '../../../type/type-metadata';
+import { DeclarationScope } from '../../declaration-scope';
+import { ExpressionMetadata } from '../expression-metadata';
+
+export class IdExpressionMetadata extends ExpressionMetadata {
+  type: TypeMetadata;
+
+  constructor(tree: IdExpressionTree, scope: DeclarationScope) {
+    super();
+    const declaration = scope.get(tree.id.text);
+    if (
+      tree.id.text[0] == tree.id.text[0].toUpperCase() &&
+      declaration.type instanceof IdTypeMetadata
+    )
+      this.type = declaration.type.constructorType;
+    else this.type = declaration.type;
+
+    tree.id.metadata = new DeclarationMetadata(declaration.id, tree.metadata.type);
+  }
+}
