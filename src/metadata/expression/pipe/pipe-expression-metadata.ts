dbaaa33233b1b15aa89c1212feddc3217b70843d
nizami <nizami.dev@gmail.com>
2021-11-27 17:03:00 +0300

    Update pipe-exression-metadata

--- src/metadata/expression/pipe/pipe-expression-metadata.ts
+++ src/metadata/expression/pipe/pipe-expression-metadata.ts
@@ -1,11 +1,10 @@
-import { LambdaExpressionTree } from '../../../tree/expression/lambda-expression/lambda-expression.tree';
 import { PipeExpressionTree } from '../../../tree/expression/pipe-expression/pipe-expression.tree';
 import { DeclarationScope } from '../../declaration-scope';
-import { FunctionTypeMetadata } from '../../type/function/function-type-metadata';
+import { TypeMetadata } from '../../type/type-metadata';
 import { ExpressionMetadata } from '../expression-metadata';
 
 export class PipeExpressionMetadata extends ExpressionMetadata {
-  type: FunctionTypeMetadata;
+  type: TypeMetadata;
 
   constructor(tree: PipeExpressionTree, scope: DeclarationScope) {
     super();
