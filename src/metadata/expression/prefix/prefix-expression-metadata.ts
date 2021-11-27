6a1580f55ed4426761d57d5cc265659a7d1b7068
nizami <nizami.dev@gmail.com>
2021-11-28 01:07:42 +0300

    Fix tests

--- src/metadata/expression/prefix/prefix-expression-metadata.ts
+++ src/metadata/expression/prefix/prefix-expression-metadata.ts
@@ -1,5 +1,6 @@
 import { PrefixExpressionTree } from '../../../tree/expression/prefix-expression/prefix-expression.tree';
 import { DeclarationScope } from '../../declaration-scope';
+import { FunctionTypeMetadata } from '../../type/function/function-type-metadata';
 import { TypeMetadata } from '../../type/type-metadata';
 import { ExpressionMetadata } from '../expression-metadata';
 import { getExpressionMetadata } from '../expression-metadata-helper';
@@ -11,6 +12,8 @@ export class PrefixExpressionMetadata extends ExpressionMetadata {
     super();
 
     const declaration = getExpressionMetadata(tree.value, scope).type.declaration;
-    // this.type = declaration.get(tree.id.text);
+    const attribute = declaration.getMethodAttribute(tree.id.text, [], []);
+    if (attribute.type instanceof FunctionTypeMetadata) this.type = attribute.type.resultType;
+    else throw new Error('Wrong method type');
   }
 }
