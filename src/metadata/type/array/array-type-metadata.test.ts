0797c4fa71ceaaf073757f82cb9702c8fc2eb311
nizami <nizami.dev@gmail.com>
2021-11-28 01:35:49 +0300

    Fix tests

--- src/metadata/type/array/array-type-metadata.test.ts
+++ src/metadata/type/array/array-type-metadata.test.ts
@@ -1,12 +1,12 @@
 import { parseExpression } from '../../../tree/parse';
 import { getExpressionMetadata } from '../../expression/expression-metadata-helper';
 import { TestDeclarationScope } from '../../test-declaration-scope';
-import { IdTypeMetadata } from '../id/id-type-metadata';
+import { ArrayTypeMetadata } from './array-type-metadata';
 
 test('array', () => {
   const code = '[1, 2, 3]';
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
+  expect(metadata.type).toBeInstanceOf(ArrayTypeMetadata);
 });
