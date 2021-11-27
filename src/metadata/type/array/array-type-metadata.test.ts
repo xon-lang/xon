49a57ae49258225fe361c8b9932b65e5c915b948
nizami <nizami.dev@gmail.com>
2021-11-27 19:29:12 +0300

    Add array-type-metadata test

---
+++ src/metadata/type/array/array-type-metadata.test.ts
@@ -0,0 +1,12 @@
+import { parseExpression } from '../../../tree/parse';
+import { getExpressionMetadata } from '../../expression/expression-metadata-helper';
+import { TestDeclarationScope } from '../../test-declaration-scope';
+import { IdTypeMetadata } from '../id/id-type-metadata';
+
+test('array', () => {
+  const code = '[1, 2, 3]';
+  const tree = parseExpression(code);
+  const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
+
+  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
+});
