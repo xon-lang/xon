f9ec24cf2f4ce5173679e0035e6f6bd8f74c539d
nizami <nizami.dev@gmail.com>
2021-11-27 15:42:57 +0300

    Remove indexer type

--- src/tree/type/type-tree.helper.ts
+++ src/tree/type/type-tree.helper.ts
@@ -2,7 +2,6 @@ import {
   ArrayTypeContext,
   FunctionTypeContext,
   IdTypeContext,
-  IndexerTypeContext,
   NullableTypeContext,
   ParenthesizedTypeContext,
   TupleTypeContext,
@@ -12,7 +11,6 @@ import {
 import { ArrayTypeTree } from './array-type/array-type.tree';
 import { FunctionTypeTree } from './function-type/function-type.tree';
 import { IdTypeTree } from './id-type/id-type.tree';
-import { IndexerTypeTree } from './indexer-type/indexer-type.tree';
 import { NullableTypeTree } from './nullable-type/nullable-type.tree';
 import { TupleTypeTree } from './tuple-type/tuple-type.tree';
 import { TypeTree } from './type.tree';
@@ -21,12 +19,12 @@ import { UnionTypeTree } from './union-type/union-type.tree';
 export const getTypeTree = (ctx: TypeContext): TypeTree => {
   if (ctx === undefined) return undefined;
 
+  if (ctx instanceof ParenthesizedTypeContext) return getTypeTree(ctx.type());
+
   if (ctx instanceof ArrayTypeContext) return new ArrayTypeTree(ctx);
   if (ctx instanceof FunctionTypeContext) return new FunctionTypeTree(ctx);
   if (ctx instanceof IdTypeContext) return new IdTypeTree(ctx);
-  if (ctx instanceof IndexerTypeContext) return new IndexerTypeTree(ctx);
   if (ctx instanceof NullableTypeContext) return new NullableTypeTree(ctx);
-  if (ctx instanceof ParenthesizedTypeContext) return getTypeTree(ctx.type());
   if (ctx instanceof TupleTypeContext) return new TupleTypeTree(ctx);
   if (ctx instanceof UnionTypeContext) return new UnionTypeTree(ctx);
 
