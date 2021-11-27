1d60e3991bd6e012f4b8f659918a74229a232897
nizami <nizami.dev@gmail.com>
2021-11-27 17:41:35 +0300

    Update base type-metadata

--- src/metadata/type/type-metadata.ts
+++ src/metadata/type/type-metadata.ts
@@ -4,4 +4,8 @@ import { ClassDeclarationMetadata } from '../declaration/class/class-declaration
 export abstract class TypeMetadata {
   abstract declaration: ClassDeclarationMetadata;
   abstract scope: DeclarationScope;
+
+  abstract equals(other: TypeMetadata): boolean;
+
+  is(other: TypeMetadata) {}
 }
