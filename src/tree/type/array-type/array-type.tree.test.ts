e1da5a1976facdbfead4b06cf2d6b66ac0be3262
nizami <nizami.dev@gmail.com>
2021-11-27 15:43:31 +0300

    Fix array test

--- src/tree/type/array-type/array-type.tree.test.ts
+++ src/tree/type/array-type/array-type.tree.test.ts
@@ -2,10 +2,10 @@ import { parseType } from '../../parse';
 import { ArrayTypeTree } from './array-type.tree';
 
 test('string array', () => {
-  const code = 'String[32]';
+  const code = 'String[]';
   const tree = parseType<ArrayTypeTree>(code);
   expect(tree).toBeInstanceOf(ArrayTypeTree);
 
   expect(tree.itemType.name).toBe('String');
-  expect(tree.toString()).toBe('String[32]');
+  expect(tree.toString()).toBe('String[]');
 });
