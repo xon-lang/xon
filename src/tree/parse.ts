42a8ad17f581d8cb7b97d6e855a3bac83ba40b8d
nizami <nizami.dev@gmail.com>
2021-11-28 19:56:16 +0300

    Update parse.ts

--- src/tree/parse.ts
+++ src/tree/parse.ts
@@ -5,8 +5,7 @@ import { XonParser } from '../grammar/xon-parser';
 import { getAssignmentTree } from './assignment/assignment-tree.helper';
 import { AssignmentTree } from './assignment/assignment.tree';
 import { AttributeTree } from './attribute/attribute-tree';
-import { getClassMemberTree } from './definition/class-definition/class-member/class-member-tree.helper';
-import { ClassMemberTree } from './definition/class-definition/class-member/class-member.tree';
+import { getAttributeTree } from './attribute/attribute-tree.helper';
 import { DefinitionTree } from './definition/definition-tree';
 import { getDefinitionTree } from './definition/definition-tree-helper';
 import { ExportTree } from './export/export.tree';
@@ -19,7 +18,6 @@ import { LiteralTree } from './literal/literal.tree';
 import { SourceTree } from './source/source-tree';
 import { getStatementTree } from './statement/statement-tree.helper';
 import { StatementTree } from './statement/statement.tree';
-import { TestTree } from './test/test.tree';
 import { ThrowingErrorListener } from './throwing-error-listener';
 import { TypeParameterTree } from './type-parameter/type-parameter.tree';
 import { getTypeTree } from './type/type-tree.helper';
@@ -48,8 +46,8 @@ export const parseExpressionParameter = (code: string): ExpressionParameterTree 
 export const parseTypeParameter = (code: string): TypeParameterTree =>
   new TypeParameterTree(parse(code).typeParameter());
 
-export const parseAttribute = (code: string): AttributeTree =>
-  new AttributeTree(parse(code).attribute());
+export const parseAttribute = <T extends AttributeTree>(code: string): T =>
+  getAttributeTree(parse(code).attribute()) as T;
 
 export const parseLiteral = <T extends LiteralTree>(code: string): T =>
   getLiteralTree(parse(code).literal()) as T;
@@ -63,17 +61,12 @@ export const parseStatement = <T extends StatementTree>(code: string): T =>
 export const parseAssignment = <T extends AssignmentTree>(code: string): T =>
   getAssignmentTree(parse(code).assignment()) as T;
 
-export const parseClassMember = (code: string): ClassMemberTree =>
-  getClassMemberTree(parse(code).classMember());
-
 export const parseDefinition = <T extends DefinitionTree>(code: string): T =>
   getDefinitionTree(parse(code).definition()) as T;
 
 export const parseImport = (code: string) => new ImportTree(parse(code).library());
 
 export const parseExport = (code: string) => new ExportTree(parse(code).export());
-
-export const parseTest = (code: string) => new TestTree(parse(code).test());
 
 export const parseSource = (code: string, sourceName: string = undefined) =>
   new SourceTree(parse(code, sourceName).source());
