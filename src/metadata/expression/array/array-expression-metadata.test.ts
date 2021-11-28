34953a2d9f4e73e59789daca255cc756c5b2f8e5
nizami <nizami.dev@gmail.com>
2021-11-29 02:13:51 +0300

    Fix tests

--- src/metadata/expression/array/array-expression-metadata.test.ts
+++ src/metadata/expression/array/array-expression-metadata.test.ts
@@ -1,24 +1,25 @@
 import { parseExpression } from '../../../tree/parse';
 import { TestDeclarationScope } from '../../test-declaration-scope';
-import { IdTypeMetadata } from '../../type/id/id-type-metadata';
+import { TupleTypeMetadata } from '../../type/tuple/tuple-type-metadata';
 import { getExpressionMetadata } from '../expression-metadata-helper';
 
-test('null', () => {
-  const code = 'null';
+test('empty', () => {
+  const code = '[]';
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
-  const type = metadata.type as IdTypeMetadata;
-  expect(type.name).toBe('Null');
+  expect(metadata.type).toBeInstanceOf(TupleTypeMetadata);
+  const type = metadata.type as TupleTypeMetadata;
+  expect(type.itemsTypes.length).toBe(0);
 });
 
-test('true', () => {
-  const code = 'true';
+test('123', () => {
+  const code = '[1, 2, 3]';
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
-  const type = metadata.type as IdTypeMetadata;
-  expect(type.name).toBe('Boolean');
+  expect(metadata.type).toBeInstanceOf(TupleTypeMetadata);
+  const type = metadata.type as TupleTypeMetadata;
+  expect(type.itemsTypes.length).toBe(3);
+  expect(type.itemsTypes[0].declaration.name).toBe('Integer');
 });
--- src/metadata/expression/array/array-expression-metadata.ts
+++ src/metadata/expression/array/array-expression-metadata.ts
@@ -1,17 +1,16 @@
 import { ArrayExpressionTree } from '../../../tree/expression/array-expression/array-expression.tree';
 import { DeclarationScope } from '../../declaration-scope';
-import { ArrayTypeMetadata } from '../../type/array/array-type-metadata';
+import { TupleTypeMetadata } from '../../type/tuple/tuple-type-metadata';
 import { ExpressionMetadata } from '../expression-metadata';
 import { getExpressionMetadata } from '../expression-metadata-helper';
 
 export class ArrayExpressionMetadata extends ExpressionMetadata {
-  type: ArrayTypeMetadata;
+  type: TupleTypeMetadata;
 
   constructor(tree: ArrayExpressionTree, scope: DeclarationScope) {
     super();
 
-    // todo make union later
-    const firstItemType = getExpressionMetadata(tree.items[0], scope).type;
-    this.type = new ArrayTypeMetadata(firstItemType, scope);
+    const itemsTypes = tree.items.map((x) => getExpressionMetadata(x, scope).type);
+    this.type = new TupleTypeMetadata(itemsTypes, scope);
   }
 }
