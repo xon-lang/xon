98653af9ee3ad2263b19e78654424afedda29bde
nizami <nizami.dev@gmail.com>
2021-11-28 20:29:44 +0300

    Fix definition-attribute-tree test

--- src/tree/attribute/definition/definition-attribute-tree.test.ts
+++ src/tree/attribute/definition/definition-attribute-tree.test.ts
@@ -1,25 +1,20 @@
 import { parseAttribute } from '../../parse';
-import { IdTypeTree } from '../../type/id-type/id-type.tree';
-import { MethodAttributeTree } from './method-attribute-tree';
+import { DefinitionAttributeTree } from './definition-attribute-tree';
 
 test('value', () => {
   const code = 'a is Any\n  b = 123';
-  const tree = parseAttribute<MethodAttributeTree>(code);
-  expect(tree).toBeInstanceOf(MethodAttributeTree);
+  const tree = parseAttribute<DefinitionAttributeTree>(code);
+  expect(tree).toBeInstanceOf(DefinitionAttributeTree);
 
   expect(tree.id.text).toBe('s');
   expect(tree.isPrivate).toBe(false);
-  expect(tree.type.name).toBe('Array');
-  expect((tree.type as IdTypeTree).typeArguments[0].name).toBe('String');
 });
 
 test('value', () => {
   const code = 'a is Any\n  b = 123\n  c = "hi"';
-  const tree = parseAttribute<MethodAttributeTree>(code);
-  expect(tree).toBeInstanceOf(MethodAttributeTree);
+  const tree = parseAttribute<DefinitionAttributeTree>(code);
+  expect(tree).toBeInstanceOf(DefinitionAttributeTree);
 
   expect(tree.id.text).toBe('s');
   expect(tree.isPrivate).toBe(false);
-  expect(tree.type.name).toBe('Array');
-  expect((tree.type as IdTypeTree).typeArguments[0].name).toBe('String');
 });
