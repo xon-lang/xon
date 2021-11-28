42d38795d043e4815f295c28e980f10e7bc62cd4
nizami <nizami.dev@gmail.com>
2021-11-28 22:08:03 +0300

    Fix test

--- src/metadata/type/literal/literal-type-metadata.ts
+++ src/metadata/type/literal/literal-type-metadata.ts
@@ -7,7 +7,11 @@ import { UnionTypeMetadata } from '../union/union-type-metadata';
 export class LiteralTypeMetadata extends TypeMetadata {
   declaration: ClassDeclarationMetadata;
 
-  constructor(public name: string, public value: number | string, public scope: DeclarationScope) {
+  constructor(
+    public name: string,
+    public value: number | string | RegExp,
+    public scope: DeclarationScope,
+  ) {
     super();
     this.declaration = scope.get(name) as ClassDeclarationMetadata;
   }
