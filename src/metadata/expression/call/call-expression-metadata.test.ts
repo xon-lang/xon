fcbcc9254e442075ecfc64ad30f8bc94d137d6fb
nizami <nizami.dev@gmail.com>
2021-11-29 02:15:33 +0300

    Fix tests

--- src/metadata/expression/call/call-expression-metadata.test.ts
+++ src/metadata/expression/call/call-expression-metadata.test.ts
@@ -3,22 +3,11 @@ import { TestDeclarationScope } from '../../test-declaration-scope';
 import { IdTypeMetadata } from '../../type/id/id-type-metadata';
 import { getExpressionMetadata } from '../expression-metadata-helper';
 
-test('null', () => {
-  const code = 'null';
+test('lambda call', () => {
+  const code = '((): 1 + 1)()';
   const tree = parseExpression(code);
   const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
 
   expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
-  const type = metadata.type as IdTypeMetadata;
-  expect(type.name).toBe('Null');
-});
-
-test('true', () => {
-  const code = 'true';
-  const tree = parseExpression(code);
-  const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
-
-  expect(metadata.type).toBeInstanceOf(IdTypeMetadata);
-  const type = metadata.type as IdTypeMetadata;
-  expect(type.name).toBe('Boolean');
+  expect(metadata.type.declaration.name).toBeInstanceOf('Integer');
 });
