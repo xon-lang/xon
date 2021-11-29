fa22c57a02dc58751a552309eba0c236230c62ec
nizami <nizami.dev@gmail.com>
2021-11-29 04:54:58 +0300

    Add literal-type to helper

--- src/tree/type/type-tree.helper.ts
+++ src/tree/type/type-tree.helper.ts
@@ -2,6 +2,7 @@ import {
   ArrayTypeContext,
   FunctionTypeContext,
   IdTypeContext,
+  LiteralTypeContext,
   NullableTypeContext,
   ParenthesizedTypeContext,
   TupleTypeContext,
@@ -11,6 +12,7 @@ import {
 import { ArrayTypeTree } from './array-type/array-type.tree';
 import { FunctionTypeTree } from './function-type/function-type.tree';
 import { IdTypeTree } from './id-type/id-type.tree';
+import { LiteralTypeTree } from './literal-type/literal-type.tree';
 import { NullableTypeTree } from './nullable-type/nullable-type.tree';
 import { TupleTypeTree } from './tuple-type/tuple-type.tree';
 import { TypeTree } from './type.tree';
@@ -24,6 +26,7 @@ export const getTypeTree = (ctx: TypeContext): TypeTree => {
   if (ctx instanceof ArrayTypeContext) return new ArrayTypeTree(ctx);
   if (ctx instanceof FunctionTypeContext) return new FunctionTypeTree(ctx);
   if (ctx instanceof IdTypeContext) return new IdTypeTree(ctx);
+  if (ctx instanceof LiteralTypeContext) return new LiteralTypeTree(ctx);
   if (ctx instanceof NullableTypeContext) return new NullableTypeTree(ctx);
   if (ctx instanceof TupleTypeContext) return new TupleTypeTree(ctx);
   if (ctx instanceof UnionTypeContext) return new UnionTypeTree(ctx);
