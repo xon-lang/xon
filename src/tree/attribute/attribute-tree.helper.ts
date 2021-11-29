2da7d69c01e956beacd5f859e1cbc9ba89817278
nizami <nizami.dev@gmail.com>
2021-11-29 03:22:45 +0300

    Remove definition-attribute-tree

--- src/tree/attribute/attribute-tree.helper.ts
+++ src/tree/attribute/attribute-tree.helper.ts
@@ -1,13 +1,11 @@
 import {
   AbstractAttributeContext,
   AttributeContext,
-  DefinitionAttributeContext,
   MethodAttributeContext,
   ValueAttributeContext,
 } from '../../grammar/xon-parser';
 import { AbstractAttributeTree } from './abstract/abstract-attribute-tree';
 import { AttributeTree } from './attribute-tree';
-import { DefinitionAttributeTree } from './definition/definition-attribute-tree';
 import { MethodAttributeTree } from './method/method-attribute-tree';
 import { ValueAttributeTree } from './value/value-attribute-tree';
 
@@ -17,7 +15,6 @@ export const getAttributeTree = (ctx: AttributeContext): AttributeTree => {
   if (ctx instanceof AbstractAttributeContext) return new AbstractAttributeTree(ctx);
   if (ctx instanceof ValueAttributeContext) return new ValueAttributeTree(ctx);
   if (ctx instanceof MethodAttributeContext) return new MethodAttributeTree(ctx);
-  if (ctx instanceof DefinitionAttributeContext) return new DefinitionAttributeTree(ctx);
 };
 
 export const getAttributesTrees = (ctx: AttributeContext[]): AttributeTree[] => {
