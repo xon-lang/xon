5ed7445efb3bfe6f138be40a6e6cd2d025569934
nizami <nizami.dev@gmail.com>
2021-11-29 04:32:34 +0300

    Fix tests

--- src/tree/definition/attribute-definition/attribute-definition-tree.ts
+++ src/tree/definition/attribute-definition/attribute-definition-tree.ts
@@ -1,19 +1,16 @@
-import { AttributeDefinitionContext, ClassDefinitionContext } from '../../../grammar/xon-parser';
+import { AttributeDefinitionContext } from '../../../grammar/xon-parser';
 import { AttributeTree } from '../../attribute/attribute-tree';
 import { getAttributesTrees } from '../../attribute/attribute-tree.helper';
-import { getExpressionParametersTrees } from '../../expression-parameter/expression-parameter-tree.helper';
-import { ExpressionParameterTree } from '../../expression-parameter/expression-parameter.tree';
 import { IdToken } from '../../id-token';
 import { getTypeParametersTrees } from '../../type-parameter/type-parameter-tree.helper';
 import { TypeParameterTree } from '../../type-parameter/type-parameter.tree';
-import { getTypeTree } from '../../type/type-tree.helper';
-import { TypeTree } from '../../type/type.tree';
+import { DefinitionBaseTypeTree } from '../definition-base-type-tree';
 import { DefinitionTree } from '../definition-tree';
 
 export class AttributeDefinitionTree extends DefinitionTree {
   id: IdToken;
   typeParameters: TypeParameterTree[] = [];
-  baseType?: TypeTree;
+  baseType?: DefinitionBaseTypeTree;
   attributes: AttributeTree[] = [];
 
   constructor(public ctx?: AttributeDefinitionContext) {
@@ -22,7 +19,7 @@ export class AttributeDefinitionTree extends DefinitionTree {
 
     this.id = new IdToken(ctx._name);
     this.typeParameters = getTypeParametersTrees(ctx.typeParameters());
-    this.baseType = getTypeTree(ctx.type());
+    this.baseType = new DefinitionBaseTypeTree(ctx.definitionBaseType());
     this.attributes = getAttributesTrees(ctx.attribute());
   }
 }
