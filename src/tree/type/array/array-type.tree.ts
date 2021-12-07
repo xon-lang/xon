940293daee1e7054a2b6af97eab146cccb349154
nizami <nizami.dev@gmail.com>
2021-12-07 23:05:14 +0300

    Fix array-type tree

--- src/tree/type/array/array-type.tree.ts
+++ src/tree/type/array/array-type.tree.ts
@@ -1,12 +1,12 @@
 import { ArrayTypeContext } from '../../../grammar/xon-parser';
-import { ArrayTypeMetadata } from '../../../metadata/type/array/array-type-metadata';
+import { TypeMetadata } from '../../../metadata/type/type-metadata';
 import { LambdaTypeTree } from '../lambda/lambda-type.tree';
 import { getTypeTree } from '../type-tree.helper';
 import { TypeTree } from '../type.tree';
 import { UnionTypeTree } from '../union/union-type.tree';
 
 export class ArrayTypeTree extends TypeTree {
-  metadata: ArrayTypeMetadata;
+  metadata: TypeMetadata;
   itemType: TypeTree;
 
   constructor(public ctx: ArrayTypeContext) {
