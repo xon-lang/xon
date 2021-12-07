87b0807ec5484d763b5d88c981a6ffd8326a56a5
nizami <nizami.dev@gmail.com>
2021-12-07 21:51:40 +0300

    Update pipe expression tree

--- src/tree/expression/pipe/pipe-expression.tree.ts
+++ src/tree/expression/pipe/pipe-expression.tree.ts
@@ -1,25 +1,24 @@
 import { PipeExpressionContext } from '../../../grammar/xon-parser';
 import { PipeExpressionMetadata } from '../../../metadata/expression/pipe/pipe-expression-metadata';
-import { getParameterTree } from '../../parameter/parameter-tree.helper';
-import { ParameterTree } from '../../parameter/parameter.tree';
+import { IdToken } from '../../id-token';
 import { getExpressionTree } from '../expression-tree.helper';
 import { ExpressionTree } from '../expression.tree';
 
 export class PipeExpressionTree extends ExpressionTree {
   metadata: PipeExpressionMetadata;
-  parameter?: ParameterTree;
+  id?: IdToken;
   left: ExpressionTree;
   right: ExpressionTree;
 
   constructor(public ctx: PipeExpressionContext) {
     super();
-    this.parameter = getParameterTree(ctx.parameter()) || null;
+    this.id = (ctx.id() && IdToken.fromContext(ctx.id())) || null;
     this.left = getExpressionTree(ctx.expression(0));
     this.right = getExpressionTree(ctx.expression(1));
   }
 
   toString(): string {
-    const arg = this.parameter ? `${this.parameter}:` : '';
-    return `${this.left} |${arg} ${this.right}`;
+    const id = this.id ? `${this.id}:` : '';
+    return `${this.left} |${id} ${this.right}`;
   }
 }
