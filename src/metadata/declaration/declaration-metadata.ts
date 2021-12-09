c27305822cb6f26f9c829903d44a321925e7e9a5
nizami <nizami.dev@gmail.com>
2021-12-09 16:04:21 +0300

    Update declaration metadata

--- src/metadata/declaration/declaration-metadata.ts
+++ src/metadata/declaration/declaration-metadata.ts
@@ -1,6 +1,3 @@
-import { TypeMetadata } from '../type/type-metadata';
-
 export abstract class DeclarationMetadata {
   abstract name: string;
-  abstract type(typeArguments: TypeMetadata[]): TypeMetadata;
 }
