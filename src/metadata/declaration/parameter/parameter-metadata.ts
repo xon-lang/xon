05daef527b657ac27227fe639f237be6f41c1607
nizami <nizami.dev@gmail.com>
2021-12-09 19:00:57 +0300

    Update parameter metadata

--- src/metadata/declaration/parameter/parameter-metadata.ts
+++ src/metadata/declaration/parameter/parameter-metadata.ts
@@ -1,18 +1,10 @@
-import { ParameterTree } from '../../../tree/parameter/parameter.tree';
 import { DeclarationScope } from '../../declaration-scope';
 import { TypeMetadata } from '../../type/type-metadata';
-import { getTypeMetadata } from '../../type/type-metadata-helper';
 import { DeclarationMetadata } from '../declaration-metadata';
 
 export class ParameterMetadata extends DeclarationMetadata {
-  name: string;
-  type: TypeMetadata;
-
-  constructor(private tree: ParameterTree, private scope: DeclarationScope) {
+  constructor(public name: string, private type: TypeMetadata, private scope: DeclarationScope) {
     super();
-
-    this.name = tree.id.text;
-    this.type = getTypeMetadata(this.tree.type, this.scope);
   }
 
   toString() {
