9abd28cfcea198f2d4423790a8404f1e7e5923d4
nizami <nizami.dev@gmail.com>
2021-12-23 00:04:56 +0300

    Update parameter node

--- src/ast/parameter/parameter-node.ts
+++ src/ast/parameter/parameter-node.ts
@@ -1,20 +1,30 @@
 import { ParameterContext } from '../../grammar/xon-parser';
+import { BodyNode } from '../body/body-node';
+import { getBodyNode } from '../body/body-node-helper';
 import { ExpressionNode } from '../expression/expression-node';
-import { getExpressionNode } from '../expression/expression-node-helper';
+import { getExpressionNode, getExpressionNodes } from '../expression/expression-node-helper';
 import { IdToken } from '../id-token';
 import { Node } from '../node';
 
 export class ParameterNode extends Node {
+  modifier: IdToken;
+  hasSpread: boolean;
   id: IdToken;
+  generics?: ExpressionNode[];
   type?: ExpressionNode;
+  body?: BodyNode;
   meta?: IdToken;
 
   constructor(public ctx: ParameterContext) {
     super();
 
-    this.id = IdToken.fromContext(ctx._name);
+    this.modifier = (ctx.modifier() && IdToken.fromContext(ctx.modifier())) || null;
+    this.hasSpread = !!ctx.SPREAD();
+    this.id = IdToken.fromContext(ctx.parameterId());
+    this.generics = getExpressionNodes(ctx.generics()) || null;
     this.type = getExpressionNode(ctx.expr()) || null;
-    this.meta = (ctx._meta && IdToken.fromContext(ctx._meta)) || null;
+    this.body = getBodyNode(ctx.body()) || null;
+    this.meta = (ctx._meta && new IdToken(ctx._meta)) || null;
   }
 
   toString(): string {
