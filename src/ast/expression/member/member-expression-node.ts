d122f483d796d0e1be0da0543d36434ba74de044
nizami <nizami.dev@gmail.com>
2021-12-22 16:20:52 +0300

    Update member expression node

--- src/ast/expression/member/member-expression-node.ts
+++ src/ast/expression/member/member-expression-node.ts
@@ -2,21 +2,26 @@ import { MemberExpressionContext } from '../../../grammar/xon-parser';
 import { MemberExpressionMetadata } from '../../../metadata/expression/member/member-expression-metadata';
 import { IdToken } from '../../id-token';
 import { ExpressionNode } from '../expression-node';
-import { getExpressionNode } from '../expression-node-helper';
+import { getExpressionNode, getExpressionNodes } from '../expression-node-helper';
 
 export class MemberExpressionNode extends ExpressionNode {
   metadata: MemberExpressionMetadata;
   instance: ExpressionNode;
   id: IdToken;
+  generics: ExpressionNode[];
 
   constructor(public ctx: MemberExpressionContext) {
     super();
 
     this.instance = getExpressionNode(ctx.expr());
-    this.id = IdToken.fromContext(ctx.attrId());
+    this.id = IdToken.fromContext(ctx.id());
+    this.generics = getExpressionNodes(ctx.generics());
   }
 
   toString(): string {
+    if (this.generics.length) {
+      return `${this.instance}.${this.id}<${this.generics.join(', ')}>`;
+    }
     return `${this.instance}.${this.id}`;
   }
 }
