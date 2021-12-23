e3a163795c63553f074dcc67bf47d34e0e5f22c9
nizami <nizami.dev@gmail.com>
2021-12-23 18:15:05 +0300

    Update method expression node

--- src/ast/expression/method/method-expression-node.ts
+++ src/ast/expression/method/method-expression-node.ts
@@ -1,7 +1,5 @@
 import { MethodExpressionContext } from '../../../grammar/xon-parser';
 import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata';
-import { BodyNode } from '../../body/body-node';
-import { getBodyNode } from '../../body/body-node-helper';
 import { ParameterNode } from '../../parameter/parameter-node';
 import { getParameterNodes } from '../../parameter/parameter-node-helper';
 import { ExpressionNode } from '../expression-node';
@@ -11,19 +9,16 @@ export class MethodExpressionNode extends ExpressionNode {
   metadata: ExpressionMetadata;
   parameters: ParameterNode[] = [];
   resultType?: ExpressionNode;
-  body?: BodyNode;
 
   constructor(public ctx: MethodExpressionContext) {
     super();
 
     this.parameters = getParameterNodes(ctx.parameter());
     this.resultType = getExpressionNode(ctx.expr()) || null;
-    this.body = getBodyNode(ctx.body()) || null;
   }
 
   toString(): string {
     const resultType = this.resultType ? ' ' + this.resultType : '';
-    const body = this.body ? this.body : '';
-    return `(${this.parameters.join(', ')})${resultType}${body}`;
+    return `(${this.parameters.join(', ')})${resultType}`;
   }
 }
