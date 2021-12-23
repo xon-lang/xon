de368e83f4f9172b3d999805bcdd334c2a61b39b
nizami <nizami.dev@gmail.com>
2021-12-23 17:18:50 +0300

    Fix tests

--- src/ast/expression/expression-node.test.ts
+++ src/ast/expression/expression-node.test.ts
@@ -25,13 +25,13 @@ test('3', () => {
 test('4', () => {
   const code = '2>=1<=3 || 4!=5 && 6';
   const node = parseExpression(code);
-  expect(node.toString()).toBe('2 >= 1 && 1 <= 3 || 4 != 5 && 6');
+  expect(node.toString()).toBe('2 >= 1 <= 3 || 4 != 5 && 6');
   // expect(evalExpression(node)).toBe((1 * (2 + (3 - 4))) / 5);
 });
 
 test('5', () => {
   const code = '1<2<3>0<=1>=5';
   const node = parseExpression(code);
-  expect(node.toString()).toBe('1 < 2 && 2 < 3 && 3 > 0 && 0 <= 1 && 1 >= 5');
+  expect(node.toString()).toBe('1 < 2 < 3 > 0 <= 1 >= 5');
   // expect(evalExpression(node)).toBe((1 * (2 + (3 - 4))) / 5);
 });
