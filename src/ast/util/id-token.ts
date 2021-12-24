a2bc418e3180a93c49cf163e5fc5a3cf1aa0b573
nizami <nizami.dev@gmail.com>
2021-12-24 14:50:03 +0300

    Update id-token.ts

--- src/ast/util/id-token.ts
+++ src/ast/util/id-token.ts
@@ -1,33 +1,19 @@
-import { ParserRuleContext, Token } from 'antlr4ts';
+import { Token } from 'antlr4ts';
 import { DeclarationMetadata } from '../../metadata/declaration/declaration-metadata';
 import { SourceReference } from './source-reference';
 
 export class IdToken {
-  text: string;
   sourceReference: SourceReference;
+  text: string;
   metadata: DeclarationMetadata;
 
   constructor(token?: Token) {
     if (!token) return;
-
-    this.text = token.text;
     this.sourceReference = SourceReference.fromToken(token);
+    this.text = token.text;
   }
 
   toString(): string {
     return this.text;
-  }
-
-  static fromContext(context: ParserRuleContext): IdToken {
-    const id = new IdToken();
-    id.text = context.text;
-    id.sourceReference = SourceReference.fromContext(context);
-    return id;
-  }
-
-  static fromText(text: string): IdToken {
-    const id = new IdToken();
-    id.text = text;
-    return id;
   }
 }
