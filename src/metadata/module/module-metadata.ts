619cbc710e70f9c9f208de76a5f4d418963ff6cb
nizami <nizami.dev@gmail.com>
2021-12-24 21:33:36 +0300

    Update module metadata

--- src/metadata/module/module-metadata.ts
+++ src/metadata/module/module-metadata.ts
@@ -1,28 +1,28 @@
 import * as glob from 'glob';
 import * as path from 'path';
+import { DeclarationStatementNode } from '../../ast/statement/declaration/declaration-statement-node';
 import { parseSourceFile } from '../../ast/util/parse';
 import { DeclarationScope } from '../declaration-scope';
-import { DefinitionMetadata } from '../declaration/definition/definition-metadata';
-import { getDefinitionMetadata } from '../declaration/definition/definition-metadata-helper';
+import { DeclarationMetadata } from '../declaration/declaration-metadata';
+import { getDeclarationMetadata } from '../declaration/declaration-metadata-helper';
 
 export class ModuleMetadata {
-  definitions: DefinitionMetadata[] = [];
+  declarations: DeclarationMetadata[] = [];
 
   constructor(moduleDir: string, scope: DeclarationScope) {
     const innerScope = new DeclarationScope(scope);
-
     const globPath = path.resolve(moduleDir, '**/*.xon');
     const sources = glob.sync(globPath).map((x) => parseSourceFile(x));
+
     for (const source of sources) {
-      // for (const library of source.imports) {
-      //   // imports
-      // }
-      for (const definition of source.definitions) {
-        const definitionMetadata = getDefinitionMetadata(definition, innerScope);
-        this.definitions.push(definitionMetadata);
+      for (const statement of source.statements) {
+        if (statement instanceof DeclarationStatementNode) {
+          const declaration = getDeclarationMetadata(statement.declaration, innerScope);
+          this.declarations.push(declaration);
+        }
       }
     }
 
-    innerScope.declarations = this.definitions;
+    innerScope.declarations = this.declarations;
   }
 }
