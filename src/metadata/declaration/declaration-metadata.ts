f01b26d8e8a0590aa1ddfa28d72f37fd8ff68ea5
nizami <nizami.dev@gmail.com>
2021-12-25 17:07:52 +0300

    Update declarations metadata

--- src/metadata/declaration/declaration-metadata.ts
+++ src/metadata/declaration/declaration-metadata.ts
@@ -1,6 +1,9 @@
 import { SourceReference } from '../../ast/util/source-reference';
+import { ExpressionMetadata } from '../expression/expression-metadata';
 
 export interface DeclarationMetadata {
   sourceReference: SourceReference;
   name: string;
+  generics(): ExpressionMetadata[];
+  attributes(): DeclarationMetadata[];
 }
