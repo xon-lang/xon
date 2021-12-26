1e2649541aa7fa3195a10329d097199a26afb62e
nizami <nizami.dev@gmail.com>
2021-12-26 08:57:08 +0300

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ mkdir dist
 find src -name "*.ts" -a ! -name "*.test.ts" | cpio -p -dumv dist
 cp package.json dist
 cp README.md dist
-# cp -- *.md dist
+cp LICENSE dist
 
 npx cti create -e tests, test-helper, .antlr -i .test.ts -w -f ./dist
 npx tsc -p tsconfig.dist.json
