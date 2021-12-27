b13b82d7ed745324d70320aac6480088c25f9b08
nizami <nizami.dev@gmail.com>
2021-12-27 13:09:46 +0300

    Update node.ts

--- src/node/node.ts
+++ src/node/node.ts
@@ -1,13 +1,14 @@
-// import { ParserRuleContext } from 'antlr4ts';
-// import { ExpressionMetadata } from '../metadata/expression/expression-metadata';
-// import { SourceReference } from '../tree/util/source-reference';
+import { ParserRuleContext } from 'antlr4ts';
+import { ExpressionMetadata } from '../metadata/expression/expression-metadata';
+import { Tree } from '../tree/tree';
+import { SourceReference } from '../tree/util/source-reference';
 
-// export interface ExpressionNode {
-//   context: ParserRuleContext;
-//   sourceReference: SourceReference;
-//   // tree: Tree
-//   metadata: ExpressionMetadata;
+export interface ExpressionNode {
+  context: ParserRuleContext;
+  sourceReference: SourceReference;
+  tree: Tree;
+  metadata: ExpressionMetadata;
 
-//   format(): string;
-//   evaluate(): unknown;
-// }
+  format(): string;
+  evaluate(): unknown;
+}
