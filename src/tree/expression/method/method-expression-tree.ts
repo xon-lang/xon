a060bcea5dca2e30fee6b5a815bc44ae219298df
nizami <nizami.dev@gmail.com>
2022-01-08 19:33:04 +0300

    Update method expression tree

--- src/tree/expression/method/method-expression-tree.ts
+++ src/tree/expression/method/method-expression-tree.ts
@@ -1,18 +1,18 @@
 import { MethodExpressionContext } from '../../../grammar/xon-parser';
 import { SourceReference } from '../../../util/source-reference';
-import { DeclarationTree } from '../../declaration/declaration-tree';
-import { getDeclarationTrees } from '../../declaration/declaration-tree-helper';
+import { ParameterTree } from '../../parameter/parameter-tree';
+import { getParameterTrees } from '../../parameter/parameter-tree-helper';
 import { ExpressionTree } from '../expression-tree';
 import { getExpressionTree } from '../expression-tree-helper';
 
 export class MethodExpressionTree implements ExpressionTree {
   sourceReference: SourceReference;
-  parameters: DeclarationTree[] = [];
+  parameters: ParameterTree[] = [];
   result?: ExpressionTree;
 
   constructor(ctx: MethodExpressionContext) {
     this.sourceReference = SourceReference.fromContext(ctx);
-    this.parameters = getDeclarationTrees(ctx.declaration());
+    this.parameters = getParameterTrees(ctx.parameter());
     this.result = getExpressionTree(ctx.expr()) || null;
   }
 
