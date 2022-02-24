27b853459e73b51642e18bba2b946f7e0fd21237
nizami <nizami.dev@gmail.com>
2022-02-24 22:52:04 +0300

    Update method expression

--- src/tree/expression/method/method-expression-tree.test.ts
+++ src/tree/expression/method/method-expression-tree.test.ts
@@ -1,22 +1,30 @@
 import { evaluate } from '../../../util/evaluate';
 import { parseExpression } from '../../../util/parse';
+import { SingleBodyTree } from '../../body/single/single-body-tree';
+import { ExpressionStatementTree } from '../../statement/expression/expression-statement-tree';
 import { MethodExpressionTree } from './method-expression-tree';
 
 test('has argument', () => {
-  const code = '(x) x + 42';
+  const code = '(x): x + 42';
   const tree = parseExpression(code) as MethodExpressionTree;
   expect(tree).toBeInstanceOf(MethodExpressionTree);
 
   expect(tree.parameters.length).toBe(1);
   expect(tree.parameters[0].id.name.text).toBe('x');
-  expect(evaluate(tree.result, { x: 37 })).toBe(37 + 42);
+  expect(
+    evaluate(((tree.body as SingleBodyTree).statement as ExpressionStatementTree).expression, {
+      x: 37,
+    }),
+  ).toBe(37 + 42);
 });
 
 test('no arguments', () => {
-  const code = '() 42+45';
+  const code = '(): 42+45';
   const tree = parseExpression(code) as MethodExpressionTree;
   expect(tree).toBeInstanceOf(MethodExpressionTree);
 
   expect(tree.parameters.length).toBe(0);
-  expect(evaluate(tree.result)).toBe(42 + 45);
+  expect(
+    evaluate(((tree.body as SingleBodyTree).statement as ExpressionStatementTree).expression),
+  ).toBe(42 + 45);
 });
--- src/tree/expression/method/method-expression-tree.ts
+++ src/tree/expression/method/method-expression-tree.ts
@@ -1,22 +1,23 @@
 import { MethodExpressionContext } from '../../../grammar/xon-parser';
 import { SourceReference } from '../../../util/source-reference';
+import { BodyTree } from '../../body/body-tree';
+import { getBodyTree } from '../../body/body-tree-helper';
 import { ParameterTree } from '../../parameter/parameter-tree';
 import { getParameterTrees } from '../../parameter/parameter-tree-helper';
 import { ExpressionTree } from '../expression-tree';
-import { getExpressionTree } from '../expression-tree-helper';
 
 export class MethodExpressionTree implements ExpressionTree {
   sourceReference: SourceReference;
   parameters: ParameterTree[] = [];
-  result: ExpressionTree;
+  body: BodyTree;
 
   constructor(ctx: MethodExpressionContext) {
     this.sourceReference = SourceReference.fromContext(ctx);
     this.parameters = getParameterTrees(ctx.parameter());
-    this.result = getExpressionTree(ctx.expr());
+    this.body = getBodyTree(ctx.body());
   }
 
   toString(): string {
-    return `(${this.parameters.join(', ')}) ${this.result}`;
+    return `(${this.parameters.join(', ')})${this.body}`;
   }
 }
