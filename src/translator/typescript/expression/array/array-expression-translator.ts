408b13f249b13798e1cc23e15c44b1cade872b75
nizami <nizami.dev@gmail.com>
2022-02-26 00:22:47 +0300

    Fix array expression translator

--- src/translator/typescript/expression/array/array-expression-translator.ts
+++ src/translator/typescript/expression/array/array-expression-translator.ts
@@ -1,10 +1,12 @@
 import { ArrayExpressionTree } from '../../../../tree/expression/array/array-expression-tree';
 import { ExpressionTranslator } from '../expression-translator';
+import { getExpressionTranslators } from '../expression-translator-helper';
 
 export class ArrayExpressionTranslator implements ExpressionTranslator {
   constructor(private tree: ArrayExpressionTree, private isType: boolean) {}
 
   toString(): string {
-    return this.tree.toString();
+    const items = getExpressionTranslators(this.tree.items, this.isType);
+    return `[${items.join(', ')}]`;
   }
 }
