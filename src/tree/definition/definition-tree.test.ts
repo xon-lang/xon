d5dc9304739c46a7532485bbbc1308f85500e65b
nizami <nizami.dev@gmail.com>
2022-02-25 13:20:32 +0300

    Update definition tree test

--- src/tree/definition/definition-tree.test.ts
+++ src/tree/definition/definition-tree.test.ts
@@ -12,3 +12,16 @@ test('model cat', () => {
   expect((tree.base as IdExpressionTree).id.name.text).toBe('Animal');
   expect(tree.attributes.length).toBe(0);
 });
+
+test('model animal with only attribute', () => {
+  const code = 'model Animal\n   abc Integer';
+  const tree = parseDefinition(code);
+  expect(tree).toBeInstanceOf(DefinitionTree);
+
+  expect(tree.modifier.text).toBe('model');
+  expect(tree.id.name.text).toBe('Animal');
+  expect(tree.base).toBe(null);
+  expect(tree.attributes.length).toBe(1);
+  expect(tree.attributes[0].id.name.text).toBe('abc');
+  expect((tree.attributes[0].type as IdExpressionTree).id.name.text).toBe('Integer');
+});
