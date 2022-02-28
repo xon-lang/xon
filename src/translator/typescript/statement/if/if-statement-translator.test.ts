4e064c49b22de32af93eb21b71efd63f0da1b246
nizami <nizami.dev@gmail.com>
2022-02-28 21:16:37 +0300

    Update if statement translator

--- src/translator/typescript/statement/if/if-statement-translator.test.ts
+++ src/translator/typescript/statement/if/if-statement-translator.test.ts
@@ -3,10 +3,18 @@ import { getStatementTranslator } from '../statement-translator-helper';
 import { IfStatementTranslator } from './if-statement-translator';
 
 test('single body', () => {
-  const code = 'if 12+(45/9) :    14+(144/12) else: 2   *   4   ';
+  const code = 'if 12+(45/9)\n  14+(144/12)\nelse\n  2   *   4   ';
   const tree = parseStatement(code);
   const translator = getStatementTranslator(tree);
 
   expect(translator).toBeInstanceOf(IfStatementTranslator);
-  expect(translator.toString()).toBe('if 12 + (45 / 9): 14 + (144 / 12)\nelse: 2 * 4');
+  expect(translator.toString()).toBe(
+    `
+if (12 + (45 / 9)) {
+  14 + (144 / 12)
+} else {
+  2 * 4
+}
+  `.trim(),
+  );
 });
--- src/translator/typescript/statement/if/if-statement-translator.ts
+++ src/translator/typescript/statement/if/if-statement-translator.ts
@@ -1,11 +1,19 @@
-import { ArrayExpressionTree } from '../../../../tree/expression/array/array-expression-tree';
 import { IfStatementTree } from '../../../../tree/statement/if/if-statement-tree';
+import { getBodyTranslator } from '../../body/body-translator-helper';
+import { getExpressionTranslator } from '../../expression/expression-translator-helper';
 import { StatementTranslator } from '../statement-translator';
 
 export class IfStatementTranslator implements StatementTranslator {
   constructor(private tree: IfStatementTree) {}
 
   toString(): string {
-    return this.tree.toString();
+    const condition = getExpressionTranslator(this.tree.condition, false);
+    const thenBody = getBodyTranslator(this.tree.thenBody).toString();
+    const elseBody = (this.tree.elseBody && getBodyTranslator(this.tree.elseBody).toString()) || '';
+    let result = `if (${condition}) {\n${thenBody.replace(/^(.+)/gm, '  $1')}\n}`;
+    if (elseBody) {
+      result += ` else {\n${elseBody.replace(/^(.+)/gm, '  $1')}\n}`;
+    }
+    return result;
   }
 }
