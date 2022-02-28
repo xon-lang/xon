3035d3a8d9ff5815209611af8d9316d5311db377
nizami <nizami.dev@gmail.com>
2022-02-28 21:15:59 +0300

    Update  expression translators

--- src/translator/typescript/statement/import/import-statement-translator.test.ts
+++ src/translator/typescript/statement/import/import-statement-translator.test.ts
@@ -1,12 +1,12 @@
 import { parseStatement } from '../../../../util/parse';
-import { IfStatementTranslator } from '../if/if-statement-translator';
 import { getStatementTranslator } from '../statement-translator-helper';
+import { ImportStatementTranslator } from './import-statement-translator';
 
 test('single body', () => {
-  const code = 'if 12+(45/9) :    14+(144/12) else: 2   *   4   ';
+  const code = 'import abc: ABC';
   const tree = parseStatement(code);
   const translator = getStatementTranslator(tree);
 
-  expect(translator).toBeInstanceOf(IfStatementTranslator);
-  expect(translator.toString()).toBe('if 12 + (45 / 9): 14 + (144 / 12)\nelse: 2 * 4');
+  expect(translator).toBeInstanceOf(ImportStatementTranslator);
+  expect(translator.toString()).toBe('import { ABC } from abc');
 });
