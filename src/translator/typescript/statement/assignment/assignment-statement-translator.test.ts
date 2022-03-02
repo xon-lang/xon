ec824cbd985360d477865338c6e38f8348d8ce92
nizami <nizami.dev@gmail.com>
2022-03-02 21:55:44 +0300

    Fix assngment tests

--- src/translator/typescript/statement/assignment/assignment-statement-translator.test.ts
+++ src/translator/typescript/statement/assignment/assignment-statement-translator.test.ts
@@ -3,7 +3,7 @@ import { getStatementTranslator } from '../statement-translator-helper';
 import { AssignmentStatementTranslator } from './assignment-statement-translator';
 
 test('return value', () => {
-  const code = 'set a = 123';
+  const code = 'a = 123';
   const tree = parseStatement(code);
   const translator = getStatementTranslator(tree);
 
