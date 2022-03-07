a56416a9ac1d0fe1cb66a760f322ce8d4bc015d4
nizami <nizami.dev@gmail.com>
2022-03-07 14:21:24 +0300

    Add definition test

--- src/tree/definition/definition-tree.test.ts
+++ src/tree/definition/definition-tree.test.ts
@@ -1,4 +1,6 @@
-import { parseDefinition } from '../../util/parse';
+import fs from 'fs';
+import { Issue } from '../../issue-service/issue';
+import { parseDefinition, parseSourceFile } from '../../util/parse';
 import { IdExpressionTree } from '../expression/id/id-expression-tree';
 import { DefinitionTree } from './definition-tree';
 
@@ -24,4 +26,21 @@ test('model animal with only attribute', () => {
   expect(tree.attributes.length).toBe(1);
   expect(tree.attributes[0].id.name.text).toBe('abc');
   expect((tree.attributes[0].type as IdExpressionTree).id.name.text).toBe('Integer');
+});
+
+test('1-error.xon', () => {
+  try {
+    const tree = parseSourceFile('src/tree/definition/test-files/1-error.xon');
+    expect(tree.definitions[0]).toBeInstanceOf(DefinitionTree);
+
+    const formatted = tree.toString();
+    fs.writeFileSync('src/tree/definition/test-files/1-error.fmt.xon', formatted);
+  } catch (error) {
+    expect(error).toBeInstanceOf(Issue);
+    if (error instanceof Issue) {
+      expect(error.line).toBe(3);
+      expect(error.column).toBe(14);
+      expect(error.column + error.message.length).toBe(32);
+    }
+  }
 });
