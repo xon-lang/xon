15c651b736810f33e176a4bd75a7948dd27e5a81
nizami <nizami.dev@gmail.com>
2022-03-10 01:27:18 +0300

    Update cli.ts

--- src/util/cli.ts
+++ src/util/cli.ts
@@ -5,12 +5,16 @@ import { getSourceTranslator } from '../translator/typescript/source/source-tran
 import { parseSourceFile } from './parse';
 
 const sourceDir = 'src/**/*[!draft].xon';
+const cmdParam = process.argv[2];
 for (const xonPath of glob.sync(sourceDir)) {
+  if (!cmdParam || !xonPath.includes(cmdParam)) {
+    continue;
+  }
   const stat = fs.lstatSync(xonPath);
   if (path.dirname(xonPath).endsWith('test-files') || stat.isDirectory()) continue;
   const tree = parseSourceFile(xonPath);
   const translator = getSourceTranslator(tree);
-  const destPath = path.resolve(path.dirname(xonPath), path.basename(xonPath, '.xon') + '.gen.ts');
+  const destPath = path.resolve(path.dirname(xonPath), path.basename(xonPath, '.xon') + '.ts');
   fs.writeFileSync(destPath, translator.toString());
   console.log(destPath);
 }
