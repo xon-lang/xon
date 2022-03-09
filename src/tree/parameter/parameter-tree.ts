8e495db96eef43832efb15e320d8832f2260876f
nizami <nizami.dev@gmail.com>
2022-03-10 02:35:55 +0300

    Back parameter-tree.ts

---
+++ src/tree/parameter/parameter-tree.ts
@@ -0,0 +1,35 @@
+import { ParameterContext } from '../../grammar/xon-parser';
+import { ParameterDeclarationMetadata } from '../../metadata/declaration/parameter/parameter-declaration-metadata';
+import { SourceRange } from '../../util/source-range';
+import { BodyTree } from '../body/body-tree';
+import { getBodyTree } from '../body/body-tree-helper';
+import { ExpressionTree } from '../expression/expression-tree';
+import { getExpressionTree } from '../expression/expression-tree-helper';
+import { MethodExpressionTree } from '../expression/method/method-expression-tree';
+import { IdTree } from '../id/id-tree';
+import { getIdTree } from '../id/id-tree-helper';
+import { Tree } from '../tree';
+
+export class ParameterTree implements Tree {
+  sourceReference: SourceRange;
+  metadata: ParameterDeclarationMetadata;
+  id: IdTree;
+  type?: ExpressionTree;
+  body?: BodyTree;
+
+  constructor(private ctx: ParameterContext) {
+    this.sourceReference = SourceRange.fromContext(ctx);
+    this.id = getIdTree(ctx.id());
+    this.type = getExpressionTree(ctx.expr());
+    this.body = getBodyTree(ctx.body());
+  }
+
+  toString() {
+    let type = this.type?.toString() || '';
+    if (type && (this.ctx.id().OPERATOR() || !(this.type instanceof MethodExpressionTree)))
+      type = ' ' + type;
+
+    const body = (this.body && this.body) || '';
+    return this.id + type + body;
+  }
+}
