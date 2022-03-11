40018fe8bb2967dd22f1d4fd3ee6355f67cbc746
nizami <nizami.dev@gmail.com>
2022-03-11 16:05:12 +0300

    Add boolean in core.ts

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -2,5 +2,6 @@ export type Any = any;
 export type Unknown = unknown;
 export type String = string;
 export type Number = number;
+export type Boolean = boolean;
 export type None = null;
 export const none = null;
