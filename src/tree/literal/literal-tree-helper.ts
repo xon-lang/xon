559f170a4b864221b0c392733c7d681d3a907e0f
nizami <nizami.dev@gmail.com>
2022-03-11 21:56:27 +0300

    Remove regexp literal tree

--- src/tree/literal/literal-tree-helper.ts
+++ src/tree/literal/literal-tree-helper.ts
@@ -2,7 +2,6 @@ import {
   FloatLiteralContext,
   IntegerLiteralContext,
   LiteralContext,
-  RegexLiteralContext,
   StringLiteralContext,
 } from '../../grammar/xon-parser';
 import { Issue } from '../../issue-service/issue';
@@ -10,7 +9,6 @@ import { none } from '../../lib/core';
 import { FloatLiteralTree } from './float/float-literal-tree';
 import { IntegerLiteralTree } from './integer/integer-literal-tree';
 import { LiteralTree } from './literal-tree';
-import { RegexLiteralTree } from './regex/regex-literal-tree';
 import { StringLiteralTree } from './string/string-literal-tree';
 
 export const getLiteralTree = (ctx: LiteralContext): LiteralTree => {
@@ -18,7 +16,6 @@ export const getLiteralTree = (ctx: LiteralContext): LiteralTree => {
 
   if (ctx instanceof FloatLiteralContext) return new FloatLiteralTree(ctx);
   if (ctx instanceof IntegerLiteralContext) return new IntegerLiteralTree(ctx);
-  if (ctx instanceof RegexLiteralContext) return new RegexLiteralTree(ctx);
   if (ctx instanceof StringLiteralContext) return new StringLiteralTree(ctx);
 
   Issue.errorFromContext(ctx, `Literal tree not found for "${ctx.constructor.name}"`);
