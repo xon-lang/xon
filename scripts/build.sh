5c07da51ea1b26206f806270b8676db7669d1caf
nizami <nizami.dev@gmail.com>
2022-03-11 05:22:56 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,20 @@
 rm -rf dist
 mkdir dist
 
-find src -name "*.ts" -a ! -name "*.test.ts" ! -name "*.gen.ts" | cpio -p -dumv dist
+find src -a -name "*.ts" ! -name "*.test.ts" ! -name "*.gen.ts" | cpio -p -dumv dist
 cp package.json dist
 cp README.md dist
 cp LICENSE dist
 cp tsconfig.dist.json dist
+cp tsconfig.json dist
 
 cd dist
 npx cti create -e tests, test-helper, .antlr -i .test.ts -w -f .
-npx tsc -p tsconfig.dist.json dist
-npx tsconfig-replace-paths --project tsconfig.json
-find . -name "*.ts" -a ! -name "*.d.ts" -exec rm -rf {} \;
+npx tsc --project tsconfig.dist.json
+npx tsconfig-replace-paths --project tsconfig.dist.json
+
+find . -a -name "*.ts" ! -name "*.d.ts" -exec rm -rf {} \;
+rm -rf tsconfig.dist.json
+rm -rf tsconfig.json
 git init
 cd ..
