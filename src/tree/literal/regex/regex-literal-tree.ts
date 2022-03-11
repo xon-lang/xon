06752b04fc8e5e352d8932057692c84283bb9812
nizami <nizami.dev@gmail.com>
2022-03-11 21:53:28 +0300

    Translate regex-literal-tree.ts

--- src/tree/literal/regex/regex-literal-tree.ts
+++ src/tree/literal/regex/regex-literal-tree.ts
@@ -1,16 +1,23 @@
-import { RegexLiteralContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { LiteralTree } from '../literal-tree';
+// this code was generated
 
-export class RegexLiteralTree implements LiteralTree {
-  sourceRange: SourceRange;
-  value: RegExp;
-  pattern: String;
+import { RegexLiteralContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { LiteralTree } from '../literal-tree'
+
+export class RegexLiteralTree extends LiteralTree {
+  ctx: RegexLiteralContext
+  sourceRange: SourceRange
+  value: RegExp
+  pattern: String
 
   constructor(ctx: RegexLiteralContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.pattern = ctx.text.slice(1, -1).replace(/\\\//g, '/');
-    this.value = new RegExp(this.pattern, 'g');
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.value = new RegExp(this.pattern, 'g')
+    this.pattern = ctx.text.slice(1, -1).replace(/\\\//g, '/')
   }
 }
+
+// this code was generated
