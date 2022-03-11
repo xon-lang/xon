c403a82e3002b2ebd202cb791fb2b09d825961a8
nizami <nizami.dev@gmail.com>
2022-03-11 22:59:10 +0300

    Translate while-statement-tree.ts

--- src/tree/statement/while/while-statement-tree.ts
+++ src/tree/statement/while/while-statement-tree.ts
@@ -1,24 +1,31 @@
-import { WhileStatementContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { BodyTree } from '../../body/body-tree';
-import { getBodyTree } from '../../body/body-tree-helper';
-import { ExpressionTree } from '../../expression/expression-tree';
-import { getExpressionTree } from '../../expression/expression-tree-helper';
-import { StatementTree } from '../statement-tree';
+// this code was generated
 
-export class WhileStatementTree implements StatementTree {
-  sourceRange: SourceRange;
-  expression: ExpressionTree;
-  body: BodyTree;
+import { WhileStatementContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { getBodyTree } from '../../body/body-tree-helper'
+import { BodyTree } from '../../body/body-tree'
+import { getExpressionTree } from '../../expression/expression-tree-helper'
+import { ExpressionTree } from '../../expression/expression-tree'
+import { StatementTree } from '../statement-tree'
+
+export class WhileStatementTree extends StatementTree {
+  ctx: WhileStatementContext
+  sourceRange: SourceRange
+  expression: ExpressionTree
+  body: BodyTree
 
   constructor(ctx: WhileStatementContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.expression = getExpressionTree(ctx.expr());
-    this.body = getBodyTree(ctx.body());
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.expression = getExpressionTree(ctx.expr())
+    this.body = getBodyTree(ctx.body())
   }
 
   toString(): String {
-    return `while ${this.expression}${this.body}`;
+    return `while ${this.expression}${this.body}`
   }
 }
+
+// this code was generated
