ce1fffebbc505b6f2f97cabc24d3bff6827f2a64
nizami <nizami.dev@gmail.com>
2022-03-11 22:47:05 +0300

    Translate if-statement-tree.ts

--- src/tree/statement/if/if-statement-tree.ts
+++ src/tree/statement/if/if-statement-tree.ts
@@ -1,28 +1,33 @@
-import { IfStatementContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { BodyTree } from '../../body/body-tree';
-import { getBodyTree } from '../../body/body-tree-helper';
-import { ExpressionTree } from '../../expression/expression-tree';
-import { getExpressionTree } from '../../expression/expression-tree-helper';
-import { StatementTree } from '../statement-tree';
+// this code was generated
 
-export class IfStatementTree implements StatementTree {
-  sourceRange: SourceRange;
-  condition: ExpressionTree;
-  thenBody: BodyTree;
-  elseBody?: BodyTree;
+import { IfStatementContext } from '../../../grammar/xon-parser'
+import { String, None } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { getBodyTree } from '../../body/body-tree-helper'
+import { BodyTree } from '../../body/body-tree'
+import { getExpressionTree } from '../../expression/expression-tree-helper'
+import { ExpressionTree } from '../../expression/expression-tree'
+import { StatementTree } from '../statement-tree'
+
+export class IfStatementTree extends StatementTree {
+  ctx: IfStatementContext
+  sourceRange: SourceRange
+  condition: ExpressionTree
+  thenBody: BodyTree
+  elseBody?: (BodyTree | None)
 
   constructor(ctx: IfStatementContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.condition = getExpressionTree(ctx.expr());
-    this.thenBody = getBodyTree(ctx._thenBody);
-    this.elseBody = getBodyTree(ctx._elseBody);
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.condition = getExpressionTree(ctx.expr())
+    this.thenBody = getBodyTree(ctx._thenBody)
+    this.elseBody = getBodyTree(ctx._elseBody)
   }
 
   toString(): String {
-    return `if ${this.condition}${this.thenBody}${
-      (this.elseBody && '\nelse' + this.elseBody) || ''
-    }`;
+    return `if ${this.condition}${this.thenBody}${(this.elseBody && '\nelse' + this.elseBody) || ''}`
   }
 }
+
+// this code was generated
