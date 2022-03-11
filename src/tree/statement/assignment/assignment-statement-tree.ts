35be3856853d506bc20fe4ec7de41ce7daff4cb3
nizami <nizami.dev@gmail.com>
2022-03-11 22:06:19 +0300

    Translate assignment-statement-tree.ts

--- src/tree/statement/assignment/assignment-statement-tree.ts
+++ src/tree/statement/assignment/assignment-statement-tree.ts
@@ -1,22 +1,29 @@
-import { AssignmentStatementContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { ExpressionTree } from '../../expression/expression-tree';
-import { getExpressionTree } from '../../expression/expression-tree-helper';
-import { StatementTree } from '../statement-tree';
+// this code was generated
 
-export class AssignmentStatementTree implements StatementTree {
-  sourceRange: SourceRange;
-  variable: ExpressionTree;
-  value: ExpressionTree;
+import { AssignmentStatementContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { getExpressionTree } from '../../expression/expression-tree-helper'
+import { ExpressionTree } from '../../expression/expression-tree'
+import { StatementTree } from '../statement-tree'
+
+export class AssignmentStatementTree extends StatementTree {
+  ctx: AssignmentStatementContext
+  sourceRange: SourceRange
+  variable: ExpressionTree
+  value: ExpressionTree
 
   constructor(ctx: AssignmentStatementContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.variable = getExpressionTree(ctx.expr(0));
-    this.value = getExpressionTree(ctx.expr(1));
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.variable = getExpressionTree(ctx.expr(0))
+    this.value = getExpressionTree(ctx.expr(1))
   }
 
   toString(): String {
-    return `${this.variable} = ${this.value}`;
+    return `${this.variable} = ${this.value}`
   }
 }
+
+// this code was generated
