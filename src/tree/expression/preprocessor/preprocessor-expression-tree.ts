b0b00fed5f497d31bc45d701c03f339f92ce553d
nizami <nizami.dev@gmail.com>
2022-03-11 21:42:44 +0300

    Translate preprocessor-expression-tree.ts

--- src/tree/expression/preprocessor/preprocessor-expression-tree.ts
+++ src/tree/expression/preprocessor/preprocessor-expression-tree.ts
@@ -1,20 +1,27 @@
-import { PreprocessorExpressionContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata';
-import { SourceRange } from '../../../util/source-range';
-import { ExpressionTree } from '../expression-tree';
+// this code was generated
 
-export class PreprocessorExpressionTree implements ExpressionTree {
-  metadata: ExpressionMetadata;
-  sourceRange: SourceRange;
-  value: String;
+import { PreprocessorExpressionContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata'
+import { SourceRange } from '../../../util/source-range'
+import { ExpressionTree } from '../expression-tree'
+
+export class PreprocessorExpressionTree extends ExpressionTree {
+  metadata: ExpressionMetadata
+  ctx: PreprocessorExpressionContext
+  sourceRange: SourceRange
+  value: String
 
   constructor(ctx: PreprocessorExpressionContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.value = ctx.PREPROCESSOR().text.trim().slice(2, -1);
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.value = ctx.PREPROCESSOR().text.trim().slice(2, -1)
   }
 
   toString(): String {
-    return `#{${this.value}}`;
+    return `#{${this.value}}`
   }
 }
+
+// this code was generated
