012e4970b513ec04b909d8c2d406e573b9f242b3
nizami <nizami.dev@gmail.com>
2022-03-11 21:53:41 +0300

    Translate string-literal-tree.ts

--- src/tree/literal/string/string-literal-tree.ts
+++ src/tree/literal/string/string-literal-tree.ts
@@ -1,14 +1,21 @@
-import { StringLiteralContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { LiteralTree } from '../literal-tree';
+// this code was generated
 
-export class StringLiteralTree implements LiteralTree {
-  sourceRange: SourceRange;
-  value: String;
+import { StringLiteralContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { LiteralTree } from '../literal-tree'
+
+export class StringLiteralTree extends LiteralTree {
+  ctx: StringLiteralContext
+  sourceRange: SourceRange
+  value: String
 
   constructor(ctx: StringLiteralContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.value = ctx.text.slice(1, -1).replace(/\\'/g, "'");
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.value = ctx.text.slice(1, -1).replace(/\\'/g, "'")
   }
 }
+
+// this code was generated
