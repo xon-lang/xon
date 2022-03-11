de84e3ca502f14507d09007cf2417a3bd1d3ace7
nizami <nizami.dev@gmail.com>
2022-03-11 05:11:23 +0300

    Remove id translator

--- src/translator/typescript/id/id-translator-helper.ts
+++
@@ -1,8 +0,0 @@
-import { IdTree } from '../../../tree/id/id-tree';
-import { IdTranslator } from './id-translator';
-
-export const getIdTranslator = (tree: IdTree, useModifier = false): IdTranslator => {
-  if (!tree) return null;
-
-  return new IdTranslator(tree, useModifier);
-};
