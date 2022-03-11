b1b7d04e7177d2e68e72bcee3633cf1763ffbe70
nizami <nizami.dev@gmail.com>
2022-03-11 13:43:28 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,6 @@ cp tsconfig.json dist
 cd dist
 npx cti create -e tests, test-helper, .antlr -i .test.ts -w -f .
 npx tsc --project tsconfig.dist.json
-npx tsconfig-replace-paths --project tsconfig.dist.json
 
 find . -a -name "*.ts" ! -name "*.d.ts" -exec rm -rf {} \;
 rm -rf tsconfig.dist.json
