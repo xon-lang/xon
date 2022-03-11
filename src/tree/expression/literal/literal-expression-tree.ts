af2628526c6f6e49838b20efd98208c15f9c66d5
nizami <nizami.dev@gmail.com>
2022-03-11 16:57:39 +0300

    Translate literal-expression-tree.ts

--- src/tree/expression/literal/literal-expression-tree.ts
+++ src/tree/expression/literal/literal-expression-tree.ts
@@ -1,22 +1,29 @@
-import { LiteralExpressionContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { LiteralExpressionMetadata } from '../../../metadata/expression/literal/literal-expression-metadata';
-import { SourceRange } from '../../../util/source-range';
-import { LiteralTree } from '../../literal/literal-tree';
-import { getLiteralTree } from '../../literal/literal-tree-helper';
-import { ExpressionTree } from '../expression-tree';
+// this code was generated
 
-export class LiteralExpressionTree implements ExpressionTree {
-  sourceRange: SourceRange;
-  metadata: LiteralExpressionMetadata;
-  literal: LiteralTree;
+import { LiteralExpressionContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { LiteralExpressionMetadata } from '../../../metadata/expression/literal/literal-expression-metadata'
+import { SourceRange } from '../../../util/source-range'
+import { getLiteralTree } from '../../literal/literal-tree-helper'
+import { LiteralTree } from '../../literal/literal-tree'
+import { ExpressionTree } from '../expression-tree'
+
+export class LiteralExpressionTree extends ExpressionTree {
+  metadata: LiteralExpressionMetadata
+  ctx: LiteralExpressionContext
+  sourceRange: SourceRange
+  literal: LiteralTree
 
   constructor(ctx: LiteralExpressionContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.literal = ctx && getLiteralTree(ctx.literal());
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.literal = ctx && getLiteralTree(ctx.literal())
   }
 
   toString(): String {
-    return this.literal.sourceRange.rangeText;
+    return this.literal.sourceRange.rangeText
   }
 }
+
+// this code was generated
