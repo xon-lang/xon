4c00dff76ac9c9392b82d1ded744b8a968b5b24e
nizami <nizami.dev@gmail.com>
2022-03-11 22:52:06 +0300

    Translate import-statement-tree.ts

--- src/tree/statement/import/import-statement-tree.ts
+++ src/tree/statement/import/import-statement-tree.ts
@@ -1,25 +1,29 @@
-import { ImportStatementContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { ExpressionTree } from '../../expression/expression-tree';
-import { getExpressionTree, getExpressionTrees } from '../../expression/expression-tree-helper';
-import { StatementTree } from '../statement-tree';
+// this code was generated
 
-export class ImportStatementTree implements StatementTree {
-  sourceRange: SourceRange;
-  path: ExpressionTree;
-  members?: ExpressionTree[];
+import { ImportStatementContext } from '../../../grammar/xon-parser'
+import { String, None } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { getExpressionTree, getExpressionTrees } from '../../expression/expression-tree-helper'
+import { ExpressionTree } from '../../expression/expression-tree'
+import { StatementTree } from '../statement-tree'
+
+export class ImportStatementTree extends StatementTree {
+  sourceRange: SourceRange
+  path: ExpressionTree
+  members?: (ExpressionTree[] | None)
 
   constructor(ctx: ImportStatementContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.path = getExpressionTree(ctx._path);
-    this.members = getExpressionTrees(ctx._members);
+    super()
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.path = getExpressionTree(ctx._path)
+    this.members = getExpressionTrees(ctx._members)
   }
 
   toString(): String {
-    const members = this.members.sort((a, b) =>
-      a.sourceRange.rangeText.localeCompare(b.sourceRange.rangeText),
-    );
-    return `import ${this.path}: ${members.join(', ')}`;
+    let members
+    members = this.members.sort((a, b) => a.sourceRange.rangeText.localeCompare(b.sourceRange.rangeText))
+    return `import ${this.path}: ${members.join(', ')}`
   }
 }
+
+// this code was generated
