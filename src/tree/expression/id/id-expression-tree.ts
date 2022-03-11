e4346f53cef7b961708e3e26c2ddf99cfea962f0
nizami <nizami.dev@gmail.com>
2022-03-11 16:32:47 +0300

    Translate id-expression-tree.ts

--- src/tree/expression/id/id-expression-tree.ts
+++ src/tree/expression/id/id-expression-tree.ts
@@ -1,21 +1,28 @@
-import { IdExpressionContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata';
-import { getIdToken, IdToken } from '../../../util/id-token';
-import { SourceRange } from '../../../util/source-range';
-import { ExpressionTree } from '../expression-tree';
+// this code was generated
 
-export class IdExpressionTree implements ExpressionTree {
-  sourceRange: SourceRange;
-  metadata: ExpressionMetadata;
-  name: IdToken;
+import { IdExpressionContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata'
+import { getIdToken, IdToken } from '../../../util/id-token'
+import { SourceRange } from '../../../util/source-range'
+import { ExpressionTree } from '../expression-tree'
+
+export class IdExpressionTree extends ExpressionTree {
+  metadata: ExpressionMetadata
+  ctx: IdExpressionContext
+  sourceRange: SourceRange
+  name: IdToken
 
   constructor(ctx: IdExpressionContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.name = getIdToken(ctx._name);
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.name = getIdToken(ctx._name)
   }
 
   toString(): String {
-    return this.name.toString();
+    return this.name.toString()
   }
 }
+
+// this code was generated
