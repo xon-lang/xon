ca160d82b1702974e15ab9d8cc16b03e1ad4db99
nizami <nizami.dev@gmail.com>
2022-03-11 22:35:05 +0300

    Translate for-statement-tree.ts

--- src/tree/statement/for/for-statement-tree.ts
+++ src/tree/statement/for/for-statement-tree.ts
@@ -1,29 +1,37 @@
-import { ForStatementContext } from '../../../grammar/xon-parser';
-import { none, String } from '../../../lib/core';
-import { SourceRange } from '../../../util/source-range';
-import { BodyTree } from '../../body/body-tree';
-import { getBodyTree } from '../../body/body-tree-helper';
-import { ExpressionTree } from '../../expression/expression-tree';
-import { getExpressionTree } from '../../expression/expression-tree-helper';
-import { StatementTree } from '../statement-tree';
+// this code was generated
 
-export class ForStatementTree implements StatementTree {
-  sourceRange: SourceRange;
-  valueVarName?: String;
-  indexVarName?: String;
-  expression: ExpressionTree;
-  body: BodyTree;
+import { ForStatementContext } from '../../../grammar/xon-parser'
+import { none, String, None } from '../../../lib/core'
+import { SourceRange } from '../../../util/source-range'
+import { getBodyTree } from '../../body/body-tree-helper'
+import { BodyTree } from '../../body/body-tree'
+import { getExpressionTree } from '../../expression/expression-tree-helper'
+import { ExpressionTree } from '../../expression/expression-tree'
+import { StatementTree } from '../statement-tree'
+
+export class ForStatementTree extends StatementTree {
+  ctx: ForStatementContext
+  sourceRange: SourceRange
+  valueVarName?: (String | None)
+  indexVarName?: (String | None)
+  expression: ExpressionTree
+  body: BodyTree
 
   constructor(ctx: ForStatementContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.valueVarName = ctx._value?.text || none;
-    this.indexVarName = ctx._index?.text || none;
-    this.expression = getExpressionTree(ctx.expr());
-    this.body = getBodyTree(ctx.body());
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.valueVarName = ctx._value?.text || none
+    this.indexVarName = ctx._index?.text || none
+    this.expression = getExpressionTree(ctx.expr())
+    this.body = getBodyTree(ctx.body())
   }
 
   toString(): String {
-    const vars = [this.valueVarName, this.indexVarName].filter((x) => x).join(',');
-    return `for ${(vars && vars + ' in ') || ''}${this.expression}\n${this.body}`;
+    let vars
+    vars = [this.valueVarName, this.indexVarName].filter((x) => x).join(',')
+    return `for ${(vars && vars + ' in ') || ''}${this.expression}\n${this.body}`
   }
 }
+
+// this code was generated
