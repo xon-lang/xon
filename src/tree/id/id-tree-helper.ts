e2da9aba989f5bbb769d0d2e28e410754aa0aaf8
nizami <nizami.dev@gmail.com>
2022-03-11 05:02:41 +0300

    Remove id tree

--- src/tree/id/id-tree-helper.ts
+++
@@ -1,8 +0,0 @@
-import { IdContext } from '../../grammar/xon-parser';
-import { IdTree } from './id-tree';
-
-export const getIdTree = (ctx: IdContext): IdTree => {
-  if (!ctx) return null;
-
-  return new IdTree(ctx);
-};
