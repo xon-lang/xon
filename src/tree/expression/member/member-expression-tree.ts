962830efcfb9d42a6d6e365f10a5c69a910f2692
nizami <nizami.dev@gmail.com>
2022-03-11 20:29:47 +0300

    Translate member-expression-tree.ts

--- src/tree/expression/member/member-expression-tree.ts
+++ src/tree/expression/member/member-expression-tree.ts
@@ -1,24 +1,31 @@
-import { MemberExpressionContext } from '../../../grammar/xon-parser';
-import { String } from '../../../lib/core';
-import { MemberExpressionMetadata } from '../../../metadata/expression/member/member-expression-metadata';
-import { getIdToken, IdToken } from '../../../util/id-token';
-import { SourceRange } from '../../../util/source-range';
-import { ExpressionTree } from '../expression-tree';
-import { getExpressionTree } from '../expression-tree-helper';
+// this code was generated
 
-export class MemberExpressionTree implements ExpressionTree {
-  sourceRange: SourceRange;
-  metadata: MemberExpressionMetadata;
-  instance: ExpressionTree;
-  name: IdToken;
+import { MemberExpressionContext } from '../../../grammar/xon-parser'
+import { String } from '../../../lib/core'
+import { MemberExpressionMetadata } from '../../../metadata/expression/member/member-expression-metadata'
+import { getIdToken, IdToken } from '../../../util/id-token'
+import { SourceRange } from '../../../util/source-range'
+import { getExpressionTree } from '../expression-tree-helper'
+import { ExpressionTree } from '../expression-tree'
+
+export class MemberExpressionTree extends ExpressionTree {
+  metadata: MemberExpressionMetadata
+  ctx: MemberExpressionContext
+  sourceRange: SourceRange
+  instance: ExpressionTree
+  name: IdToken
 
   constructor(ctx: MemberExpressionContext) {
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.instance = getExpressionTree(ctx.expr());
-    this.name = getIdToken(ctx._name);
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.instance = getExpressionTree(ctx.expr())
+    this.name = getIdToken(ctx._name)
   }
 
   toString(): String {
-    return `${this.instance}.${this.name}`;
+    return `${this.instance}.${this.name}`
   }
 }
+
+// this code was generated
