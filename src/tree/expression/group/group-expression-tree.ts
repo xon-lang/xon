d646b67e6d0210d0a03e00f060527d928bc977a4
nizami <nizami.dev@gmail.com>
2022-03-15 01:20:10 +0300

    Translate

--- src/tree/expression/group/group-expression-tree.ts
+++ src/tree/expression/group/group-expression-tree.ts
@@ -4,8 +4,8 @@ import { GroupExpressionContext } from '../../../grammar/xon-parser'
 import { String } from '../../../lib/core'
 import { ExpressionMetadata } from '../../../metadata/expression/expression-metadata'
 import { SourceRange } from '../../../util/source-range'
-import { ExpressionTree } from '../expression-tree'
 import { getExpressionTree } from '../expression-tree-helper'
+import { ExpressionTree } from '../expression-tree'
 
 export class GroupExpressionTree extends ExpressionTree {
   metadata: ExpressionMetadata
