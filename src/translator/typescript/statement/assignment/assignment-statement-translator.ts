0fd5c5696ef31b8a5eb5b240287038ed53c5fb04
nizami <nizami.dev@gmail.com>
2022-03-15 20:36:17 +0300

    Update assignment translator

--- src/translator/typescript/statement/assignment/assignment-statement-translator.ts
+++ src/translator/typescript/statement/assignment/assignment-statement-translator.ts
@@ -1,5 +1,8 @@
 import { String } from '../../../../lib/core';
+import { SingleBodyTree } from '../../../../tree/body/single/single-body-tree';
 import { AssignmentStatementTree } from '../../../../tree/statement/assignment/assignment-statement-tree';
+import { ExpressionStatementTree } from '../../../../tree/statement/expression/expression-statement-tree';
+import { getBodyTranslator } from '../../body/body-translator-helper';
 import { getExpressionTranslator } from '../../expression/expression-translator-helper';
 import { StatementTranslator } from '../statement-translator';
 
@@ -8,10 +11,22 @@ export class AssignmentStatementTranslator implements StatementTranslator {
 
   toString(): String {
     const variable = getExpressionTranslator(this.tree.variable, false);
-    const value = getExpressionTranslator(this.tree.value, false);
+    const body = getBodyTranslator(this.tree.body);
     // if (variable instanceof IdExpressionTranslator) {
     //   return `const ${variable} = ${value}`;
     // }
-    return `${variable} = ${value}`;
+
+    if (body instanceof SingleBodyTree) {
+      if (body.statement instanceof ExpressionStatementTree) {
+        return `${variable} = ${body}`;
+      }
+
+      throw new Error('Not Implemented');
+    }
+
+    return `${variable} = ${body}`;
   }
 }
+
+// a = if a: 1 else: 2
+// if() {}
