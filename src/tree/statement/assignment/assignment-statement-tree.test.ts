2b1610ab21648f2d565bf6102b917c7870025a84
nizami <nizami.dev@gmail.com>
2022-03-15 21:13:50 +0300

    Update assignment statement

--- src/tree/statement/assignment/assignment-statement-tree.test.ts
+++ src/tree/statement/assignment/assignment-statement-tree.test.ts
@@ -1,13 +1,36 @@
 import { parseStatement } from '../../../util/parse';
+import { SingleBodyTree } from '../../body/single/single-body-tree';
 import { IdExpressionTree } from '../../expression/id/id-expression-tree';
 import { LiteralExpressionTree } from '../../expression/literal/literal-expression-tree';
+import { ExpressionStatementTree } from '../expression/expression-statement-tree';
 import { AssignmentStatementTree } from './assignment-statement-tree';
 
-test('variable assignment', () => {
-  const code = 'a = 1';
+test('variable assignment colon', () => {
+  const code = 'a : 1';
   const tree = parseStatement(code) as AssignmentStatementTree;
 
   expect(tree).toBeInstanceOf(AssignmentStatementTree);
   expect((tree.variable as IdExpressionTree).name.text).toBe('a');
-  expect((tree.value as LiteralExpressionTree).literal.value).toBe(1);
+  expect(
+    (
+      ((tree.body as SingleBodyTree).statement as ExpressionStatementTree)
+        .expression as LiteralExpressionTree
+    ).literal.value,
+  ).toBe(1);
+  expect(tree.toString()).toBe('a: 1');
+});
+
+test('variable assignment equals', () => {
+  const code = 'a  =  1';
+  const tree = parseStatement(code) as AssignmentStatementTree;
+
+  expect(tree).toBeInstanceOf(AssignmentStatementTree);
+  expect((tree.variable as IdExpressionTree).name.text).toBe('a');
+  expect(
+    (
+      ((tree.body as SingleBodyTree).statement as ExpressionStatementTree)
+        .expression as LiteralExpressionTree
+    ).literal.value,
+  ).toBe(1);
+  expect(tree.toString()).toBe('a = 1');
 });
--- src/tree/statement/assignment/assignment-statement-tree.ts
+++ src/tree/statement/assignment/assignment-statement-tree.ts
@@ -3,6 +3,8 @@
 import { AssignmentStatementContext } from '../../../grammar/xon-parser'
 import { String } from '../../../lib/core'
 import { SourceRange } from '../../../util/source-range'
+import { getBodyTree } from '../../body/body-tree-helper'
+import { BodyTree } from '../../body/body-tree'
 import { getExpressionTree } from '../../expression/expression-tree-helper'
 import { ExpressionTree } from '../../expression/expression-tree'
 import { StatementTree } from '../statement-tree'
@@ -11,18 +13,18 @@ export class AssignmentStatementTree extends StatementTree {
   ctx: AssignmentStatementContext
   sourceRange: SourceRange
   variable: ExpressionTree
-  value: ExpressionTree
+  body: BodyTree
 
   constructor(ctx: AssignmentStatementContext) {
     super()
     this.ctx = ctx
     this.sourceRange = SourceRange.fromContext(ctx)
-    this.variable = getExpressionTree(ctx.expr(0))
-    this.value = getExpressionTree(ctx.expr(1))
+    this.variable = getExpressionTree(ctx.expr())
+    this.body = getBodyTree(ctx.body())
   }
 
   toString(): String {
-    return `${this.variable} = ${this.value}`
+    return `${this.variable}${this.body}`
   }
 }
 
