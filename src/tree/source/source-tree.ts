f8789bca6203a25c8b9e487847600d5ed73b7af2
nizami <nizami.dev@gmail.com>
2022-03-15 16:46:44 +0300

    Update source tree

--- src/tree/source/source-tree.ts
+++ src/tree/source/source-tree.ts
@@ -3,6 +3,8 @@
 import { SourceContext } from '../../grammar/xon-parser'
 import { String } from '../../lib/core'
 import { SourceRange } from '../../util/source-range'
+import { getAttributeTrees } from '../attribute/attribute-tree-helper'
+import { AttributeTree } from '../attribute/attribute-tree'
 import { getDefinitionTrees } from '../definition/definition-tree-helper'
 import { DefinitionTree } from '../definition/definition-tree'
 import { ImportStatementTree } from '../statement/import/import-statement-tree'
@@ -14,6 +16,7 @@ export class SourceTree extends Tree {
   ctx: SourceContext
   sourceRange: SourceRange
   statements: StatementTree[]
+  attributes: AttributeTree[]
   definitions: DefinitionTree[]
 
   constructor(ctx: SourceContext) {
@@ -21,15 +24,17 @@ export class SourceTree extends Tree {
     this.ctx = ctx
     this.sourceRange = SourceRange.fromContext(ctx)
     this.statements = getStatementTrees(ctx.statement())
+    this.attributes = getAttributeTrees(ctx.attribute())
     this.definitions = getDefinitionTrees(ctx.definition())
   }
 
   toString(): String {
-    let importStatements, otherStatements, definitions, result
+    let importStatements, otherStatements, attributes, definitions, result
     importStatements = this.statements.filter((x) => x instanceof ImportStatementTree).map((x) => x as ImportStatementTree).sort((a, b) => a.path.toString().localeCompare(b.path.toString())).join('\n')
     otherStatements = this.statements.filter((x) => !(x instanceof ImportStatementTree)).join('\n')
+    attributes = this.attributes.join('\n\n')
     definitions = this.definitions.join('\n\n')
-    result = [importStatements, otherStatements, definitions].filter(Boolean).join('\n\n')
+    result = [importStatements, otherStatements, attributes, definitions].filter(Boolean).join('\n\n')
     return (result && result + '\n') || ''
   }
 }
