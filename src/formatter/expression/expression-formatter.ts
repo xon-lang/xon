dc87f35d7bfa2b384ba1d70f93c3419d02ed6415
nizami <nizami.dev@gmail.com>
2022-03-28 08:06:40 +0300

    Update expression formatter

--- src/formatter/expression/expression-formatter.ts
+++ src/formatter/expression/expression-formatter.ts
@@ -1,3 +1,6 @@
+import { ExprContext } from '../../grammar/xon-parser';
 import { Formatter } from '../formatter';
 
-export abstract class ExpressionFormatter extends Formatter {}
+export abstract class ExpressionFormatter extends Formatter {
+  ctx: ExprContext;
+}
