00cb704f5dbf54b450f7a0758da39d050999576d
nizami <nizami.dev@gmail.com>
2022-03-28 07:16:03 +0300

    Add literal expression formatter

---
+++ src/formatter/expression/literal/literal-expression-formatter.test.ts
@@ -0,0 +1,17 @@
+import { parse } from '../../../util/parse';
+import { defaultFormatterConfig } from '../../formatter-config';
+import { getExpressionFormatter } from '../expression-formatter-helper';
+import { LiteralExpressionFormatter } from './literal-expression-formatter';
+
+test('integer', () => {
+  const code = '123';
+  const ctx = parse(code).expr();
+  const formatter = getExpressionFormatter(
+    ctx,
+    defaultFormatterConfig,
+  ) as LiteralExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(LiteralExpressionFormatter);
+  expect(formatter.toString()).toBe('123');
+  expect(formatter.indent(2).toString()).toBe('    123');
+});
---
+++ src/formatter/expression/literal/literal-expression-formatter.ts
@@ -0,0 +1,13 @@
+import { LiteralExpressionContext } from '../../../grammar/xon-parser';
+import { FormatterConfig } from '../../formatter-config';
+import { ExpressionFormatter } from '../expression-formatter';
+
+export class LiteralExpressionFormatter extends ExpressionFormatter {
+  constructor(public ctx: LiteralExpressionContext, public config: FormatterConfig) {
+    super();
+  }
+
+  toString() {
+    return this.indentString + this.ctx.text;
+  }
+}
