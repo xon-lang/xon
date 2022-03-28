02ed92f8a177f5634cfdb46c640641240fcc34ee
nizami <nizami.dev@gmail.com>
2022-03-28 07:17:42 +0300

    Add member expression formatter

---
+++ src/formatter/expression/member/member-expression-formatter.test.ts
@@ -0,0 +1,39 @@
+import { parse } from '../../../util/parse';
+import { FormatterConfig } from '../../formatter-config';
+import { getExpressionFormatter } from '../expression-formatter-helper';
+import { MemberExpressionFormatter } from './member-expression-formatter';
+
+test('properties only', () => {
+  const code = `a.b.c.d.efg.hij`;
+  const ctx = parse(code).expr();
+  const config = new FormatterConfig();
+  config.printWidth = 5;
+  const formatter = getExpressionFormatter(ctx, config) as MemberExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(MemberExpressionFormatter);
+  expect(formatter.toString()).toBe(
+    `a
+  .b
+  .c
+  .d
+  .efg
+  .hij`.trim(),
+  );
+});
+
+test('members with method call', () => {
+  const code = `a.b.c(a).dom`;
+  const ctx = parse(code).expr();
+  const config = new FormatterConfig();
+  config.printWidth = 5;
+  const formatter = getExpressionFormatter(ctx, config) as MemberExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(MemberExpressionFormatter);
+  console.log(formatter.toString());
+  
+  expect(formatter.toString()).toBe(
+    `a.b.c(
+  a,
+).dom`.trim(),
+  );
+});
---
+++ src/formatter/expression/member/member-expression-formatter.ts
@@ -0,0 +1,32 @@
+import { MemberExpressionContext } from '../../../grammar/xon-parser';
+import { FormatterConfig } from '../../formatter-config';
+import { ExpressionFormatter } from '../expression-formatter';
+import { getExpressionFormatter } from '../expression-formatter-helper';
+
+export class MemberExpressionFormatter extends ExpressionFormatter {
+  broken: boolean;
+
+  constructor(public ctx: MemberExpressionContext, public config: FormatterConfig) {
+    super();
+  }
+
+  toString() {
+    const expression = getExpressionFormatter(this.ctx.expr(), this.config).indent(
+      this.indentCount,
+    );
+    const name = this.ctx._name.text;
+
+    let result = `${expression}.${name}`;
+    const endLineCharPosition = this.config.endLineCharPosition(result);
+
+    if (this.broken || this.brokenMember || endLineCharPosition > this.config.printWidth) {
+      this.broken = true;
+      this.brokenMember = true;
+      result = `${expression.breakMember(true)}\n${this.config.indent(
+        this.indentCount + 1,
+      )}.${name}`;
+    }
+
+    return result;
+  }
+}
