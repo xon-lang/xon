219a8c78bee84e0ca667055062e7f929260e968c
nizami <nizami.dev@gmail.com>
2022-03-28 07:15:35 +0300

    Add base formatter

---
+++ src/formatter/formatter.ts
@@ -0,0 +1,31 @@
+import { ParserRuleContext } from 'antlr4ts';
+import { Number } from '../lib/core';
+import { FormatterConfig } from './formatter-config';
+
+export abstract class Formatter {
+  indentCount: Number = 0;
+  indentString: String = '';
+  broken: Boolean = false;
+  brokenMember: Boolean = false;
+
+  ctx: ParserRuleContext;
+  config: FormatterConfig;
+
+  indent(value: number): Formatter {
+    this.indentCount = value;
+    this.indentString = this.config.indent(value);
+    return this;
+  }
+
+  break(value: Boolean): Formatter {
+    this.broken = value;
+    return this;
+  }
+
+  breakMember(value: Boolean): Formatter {
+    this.brokenMember = value;
+    return this;
+  }
+
+  abstract toString(): string;
+}
