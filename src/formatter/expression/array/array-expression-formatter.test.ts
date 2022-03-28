354d3b22c993ed4da0cd3dbcfc3ed4b10cd45fda
nizami <nizami.dev@gmail.com>
2022-03-28 07:16:48 +0300

    Add array expression formatter

---
+++ src/formatter/expression/array/array-expression-formatter.test.ts
@@ -0,0 +1,70 @@
+import { parse } from '../../../util/parse';
+import { defaultFormatterConfig, FormatterConfig } from '../../formatter-config';
+import { getExpressionFormatter } from '../expression-formatter-helper';
+import { ArrayExpressionFormatter } from './array-expression-formatter';
+
+test('integers', () => {
+  const code = '[1, 2, 3]';
+  const ctx = parse(code).expr();
+  const formatter = getExpressionFormatter(ctx, defaultFormatterConfig) as ArrayExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(ArrayExpressionFormatter);
+  expect(formatter.toString()).toBe('[1, 2, 3]');
+  expect(formatter.indent(2).toString()).toBe('    [1, 2, 3]');
+});
+
+test('integers', () => {
+  const code = '[1, 2, 3]';
+  const ctx = parse(code).expr();
+  const config = new FormatterConfig();
+  config.printWidth = 3;
+  const formatter = getExpressionFormatter(ctx, config) as ArrayExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(ArrayExpressionFormatter);
+  expect(formatter.toString()).toBe(
+    `
+[
+  1,
+  2,
+  3,
+]`.trim(),
+  );
+
+  expect(formatter.indent(2).toString()).toBe(
+    `    [
+      1,
+      2,
+      3,
+    ]`,
+  );
+});
+
+test('contains new line', () => {
+  const code = `[1, [2
+  ], 3]`;
+  const ctx = parse(code).expr();
+  const config = new FormatterConfig();
+  config.printWidth = 30;
+  const formatter = getExpressionFormatter(ctx, config) as ArrayExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(ArrayExpressionFormatter);
+  expect(formatter.toString()).toBe(`[1, [2], 3]`);
+});
+
+test('array element in array', () => {
+  const code = '[[1]]';
+  const ctx = parse(code).expr();
+  const config = new FormatterConfig();
+  config.printWidth = 2;
+  const formatter = getExpressionFormatter(ctx, config) as ArrayExpressionFormatter;
+
+  expect(formatter).toBeInstanceOf(ArrayExpressionFormatter);
+  expect(formatter.toString()).toBe(
+    `
+[
+  [
+    1,
+  ],
+]`.trim(),
+  );
+});
---
+++ src/formatter/expression/array/array-expression-formatter.ts
@@ -0,0 +1,32 @@
+import { ArrayExpressionContext } from '../../../grammar/xon-parser';
+import { FormatterConfig } from '../../formatter-config';
+import { getParameterFormatter } from '../../parameter/parameter-formatter-helper';
+import { ExpressionFormatter } from '../expression-formatter';
+
+export class ArrayExpressionFormatter extends ExpressionFormatter {
+  constructor(public ctx: ArrayExpressionContext, public config: FormatterConfig) {
+    super();
+  }
+
+  toString() {
+    const parameters = this.ctx
+      .parameters()
+      .parameter()
+      .map((x) => getParameterFormatter(x, this.config));
+
+    let joinedParameters = parameters.join(', ');
+    let result = `${this.indentString}[${joinedParameters}]`;
+    const endLineCharPosition = this.config.endLineCharPosition(result);
+
+    if (
+      this.broken ||
+      endLineCharPosition > this.config.printWidth ||
+      joinedParameters.includes(this.config.nl)
+    ) {
+      joinedParameters =
+        parameters.map((x) => x.indent(this.indentCount + 1)).join(',' + this.config.nl) + ',';
+      result = `${this.indentString}[\n${joinedParameters}\n${this.indentString}]`;
+    }
+    return result;
+  }
+}
