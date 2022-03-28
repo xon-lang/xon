0bce3471056e676e71cf4652975fbc893cbd078d
nizami <nizami.dev@gmail.com>
2022-03-28 17:11:50 +0300

    Update expression statement formatter test

--- src/formatter/statement/expression/expression-statement-formatter.test.ts
+++ src/formatter/statement/expression/expression-statement-formatter.test.ts
@@ -3,12 +3,12 @@ import { defaultFormatterConfig } from '../../formatter-config';
 import { getStatementFormatter } from '../statement-formatter-helper';
 import { ExpressionStatementFormatter } from './expression-statement-formatter';
 
-test('only nullable', () => {
-  const code = 'if a: b else: c';
+test('expression', () => {
+  const code = 'abc?.def( )';
   const ctx = parse(code).statement();
   const formatter = getStatementFormatter(ctx, defaultFormatterConfig);
 
   expect(formatter).toBeInstanceOf(ExpressionStatementFormatter);
-  // expect(formatter.toString()).toBe('abc?');
-  // expect(formatter.indent(2).toString()).toBe('    abc?');
+  expect(formatter.toString()).toBe('abc?.def()');
+  expect(formatter.indent(2).toString()).toBe('    abc?.def()');
 });
