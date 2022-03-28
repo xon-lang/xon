25694d3290f7e0bb171bcb28f67428efe108be76
nizami <nizami.dev@gmail.com>
2022-03-28 07:15:46 +0300

    Add formatter config

---
+++ src/formatter/formatter-config.ts
@@ -0,0 +1,17 @@
+import { Number, String } from '../lib/core';
+
+export class FormatterConfig {
+  printWidth = 100;
+  tabWidth = 2;
+  nl = '\n';
+
+  indent(count: Number) {
+    return ' '.repeat(this.tabWidth).repeat(count);
+  }
+
+  endLineCharPosition(value: String) {
+    return value.split(this.nl).slice(-1)[0].length;
+  }
+}
+
+export const defaultFormatterConfig = new FormatterConfig();
