400b371f6abacde40ba29613c79cdda2344e0f9d
nizami <nizami.dev@gmail.com>
2022-03-29 19:48:46 +0300

    Refactoring

--- src/tree/definition/definition-tree.ts
+++ src/tree/definition/definition-tree.ts
@@ -1,52 +1,49 @@
 // this code was generated
 
-import { DefinitionContext } from '../../grammar/xon-parser';
-import { none, None, String } from '../../lib/core';
-import { DefinitionDeclarationMetadata } from '../../metadata/declaration/definition/definition-declaration-metadata';
-import { getIdToken, IdToken } from '../../util/id-token';
-import { SourceRange } from '../../util/source-range';
-import { AttributeTree } from '../attribute/attribute-tree';
-import { getAttributeTrees } from '../attribute/attribute-tree-helper';
-import { ExpressionTree } from '../expression/expression-tree';
-import { getExpressionTree } from '../expression/expression-tree-helper';
-import { ParameterTree } from '../parameter/parameter-tree';
-import { getParameterTrees } from '../parameter/parameter-tree-helper';
-import { Tree } from '../tree';
+import { DefinitionContext } from '../../grammar/xon-parser'
+import { none, None, String } from '../../lib/core'
+import { DefinitionDeclarationMetadata } from '../../metadata/declaration/definition/definition-declaration-metadata'
+import { getIdToken, IdToken } from '../../util/id-token'
+import { SourceRange } from '../../util/source-range'
+import { getAttributeTrees } from '../attribute/attribute-tree-helper'
+import { AttributeTree } from '../attribute/attribute-tree'
+import { getExpressionTree } from '../expression/expression-tree-helper'
+import { ExpressionTree } from '../expression/expression-tree'
+import { getParameterTrees } from '../parameter/parameter-tree-helper'
+import { ParameterTree } from '../parameter/parameter-tree'
+import { Tree } from '../tree'
 
 export class DefinitionTree extends Tree {
-  metadata: DefinitionDeclarationMetadata;
-  ctx: DefinitionContext;
-  sourceRange: SourceRange;
-  modifier: IdToken;
-  name: IdToken;
-  parameters: ParameterTree[];
-  base?: ExpressionTree | None;
-  attributes: AttributeTree[];
+  metadata: DefinitionDeclarationMetadata
+  ctx: DefinitionContext
+  sourceRange: SourceRange
+  modifier: IdToken
+  name: IdToken
+  parameters: ParameterTree[]
+  base?: (ExpressionTree | None)
+  attributes: AttributeTree[]
 
   constructor(ctx: DefinitionContext) {
-    super();
-    this.ctx = ctx;
-    this.sourceRange = SourceRange.fromContext(ctx);
-    this.modifier = getIdToken(ctx._modifier);
-    this.name = getIdToken(ctx._name);
-    this.parameters = getParameterTrees(ctx.parameters()?.parameter()) || none;
-    this.base = getExpressionTree(ctx.expression()) || none;
-    this.attributes = getAttributeTrees(ctx.attribute());
+    super()
+    this.ctx = ctx
+    this.sourceRange = SourceRange.fromContext(ctx)
+    this.modifier = getIdToken(ctx._modifier)
+    this.name = getIdToken(ctx._name)
+    this.parameters = getParameterTrees(ctx.parameters()?.parameter()) || none
+    this.base = getExpressionTree(ctx.expression()) || none
+    this.attributes = getAttributeTrees(ctx.attribute())
   }
 
   toString(): String {
-    let modifier, base, parameters, properties, methodsWithBody, methodsWithNoBody, attributes;
-    modifier = (this.modifier && this.modifier.text + ' ') || '';
-    base = (this.base && ' ' + this.base) || '';
-    parameters = (this.ctx.parameters() && `(${this.parameters.join(', ')})`) || '';
-    properties = this.attributes.filter((x) => !x.isMethod).join('\n');
-    methodsWithBody = this.attributes.filter((x) => x.isMethod && x.body).join('\n\n');
-    methodsWithNoBody = this.attributes.filter((x) => x.isMethod && !x.body).join('\n');
-    attributes = [properties, methodsWithBody, methodsWithNoBody]
-      .filter(Boolean)
-      .join('\n\n')
-      .replace(/^(.+)/gm, '  $1');
-    return modifier + this.name + parameters + base + ((attributes && '\n' + attributes) || '');
+    let modifier, base, parameters, properties, methodsWithBody, methodsWithNoBody, attributes
+    modifier = this.modifier.text
+    base = (this.base && ' ' + this.base) || ''
+    parameters = this.ctx.parameters() && `(${this.parameters.join(', ')})` || ''
+    properties = this.attributes.filter((x) => !x.isMethod).join('\n')
+    methodsWithBody = this.attributes.filter((x) => x.isMethod && x.body).join('\n\n')
+    methodsWithNoBody = this.attributes.filter((x) => x.isMethod && !x.body).join('\n')
+    attributes = [properties, methodsWithBody, methodsWithNoBody].filter(x=>x).join('\n\n').replace(/^(.+)/gm, '  $1')
+    return (modifier + this.name + parameters + base + ((attributes && '\n' + attributes) || ''))
   }
 }
 
--- src/tree/source/source-tree.ts
+++ src/tree/source/source-tree.ts
@@ -1,48 +1,67 @@
 // this code was generated
 
-import { SourceContext } from '../../grammar/xon-parser'
-import { String } from '../../lib/core'
-import { SourceRange } from '../../util/source-range'
-import { getAttributeTrees } from '../attribute/attribute-tree-helper'
-import { AttributeTree } from '../attribute/attribute-tree'
-import { getDefinitionTrees } from '../definition/definition-tree-helper'
-import { DefinitionTree } from '../definition/definition-tree'
-import { ImportStatementTree } from '../statement/import/import-statement-tree'
-import { getStatementTrees } from '../statement/statement-tree-helper'
-import { StatementTree } from '../statement/statement-tree'
-import { Tree } from '../tree'
+import { SourceContext } from '../../grammar/xon-parser';
+import { String } from '../../lib/core';
+import { SourceRange } from '../../util/source-range';
+import { AttributeTree } from '../attribute/attribute-tree';
+import { getAttributeTrees } from '../attribute/attribute-tree-helper';
+import { DefinitionTree } from '../definition/definition-tree';
+import { getDefinitionTrees } from '../definition/definition-tree-helper';
+import { ImportStatementTree } from '../statement/import/import-statement-tree';
+import { StatementTree } from '../statement/statement-tree';
+import { getStatementTrees } from '../statement/statement-tree-helper';
+import { Tree } from '../tree';
 
 export class SourceTree extends Tree {
-  ctx: SourceContext
-  sourceRange: SourceRange
-  statements: StatementTree[]
-  attributes: AttributeTree[]
-  definitions: DefinitionTree[]
+  ctx: SourceContext;
+  sourceRange: SourceRange;
+  statements: StatementTree[];
+  attributes: AttributeTree[];
+  definitions: DefinitionTree[];
 
   constructor(ctx: SourceContext) {
-    super()
-    this.ctx = ctx
-    this.sourceRange = SourceRange.fromContext(ctx)
-    this.statements = getStatementTrees(ctx.statement())
-    this.attributes = getAttributeTrees(ctx.attribute())
-    this.definitions = getDefinitionTrees(ctx.definition())
+    super();
+    this.ctx = ctx;
+    this.sourceRange = SourceRange.fromContext(ctx);
+    this.statements = getStatementTrees(ctx.statement());
+    this.attributes = getAttributeTrees(ctx.attribute());
+    this.definitions = getDefinitionTrees(ctx.definition());
   }
 
   toString(): String {
-    let importStatements, importStatementsMap, uniqueImportStatements, otherStatements, attributes, definitions, result
-    importStatements = this.statements.filter((x) => x instanceof ImportStatementTree).map((x) => x as ImportStatementTree)
-    importStatementsMap = {}
+    let importStatements,
+      importStatementsMap,
+      uniqueImportStatements,
+      otherStatements,
+      attributes,
+      definitions,
+      result;
+    importStatements = this.statements
+      .filter((x) => x instanceof ImportStatementTree)
+      .map((x) => x as ImportStatementTree);
+    importStatementsMap = {};
     for (let importStatement of importStatements) {
-      importStatementsMap[importStatement.path.toString()] = importStatementsMap[importStatement.path.toString()] || []
-      const members = importStatement.members.map(x => x.toString())
-      importStatementsMap[importStatement.path.toString()].push(...members)
+      importStatementsMap[importStatement.path.toString()] =
+        importStatementsMap[importStatement.path.toString()] || [];
+      const members = importStatement.members.map((x) => x.toString());
+      importStatementsMap[importStatement.path.toString()].push(...members);
     }
-    uniqueImportStatements = Object.keys(importStatementsMap).sort((a, b) => a.localeCompare(b)).map((x) => `import ${x}: ${[...new Set(importStatementsMap[x].sort((a, b) => a.localeCompare(b)))].join(', ')}`).join('\n')
-    otherStatements = this.statements.filter((x) => !(x instanceof ImportStatementTree)).join('\n')
-    attributes = this.attributes.join('\n\n')
-    definitions = this.definitions.join('\n\n')
-    result = [uniqueImportStatements, otherStatements, attributes, definitions].filter(Boolean).join('\n\n')
-    return (result && result + '\n') || ''
+    uniqueImportStatements = Object.keys(importStatementsMap)
+      .sort((a, b) => a.localeCompare(b))
+      .map(
+        (x) =>
+          `import ${x}: ${[
+            ...new Set(importStatementsMap[x].sort((a, b) => a.localeCompare(b))),
+          ].join(', ')}`,
+      )
+      .join('\n');
+    otherStatements = this.statements.filter((x) => !(x instanceof ImportStatementTree)).join('\n');
+    attributes = this.attributes.join('\n\n');
+    definitions = this.definitions.join('\n\n');
+    result = [uniqueImportStatements, otherStatements, attributes, definitions]
+      .filter((x) => x)
+      .join('\n\n');
+    return (result && result + '\n') || '';
   }
 }
 
