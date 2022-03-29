b8f64181af6936618666380a0df43c70f5d25f6c
nizami <nizami.dev@gmail.com>
2022-03-29 18:18:39 +0300

    Update attribute formatter

--- src/formatter/attibute/attribute-formatter.test.ts
+++ src/formatter/attibute/attribute-formatter.test.ts
@@ -4,11 +4,10 @@ import { AttributeFormatter } from './attribute-formatter';
 import { getAttributeFormatter } from './attribute-formatter-helper';
 
 test('id type', () => {
-  const code = 'a 123';
+  const code = 'abc (a,b,c) Number= 123';
   const ctx = parse(code).attribute();
   const formatter = getAttributeFormatter(ctx, defaultFormatterConfig);
 
   expect(formatter).toBeInstanceOf(AttributeFormatter);
-  // expect(formatter.toString()).toBe('a 123');
-  // expect(formatter.indent(2).toString()).toBe('    a 123');
+  expect(formatter.toString()).toBe('abc(a, b, c) Number = 123');
 });
--- src/formatter/attibute/attribute-formatter.ts
+++ src/formatter/attibute/attribute-formatter.ts
@@ -1,6 +1,9 @@
 import { AttributeContext } from '../../grammar/xon-parser';
+import { getBodyFormatter } from '../body/body-formatter-helper';
+import { getExpressionFormatter } from '../expression/expression-formatter-helper';
 import { Formatter } from '../formatter';
 import { FormatterConfig } from '../formatter-config';
+import { getParametersFormatter } from '../parameters/parameters-formatter-helper';
 
 export class AttributeFormatter extends Formatter {
   constructor(public ctx: AttributeContext, public config: FormatterConfig) {
@@ -8,6 +11,16 @@ export class AttributeFormatter extends Formatter {
   }
 
   toString() {
-    return '';
+    const name = this.ctx._name.text;
+    const parameters =
+      getParametersFormatter(this.ctx.parameters(), this.config)?.indent(this.indentCount) || '';
+    const type = getExpressionFormatter(this.ctx.expression(), this.config)?.indent(
+      this.indentCount,
+    );
+    const body = getBodyFormatter(this.ctx.body(), this.config).indent(this.indentCount) || '';
+    if (type) {
+      return `${name}${parameters} ${type}${body}`;
+    }
+    return `${name}${parameters}${body}`;
   }
 }
