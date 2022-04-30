ec53a145ba7a30cf7d5b763d72b18688c6a147ef
nizami <nizami.dev@gmail.com>
2022-04-30 18:04:50 +0300

    Update statement tree

--- src/tree/statement/statement-tree.ts
+++ src/tree/statement/statement-tree.ts
@@ -1,7 +1,10 @@
 // this code was generated
 
+import { StatementMetadata } from '../../metadata/statement/statement-metadata'
 import { Tree } from '../tree'
 
-export class StatementTree extends Tree {}
+export class StatementTree extends Tree {
+  metadata: StatementMetadata
+}
 
 // this code was generated
