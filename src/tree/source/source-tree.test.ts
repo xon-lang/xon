78125410766f55504f73a83187072c4fb4d56ae7
nizami <nizami.dev@gmail.com>
2022-05-14 09:19:11 +0300

    Update source tree tests

--- src/tree/source/source-tree.test.ts
+++ src/tree/source/source-tree.test.ts
@@ -1,12 +1,9 @@
 import * as fs from 'fs';
 import { parseSource, parseSourceFile } from '../../util/parse';
-import { IdExpressionTree } from '../expression/id/id-expression-tree';
-import { LiteralExpressionTree } from '../expression/literal/literal-expression-tree';
-import { ImportStatementTree } from '../statement/import/import-statement-tree';
 import { SourceTree } from './source-tree';
 
 test('two if', () => {
-  const code = `import 'xon.os': Path
+  const code = `{Path}: import 'xon.os'
 
 1+1
 if e
@@ -18,13 +15,6 @@ else
   const tree = parseSource(code);
 
   expect(tree).toBeInstanceOf(SourceTree);
-  const imports = tree.statements
-    .filter((x) => x instanceof ImportStatementTree)
-    .map((x) => x as ImportStatementTree);
-  expect(imports.length).toBe(1);
-  expect((imports[0].path as LiteralExpressionTree).literal.value).toBe('xon.os');
-  expect(imports[0].members.length).toBe(1);
-  expect((imports[0].members[0] as IdExpressionTree).name.text).toBe('Path');
 });
 
 test('formatted 1.xon', () => {
@@ -34,25 +24,6 @@ test('formatted 1.xon', () => {
   const formatted = tree.toString();
   fs.writeFileSync('src/tree/source/test-files/1.fmt.xon', formatted);
   // expect(code).toBe(formatted);
-});
-
-test('import sort and unique', () => {
-  const code = `
-import 'xon.os': Path, Path
-import 'xon.os': Path, Path, m2
-import 'xon.os2': Path, Path, Path2
-import 'xon.os': Path
-
-`.trim();
-  const tree = parseSource(code);
-
-  expect(tree).toBeInstanceOf(SourceTree);
-  expect(tree.toString()).toBe(
-    `
-import 'xon.os': m2, Path
-import 'xon.os2': Path, Path2
-`.trim() + '\n',
-  );
 });
 
 test('preprocessor in attribute', () => {
