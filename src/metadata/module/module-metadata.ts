b7000c7a6faf52134631820d1874f5bc7fd612d1
nizami <nizami.dev@gmail.com>
2022-05-14 09:55:13 +0300

    Update metadata module

--- src/metadata/module/module-metadata.ts
+++ src/metadata/module/module-metadata.ts
@@ -1,20 +1,14 @@
-import * as glob from 'glob';
-import * as path from 'path';
 import { String } from '../../lib/core';
 import { parseSourceFile } from '../../util/parse';
-import { DeclarationMetadata } from '../declaration/declaration-metadata';
 import { DeclarationScope } from '../scope/declaration-scope';
 import { getSourceMetadata } from '../source/source-metadata-helper';
 
 export class ModuleMetadata {
-  declarations: DeclarationMetadata[] = [];
-
-  constructor(moduleDir: String) {
+  constructor(files: String[]) {
     const scope = new DeclarationScope();
-    const globPath = path.resolve(moduleDir, '**/*.xon');
-    const trees = glob.sync(globPath).map((x) => parseSourceFile(x));
+    const sourceTrees = files.map((x) => parseSourceFile(x));
 
-    for (const tree of trees) {
+    for (const tree of sourceTrees) {
       tree.metadata = getSourceMetadata(tree, scope);
     }
   }
