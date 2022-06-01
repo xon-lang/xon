475c0b8a541db74ca9328fc7698a8d8f5196a049
nizami <nizami.dev@gmail.com>
2022-06-01 17:05:57 +0300

    Update metadata scopes

--- src/metadata/scope/core-declaration-scope.test.ts
+++
@@ -1,6 +0,0 @@
-import { CoreDeclarationScope } from './core-declaration-scope';
-
-test('none type', () => {
-  const coreScope = new CoreDeclarationScope();
-  expect(coreScope.none).toBeTruthy();
-});
--- src/metadata/scope/test-declaration-scope.ts
+++
@@ -1,9 +0,0 @@
-import { CoreDeclarationScope } from './core-declaration-scope';
-import { DeclarationScope } from './declaration-scope';
-
-export class TestDeclarationScope extends DeclarationScope {
-  constructor() {
-    DeclarationScope._core = new CoreDeclarationScope();
-    super(DeclarationScope._core);
-  }
-}
