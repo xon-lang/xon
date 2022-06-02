56e93bb7bb4252a6d32c9381f7c12955bfa2352c
nizami <nizami.dev@gmail.com>
2022-06-02 10:52:20 +0300

    Fix literal type expression

--- src/metadata/expression/type/literal/literal-type-expression-metadata.test.ts
+++ src/metadata/expression/type/literal/literal-type-expression-metadata.test.ts
@@ -10,27 +10,27 @@ test('integer literal', () => {
   expect(metadata.attributesScope().parameters.length).toBe(1);
 });
 
-// test('float literal', () => {
-//   const code = '1.23';
-//   const tree = parseExpression(code);
-//   const metadata = getTypeExpressionMetadata(tree, new TestDeclarationScope());
+test('float literal', () => {
+  const code = '1.23';
+  const tree = parseExpression(code);
+  const metadata = getTypeExpressionMetadata(tree, new TestDeclarationScope());
 
-//   expect(metadata.declaration().length).toBe(1);
-// });
+  expect(metadata.attributesScope().parameters.length).toBe(2);
+});
 
-// test('7 is integer', () => {
-//   const code = '7';
-//   const tree = parseExpression(code);
-//   const metadata = getTypeExpressionMetadata(tree, new TestDeclarationScope());
+test('7 is integer', () => {
+  const code = '7';
+  const tree = parseExpression(code);
+  const metadata = getTypeExpressionMetadata(tree, new TestDeclarationScope());
 
-//   const codeInteger = 'Number';
-//   const treeInteger = parseExpression(codeInteger);
-//   const metadataInteger = getTypeExpressionMetadata(treeInteger, new TestDeclarationScope());
+  const codeInteger = 'Number';
+  const treeInteger = parseExpression(codeInteger);
+  const metadataInteger = getTypeExpressionMetadata(treeInteger, new TestDeclarationScope());
 
-//   const codeFloat = 'Float';
-//   const treeFloat = parseExpression(codeFloat);
-//   const metadataFloat = getTypeExpressionMetadata(treeFloat, new TestDeclarationScope());
+  const codeFloat = 'Float';
+  const treeFloat = parseExpression(codeFloat);
+  const metadataFloat = getTypeExpressionMetadata(treeFloat, new TestDeclarationScope());
 
-//   expect(metadata.is(metadataInteger)).toBe(true);
-//   expect(metadata.is(metadataFloat)).toBe(false);
-// });
+  expect(metadata.is(metadataInteger)).toBe(true);
+  expect(metadata.is(metadataFloat)).toBe(false);
+});
--- src/metadata/expression/type/literal/literal-type-expression-metadata.ts
+++ src/metadata/expression/type/literal/literal-type-expression-metadata.ts
@@ -4,7 +4,9 @@ import { FloatLiteralTree } from '../../../../tree/literal/float/float-literal-t
 import { IntegerLiteralTree } from '../../../../tree/literal/integer/integer-literal-tree';
 import { LiteralType } from '../../../../tree/literal/literal-type';
 import { StringLiteralTree } from '../../../../tree/literal/string/string-literal-tree';
+import { DefinitionMetadata } from '../../../declaration/definition/definition-metadata';
 import { DeclarationScope } from '../../../declaration/scope/declaration-scope';
+import { IdTypeExpressionMetadata } from '../id/id-type-expression-metadata';
 import { TypeExpressionMetadata } from '../type-expression-metadata';
 
 export class LiteralTypeExpressionMetadata extends TypeExpressionMetadata {
@@ -16,21 +18,22 @@ export class LiteralTypeExpressionMetadata extends TypeExpressionMetadata {
     this.value = tree.literal.value;
   }
 
+  definition(): DefinitionMetadata {
+    if (this.tree.literal instanceof IntegerLiteralTree) return this.scope.core.integer;
+    if (this.tree.literal instanceof FloatLiteralTree) return this.scope.core.float;
+    if (this.tree.literal instanceof StringLiteralTree) return this.scope.core.string;
+  }
+
   attributesScope(): DeclarationScope {
-    if (this.tree.literal instanceof IntegerLiteralTree)
-      return this.scope.core.integer.attributesScope();
-    if (this.tree.literal instanceof FloatLiteralTree)
-      return this.scope.core.float.attributesScope();
-    if (this.tree.literal instanceof StringLiteralTree)
-      return this.scope.core.string.attributesScope();
+    return this.definition().attributesScope();
   }
 
   is(metadata: TypeExpressionMetadata): Boolean {
     if (metadata instanceof LiteralTypeExpressionMetadata && this.value === metadata.value)
       return true;
-    // if (metadata instanceof IdTypeExpressionMetadata) {
-    //   if(this.tree.literal instanceof IntegerLiteralTree && metadata.declaration().)
-    // }
+    if (metadata instanceof IdTypeExpressionMetadata) {
+      return this.definition().is(metadata.definition());
+    }
 
     throw new Error('Not implemented');
   }
