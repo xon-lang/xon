ecb891cf9df711f861f7c448af52dafb99350136
nizami <nizami.dev@gmail.com>
2022-06-02 10:17:44 +0300

    Update expression metadata helper

--- src/metadata/expression/type/type-expression-metadata-helper.ts
+++ src/metadata/expression/type/type-expression-metadata-helper.ts
@@ -1,8 +1,10 @@
 import { Issue } from '../../../issue-service/issue';
 import { ExpressionTree } from '../../../tree/expression/expression-tree';
 import { GroupExpressionTree } from '../../../tree/expression/group/group-expression-tree';
+import { IdExpressionTree } from '../../../tree/expression/id/id-expression-tree';
 import { LiteralExpressionTree } from '../../../tree/expression/literal/literal-expression-tree';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { IdTypeExpressionMetadata } from './id/id-type-expression-metadata';
 import { LiteralTypeExpressionMetadata } from './literal/literal-type-expression-metadata';
 import { TypeExpressionMetadata } from './type-expression-metadata';
 
@@ -10,14 +12,9 @@ export function getTypeExpressionMetadata(
   tree: ExpressionTree,
   scope: DeclarationScope,
 ): TypeExpressionMetadata {
-  try {
-    if (tree instanceof GroupExpressionTree)
-      return getTypeExpressionMetadata(tree.expression, scope);
-    if (tree instanceof LiteralExpressionTree)
-      return new LiteralTypeExpressionMetadata(tree, scope);
+  if (tree instanceof GroupExpressionTree) return getTypeExpressionMetadata(tree.expression, scope);
+  if (tree instanceof LiteralExpressionTree) return new LiteralTypeExpressionMetadata(tree, scope);
+  if (tree instanceof IdExpressionTree) return new IdTypeExpressionMetadata(tree, scope);
 
-    throw `Type expression metadata not found for '${tree.constructor.name}'`;
-  } catch (error) {
-    Issue.errorFromTree(tree, error.toString());
-  }
+  Issue.errorFromTree(tree, `Type expression metadata not found for '${tree.constructor.name}'`);
 }
