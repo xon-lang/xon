876a81da713cb197c7fe2d2691dda9e551a861ab
nizami <nizami.dev@gmail.com>
2022-06-03 18:59:34 +0300

    Update id value metadata

--- src/metadata/value/id/id-value-metadata.test.ts
+++ src/metadata/value/id/id-value-metadata.test.ts
@@ -1,56 +1,14 @@
 import { parseExpression } from '../../../util/parse';
-import { DefinitionDeclarationMetadata } from '../../declaration/definition/definition-declaration-metadata';
-import { TestDeclarationScope } from '../../declaration/test-declaration-metadata';
+import { TestDeclarationScope } from '../../declaration/scope/test-declaration-scope';
 import { getValueMetadata } from '../value-metadata-helper';
 import { IdValueMetadata } from './id-value-metadata';
 
 test('none model', () => {
   const code = 'None';
   const tree = parseExpression(code);
-  const metadata = getValueMetadata(tree, new TestDeclarationScope()) as IdValueMetadata;
-
-  expect(metadata).toBeInstanceOf(IdValueMetadata);
-  expect(metadata.declaration()).toBeInstanceOf(DefinitionDeclarationMetadata);
-  expect(metadata.declaration().name).toBe('None');
-  expect(metadata.declaration().length).toBe(0);
-});
-
-test('none object', () => {
-  const code = 'none';
-  const tree = parseExpression(code);
-  const metadata = getValueMetadata(tree, new TestDeclarationScope()) as IdValueMetadata;
-
-  expect(metadata).toBeInstanceOf(IdValueMetadata);
-  expect(metadata.declaration()).toBeInstanceOf(DefinitionDeclarationMetadata);
-  expect(metadata.declaration().name).toBe('none');
-  expect(metadata.declaration().length).toBe(0);
-});
-
-test('any', () => {
-  const code = 'Any';
-  const tree = parseExpression(code);
-  const metadata = getValueMetadata(tree, new TestDeclarationScope()) as IdValueMetadata;
-
-  expect(metadata).toBeInstanceOf(IdValueMetadata);
-  expect(metadata.declaration()).toBeInstanceOf(DefinitionDeclarationMetadata);
-  expect(metadata.declaration().name).toBe('Any');
-  expect(metadata.declaration().length).toBe(0);
-});
-
-test('none is None', () => {
-  const code = 'none';
-  const tree = parseExpression(code);
   const scope = new TestDeclarationScope();
-  const metadata = getValueMetadata(tree, scope);
+  const metadata = getValueMetadata(tree, scope) as IdValueMetadata;
 
-  const codeNone = 'None';
-  const treeNone = parseExpression(codeNone);
-  const metadataNone = getValueMetadata(treeNone, scope);
-
-  const codeFloat = 'Float';
-  const treeFloat = parseExpression(codeFloat);
-  const metadataFloat = getValueMetadata(treeFloat, scope);
-
-  expect(metadata.is(metadataNone)).toBe(true);
-  expect(metadata.is(metadataFloat)).toBe(false);
+  expect(metadata).toBeInstanceOf(IdValueMetadata);
+  expect(metadata.is(scope.core.none)).toBe(true);
 });
--- src/metadata/value/id/id-value-metadata.ts
+++ src/metadata/value/id/id-value-metadata.ts
@@ -1,9 +1,27 @@
+import { Any, Boolean } from '../../../lib/core';
 import { IdExpressionTree } from '../../../tree/expression/id/id-expression-tree';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { TypeMetadata } from '../../type/type-metadata';
 import { ValueMetadata } from '../value-metadata';
 
 export class IdValueMetadata extends ValueMetadata {
   constructor(private tree: IdExpressionTree, private scope: DeclarationScope) {
     super();
+  }
+
+  parameter() {
+    return this.scope.findByName(this.tree.name.text);
+  }
+
+  type(): TypeMetadata {
+    return this.parameter().type();
+  }
+
+  is(metadata: TypeMetadata): Boolean {
+    return this.type().is(metadata);
+  }
+
+  eval(): Any {
+    return this.parameter().value()?.eval();
   }
 }
