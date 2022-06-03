b18f356f8dc8b55d859fbcda248b999dc18db1dd
nizami <nizami.dev@gmail.com>
2022-06-03 15:16:45 +0300

    Update literal type expression metadata

--- src/metadata/expression/type/literal/literal-type-expression-metadata.ts
+++ src/metadata/expression/type/literal/literal-type-expression-metadata.ts
@@ -1,27 +1,13 @@
 import { Boolean } from '../../../../lib/core';
-import { LiteralExpressionTree } from '../../../../tree/expression/literal/literal-expression-tree';
-import { FloatLiteralTree } from '../../../../tree/literal/float/float-literal-tree';
-import { IntegerLiteralTree } from '../../../../tree/literal/integer/integer-literal-tree';
 import { LiteralType } from '../../../../tree/literal/literal-type';
-import { StringLiteralTree } from '../../../../tree/literal/string/string-literal-tree';
 import { DefinitionMetadata } from '../../../declaration/definition/definition-metadata';
 import { DeclarationScope } from '../../../declaration/scope/declaration-scope';
 import { IdTypeExpressionMetadata } from '../id/id-type-expression-metadata';
 import { TypeExpressionMetadata } from '../type-expression-metadata';
 
 export class LiteralTypeExpressionMetadata extends TypeExpressionMetadata {
-  value: LiteralType;
-
-  constructor(private tree: LiteralExpressionTree, private scope: DeclarationScope) {
+  constructor(public value: LiteralType, public definition: () => DefinitionMetadata) {
     super();
-
-    this.value = tree.literal.value;
-  }
-
-  definition(): DefinitionMetadata {
-    if (this.tree.literal instanceof IntegerLiteralTree) return this.scope.core.integer;
-    if (this.tree.literal instanceof FloatLiteralTree) return this.scope.core.float;
-    if (this.tree.literal instanceof StringLiteralTree) return this.scope.core.string;
   }
 
   attributesScope(): DeclarationScope {
