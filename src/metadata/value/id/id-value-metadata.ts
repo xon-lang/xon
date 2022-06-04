416e4e440b0bce2e503585c141240323aec45e9c
nizami <nizami.dev@gmail.com>
2022-06-04 11:30:53 +0300

    Update id value metadata

--- src/metadata/value/id/id-value-metadata.ts
+++ src/metadata/value/id/id-value-metadata.ts
@@ -1,6 +1,10 @@
 import { Any, Boolean } from '../../../lib/core';
 import { IdExpressionTree } from '../../../tree/expression/id/id-expression-tree';
+import { DefinitionMetadata } from '../../declaration/definition/definition-metadata';
+import { ParameterMetadata } from '../../declaration/parameter/parameter-metadata';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { DefinitionTypeMetadata } from '../../type/definition/definition-type-metadata';
+import { MethodTypeMetadata } from '../../type/method/method-type-metadata';
 import { TypeMetadata } from '../../type/type-metadata';
 import { ValueMetadata } from '../value-metadata';
 
@@ -9,12 +13,19 @@ export class IdValueMetadata extends ValueMetadata {
     super();
   }
 
-  parameter() {
-    return this.scope.findByName(this.tree.name.text);
+  declaration() {
+    return this.scope.find(this.tree.name.text);
   }
 
   type(): TypeMetadata {
-    return this.parameter().type();
+    const declaration = this.declaration();
+    if (declaration instanceof ParameterMetadata) return declaration.type();
+    if (declaration instanceof DefinitionMetadata) {
+      if (declaration.modifier === 'object') {
+        const result = () => new DefinitionTypeMetadata(() => declaration);
+        return new MethodTypeMetadata(declaration.parameters, result);
+      }
+    }
   }
 
   is(metadata: TypeMetadata): Boolean {
@@ -22,6 +33,9 @@ export class IdValueMetadata extends ValueMetadata {
   }
 
   eval(): Any {
-    return this.parameter().value()?.eval();
+    const declaration = this.declaration();
+    if (declaration instanceof ParameterMetadata) return declaration.value()?.eval();
+
+    throw new Error('Not implemented');
   }
 }
