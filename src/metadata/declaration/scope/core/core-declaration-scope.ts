0a0a1b4d2b5264fb935b8d47937464a89cabb3c3
nizami <nizami.dev@gmail.com>
2022-06-04 11:36:00 +0300

    Update core declaration scope

--- src/metadata/declaration/scope/core/core-declaration-scope.ts
+++ src/metadata/declaration/scope/core/core-declaration-scope.ts
@@ -1,5 +1,6 @@
 import { ImportProvider } from '../../../import-provider';
 import { DefinitionMetadata } from '../../definition/definition-metadata';
+import { ParameterMetadata } from '../../parameter/parameter-metadata';
 import { DeclarationScope } from '../declaration-scope';
 
 export class CoreDeclarationScope extends DeclarationScope {
@@ -8,6 +9,12 @@ export class CoreDeclarationScope extends DeclarationScope {
   }
   get boolean(): DefinitionMetadata {
     return this.find('Boolean') as DefinitionMetadata;
+  }
+  get true(): ParameterMetadata {
+    return this.find('true') as ParameterMetadata;
+  }
+  get false(): ParameterMetadata {
+    return this.find('false') as ParameterMetadata;
   }
   get integer(): DefinitionMetadata {
     return this.find('Integer') as DefinitionMetadata;
