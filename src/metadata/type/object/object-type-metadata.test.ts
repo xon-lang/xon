d90cabb0bc4e09fa8ad60f0d06e83a4424836e70
nizami <nizami.dev@gmail.com>
2022-06-05 14:08:53 +0300

    Add object type metadata tests

--- src/metadata/type/object/object-type-metadata.test.ts
+++ src/metadata/type/object/object-type-metadata.test.ts
@@ -12,3 +12,25 @@ test('object', () => {
   expect(metadata).toBeInstanceOf(ObjectTypeMetadata);
   expect(metadata.attributesScope().declarations.length).toBe(3);
 });
+
+test('object is other one', () => {
+  const code = '{a = 1, b = 2, c = 3}';
+  const tree = parseExpression(code);
+  const scope = new TestDeclarationScope();
+  const metadata = getTypeMetadata(tree, scope) as ObjectTypeMetadata;
+
+  const code2 = '{a = 1, b = 1, c = 1}';
+  const tree2 = parseExpression(code2);
+  const scope2 = new TestDeclarationScope();
+  const metadata2 = getTypeMetadata(tree2, scope2) as ObjectTypeMetadata;
+
+  const code3 = '{x = 1, b = 2, c = 3}';
+  const tree3 = parseExpression(code3);
+  const scope3 = new TestDeclarationScope();
+  const metadata3 = getTypeMetadata(tree3, scope3) as ObjectTypeMetadata;
+
+  expect(metadata).toBeInstanceOf(ObjectTypeMetadata);
+  expect(metadata.attributesScope().declarations.length).toBe(3);
+  expect(metadata.is(metadata2)).toBe(true);
+  expect(metadata.is(metadata3)).toBe(false);
+});
