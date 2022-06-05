855a3fbf3d0083cdc181b1f230274ad0aad91957
nizami <nizami.dev@gmail.com>
2022-06-05 14:08:08 +0300

    Update literal value metadata

--- src/metadata/value/literal/literal-value-metadata.ts
+++ src/metadata/value/literal/literal-value-metadata.ts
@@ -1,9 +1,19 @@
 import { LiteralExpressionTree } from '../../../tree/expression/literal/literal-expression-tree';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { TypeMetadata } from '../../type/type-metadata';
+import { getTypeMetadata } from '../../type/type-metadata-helper';
 import { ValueMetadata } from '../value-metadata';
 
 export class LiteralValueMetadata extends ValueMetadata {
   constructor(private tree: LiteralExpressionTree, private scope: DeclarationScope) {
     super();
+  }
+
+  type(): TypeMetadata {
+    return getTypeMetadata(this.tree, this.scope);
+  }
+
+  eval() {
+    return this.tree.literal.value;
   }
 }
