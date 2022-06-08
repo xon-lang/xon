eba47e289d99212ed948cdccd17e27111dfa5daf
nizami <nizami.dev@gmail.com>
2022-06-08 09:45:33 +0300

    Update import value metadata tests

--- src/metadata/value/import/import-value-metadata.test.ts
+++ src/metadata/value/import/import-value-metadata.test.ts
@@ -1,5 +1,6 @@
 import { parseExpression } from '../../../util/parse';
 import { TestDeclarationScope } from '../../declaration/scope/test-declaration-scope';
+import { ObjectTypeMetadata } from '../../type/object/object-type-metadata';
 import { getValueMetadata } from '../value-metadata-helper';
 import { ImportValueMetadata } from './import-value-metadata';
 
@@ -11,4 +12,6 @@ test('literal', () => {
 
   expect(metadata).toBeInstanceOf(ImportValueMetadata);
   expect(metadata.importScope().declarations.length).toBe(19);
+  expect(metadata.type()).toBeInstanceOf(ObjectTypeMetadata);
+  expect(metadata.type().attributesScope().declarations.length).toBe(19);
 });
