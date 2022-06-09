30d9f42f13c9c1a11be7dfa6071d710c405eb6da
nizami <nizami.dev@gmail.com>
2022-06-09 09:33:41 +0300

    Update declaration metadata

--- src/metadata/declaration/declaration-metadata.ts
+++ src/metadata/declaration/declaration-metadata.ts
@@ -1,9 +1,14 @@
+import { Boolean } from '../../lib/core';
 import { SourceRange } from '../../util/source-range';
 import { Metadata } from '../metadata';
 import { TypeMetadata } from '../type/type-metadata';
 
 export abstract class DeclarationMetadata extends Metadata {
-    abstract sourceRange: SourceRange
-    abstract name: String
-    abstract type(): TypeMetadata
+  abstract sourceRange: SourceRange;
+  abstract name: String;
+  abstract type(): TypeMetadata;
+
+  equals(other: DeclarationMetadata): Boolean {
+    return this.sourceRange.equals(other.sourceRange);
+  }
 }
