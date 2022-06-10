e2fc097c0b17c484be0fcc8ee6af0341e75c1510
nizami <nizami.dev@gmail.com>
2022-06-10 13:31:40 +0300

    Fix literal value metadata tests

--- src/metadata/value/literal/literal-value-metadata.test.ts
+++ src/metadata/value/literal/literal-value-metadata.test.ts
@@ -1,36 +1,36 @@
 import { parseExpression } from '../../../util/parse';
-import { TestDeclarationScope } from '../../declaration/test-declaration-metadata';
-import { getExpressionMetadata } from '../value-metadata-helper';
+import { TestDeclarationScope } from '../../declaration/scope/test-declaration-scope';
+import { getValueMetadata } from '../value-metadata-helper';
 
 test('integer literal', () => {
   const code = '123';
   const tree = parseExpression(code);
-  const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
+  const metadata = getValueMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.declaration().length).toBe(1);
+  expect(metadata.type().attributesScope().declarations.length).toBe(1);
 });
 
 test('float literal', () => {
   const code = '1.23';
   const tree = parseExpression(code);
-  const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
+  const metadata = getValueMetadata(tree, new TestDeclarationScope());
 
-  expect(metadata.declaration().length).toBe(1);
+  expect(metadata.type().attributesScope().declarations.length).toBe(2);
 });
 
 test('7 is integer', () => {
   const code = '7';
   const tree = parseExpression(code);
-  const metadata = getExpressionMetadata(tree, new TestDeclarationScope());
+  const metadata = getValueMetadata(tree, new TestDeclarationScope());
 
   const codeInteger = 'Number';
   const treeInteger = parseExpression(codeInteger);
-  const metadataInteger = getExpressionMetadata(treeInteger, new TestDeclarationScope());
+  const metadataInteger = getValueMetadata(treeInteger, new TestDeclarationScope());
 
   const codeFloat = 'Float';
   const treeFloat = parseExpression(codeFloat);
-  const metadataFloat = getExpressionMetadata(treeFloat, new TestDeclarationScope());
+  const metadataFloat = getValueMetadata(treeFloat, new TestDeclarationScope());
 
-  expect(metadata.is(metadataInteger)).toBe(true);
-  expect(metadata.is(metadataFloat)).toBe(false);
+  expect(metadata.type().is(metadataInteger.type())).toBe(true);
+  expect(metadata.type().is(metadataFloat.type())).toBe(false);
 });
