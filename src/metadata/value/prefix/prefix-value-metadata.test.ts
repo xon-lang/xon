bc16a2bfc226faf3653d913f1cafd4816c8dda43
nizami <nizami.dev@gmail.com>
2022-06-10 13:39:18 +0300

    Update prefix value metadata

--- src/metadata/value/prefix/prefix-value-metadata.test.ts
+++ src/metadata/value/prefix/prefix-value-metadata.test.ts
@@ -1,12 +1,14 @@
 import { parseExpression } from '../../../util/parse';
-import { TestDeclarationScope } from '../../declaration/test-declaration-metadata';
+import { TestDeclarationScope } from '../../declaration/scope/test-declaration-scope';
 import { getValueMetadata } from '../value-metadata-helper';
 import { PrefixValueMetadata } from './prefix-value-metadata';
 
 test('prefix', () => {
   const code = '+1';
   const tree = parseExpression(code);
-  const metadata = getValueMetadata(tree, new TestDeclarationScope());
+  const scope = new TestDeclarationScope();
+  const metadata = getValueMetadata(tree, scope);
 
   expect(metadata).toBeInstanceOf(PrefixValueMetadata);
+  expect(metadata.type().equals(scope.core.number.type())).toBe(true);
 });
--- src/metadata/value/prefix/prefix-value-metadata.ts
+++ src/metadata/value/prefix/prefix-value-metadata.ts
@@ -1,17 +1,44 @@
+import { Any, Unknown } from '../../../lib/core';
 import { PrefixExpressionTree } from '../../../tree/expression/prefix/prefix-expression-tree';
+import { ParameterMetadata } from '../../declaration/parameter/parameter-metadata';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { MethodTypeMetadata } from '../../type/method/method-type-metadata';
+import { TypeMetadata } from '../../type/type-metadata';
 import { ValueMetadata } from '../value-metadata';
+import { getValueMetadata } from '../value-metadata-helper';
 
 export class PrefixValueMetadata extends ValueMetadata {
-  constructor(tree: PrefixExpressionTree, scope: DeclarationScope) {
+  constructor(private tree: PrefixExpressionTree, private scope: DeclarationScope) {
     super();
-    // const declaration = getExpressionMetadata(tree.value, scope).type.declaration;
-    // const attributeType = declaration.attribute(tree.id.text, [], [], none).type([]);
-    // if (attributeType instanceof LambdaTypeMetadata) this.type = attributeType.resultType;
-    // else throw new Error('Wrong method type');
+    tree.value.metadata = getValueMetadata(tree.value, scope);
   }
 
-  declaration(): DeclarationScope {
-    throw new Error('Method not implemented.');
+  operatorDeclaration(): ParameterMetadata {
+    const declaration = this.scope.find(this.tree.name.text, (x) => {
+      if (!(x instanceof ParameterMetadata)) return false;
+
+      const type = x.type();
+      if (!(type instanceof MethodTypeMetadata)) return false;
+
+      const parameters = type.parameters();
+      if (parameters.length !== 1) return false;
+
+      return this.tree.value.metadata.type().is(parameters[0].type());
+    });
+    return declaration as ParameterMetadata;
+  }
+
+  type(): TypeMetadata {
+    const operatorDeclarationType = this.operatorDeclaration().type();
+    if (operatorDeclarationType instanceof MethodTypeMetadata) {
+      return operatorDeclarationType.resultType();
+    }
+    throw new Error('Not implemented');
+  }
+
+  eval(): Any {
+    const value = this.tree.value.metadata.eval();
+    const escapeIfString = (s: Unknown) => (typeof s === 'string' && `\`${s}\``) || s;
+    return eval(`${this.tree.name} ${escapeIfString(value)}`);
   }
 }
