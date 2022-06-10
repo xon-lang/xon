fe865c2a3def4753841a8286134c6e387f460e97
nizami <nizami.dev@gmail.com>
2022-06-10 13:25:14 +0300

    Update method value metadata

--- src/metadata/value/method/method-value-metadata.test.ts
+++ src/metadata/value/method/method-value-metadata.test.ts
@@ -1,12 +1,20 @@
 import { parseExpression } from '../../../util/parse';
-import { TestDeclarationScope } from '../../declaration/test-declaration-metadata';
+import { TestDeclarationScope } from '../../declaration/scope/test-declaration-scope';
+import { MethodTypeMetadata } from '../../type/method/method-type-metadata';
 import { getValueMetadata } from '../value-metadata-helper';
 import { MethodValueMetadata } from './method-value-metadata';
 
 test('lambda', () => {
-  const code = '[x String] =>x + x';
+  const code = '(x: Number) => x + x';
   const tree = parseExpression(code);
-  const metadata = getValueMetadata(tree, new TestDeclarationScope());
+  const scope = new TestDeclarationScope();
+  const metadata = getValueMetadata(tree, scope);
 
   expect(metadata).toBeInstanceOf(MethodValueMetadata);
+  const type = metadata.type() as MethodTypeMetadata;
+  expect(type).toBeInstanceOf(MethodTypeMetadata);
+  expect(type.parameters().length).toBe(1);
+  expect(type.parameters()[0].name).toBe('x');
+  expect(type.parameters()[0].type().is(scope.core.number.type())).toBe(true);
+  expect(type.resultType().is(scope.core.number.type())).toBe(true);
 });
--- src/metadata/value/method/method-value-metadata.ts
+++ src/metadata/value/method/method-value-metadata.ts
@@ -1,19 +1,29 @@
+import { Any } from '../../../lib/core';
 import { MethodExpressionTree } from '../../../tree/expression/method/method-expression-tree';
 import { getParameterMetadata } from '../../declaration/parameter/parameter-metadata-helper';
 import { DeclarationScope } from '../../declaration/scope/declaration-scope';
+import { MethodTypeMetadata } from '../../type/method/method-type-metadata';
+import { TypeMetadata } from '../../type/type-metadata';
 import { ValueMetadata } from '../value-metadata';
 import { getValueMetadata } from '../value-metadata-helper';
 
 export class MethodValueMetadata extends ValueMetadata {
-  parameters: DeclarationScope[];
-  value: ValueMetadata;
-
-  constructor(tree: MethodExpressionTree, scope: DeclarationScope) {
+  constructor(private tree: MethodExpressionTree, private scope: DeclarationScope) {
     super();
+    tree.parameters.forEach((x) => (x.metadata = getParameterMetadata(x, scope)[0]));
     const innerScope = scope.create();
-    for (const parameter of tree.parameters) {
-      getParameterMetadata(parameter, scope);
-    }
-    this.value = getValueMetadata(tree.value, scope);
+    tree.parameters.forEach((x) => innerScope.add(x.metadata));
+    tree.value.metadata = getValueMetadata(tree.value, innerScope);
+  }
+
+  type(): TypeMetadata {
+    return new MethodTypeMetadata(
+      () => this.tree.parameters.map((x) => x.metadata),
+      () => this.tree.value.metadata.type(),
+    );
+  }
+
+  eval(): Any {
+    throw new Error('Not implemented');
   }
 }
