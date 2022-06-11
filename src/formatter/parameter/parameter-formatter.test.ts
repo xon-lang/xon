4333ce8d19f601338a8c91956c9d1ada79d7ed7d
nizami <nizami.dev@gmail.com>
2022-06-11 09:16:07 +0300

    Update parameter formatter

--- src/formatter/parameter/parameter-formatter.test.ts
+++ src/formatter/parameter/parameter-formatter.test.ts
@@ -12,3 +12,13 @@ test('id type', () => {
   expect(formatter.toString()).toBe('a: 123');
   expect(formatter.indent(2).toString()).toBe('a: 123');
 });
+
+test('parameters', () => {
+  const code = '{a: Number = 0, b: Number, c = 0}: ABC = [1, 2, 3]';
+  const ctx = parse(code).parameter();
+  const formatter = getParameterFormatter(ctx, defaultFormatterConfig);
+
+  expect(formatter).toBeInstanceOf(ParameterFormatter);
+  expect(formatter.toString()).toBe('{a: Number = 0, b: Number, c = 0}: ABC = [1, 2, 3]');
+  expect(formatter.indent(2).toString()).toBe('{a: Number = 0, b: Number, c = 0}: ABC = [1, 2, 3]');
+});
--- src/formatter/parameter/parameter-formatter.ts
+++ src/formatter/parameter/parameter-formatter.ts
@@ -1,8 +1,15 @@
-import { ParameterContext } from '../../grammar/xon-parser';
+import {
+  BodyContext,
+  ExpressionContext,
+  ParameterContext,
+  SingleBodyContext,
+} from '../../grammar/xon-parser';
+import { String } from '../../lib/core';
 import { getBodyFormatter } from '../body/body-formatter-helper';
 import { getExpressionFormatter } from '../expression/expression-formatter-helper';
 import { Formatter } from '../formatter';
 import { FormatterConfig } from '../formatter-config';
+import { getParametersFormatter } from '../parameters/parameters-formatter-helper';
 
 export class ParameterFormatter extends Formatter {
   constructor(public ctx: ParameterContext, public config: FormatterConfig) {
@@ -10,20 +17,30 @@ export class ParameterFormatter extends Formatter {
   }
 
   toString() {
-    const typeSign = (this.ctx.COLON() && ':') || '';
-    const type =
-      (this.ctx._type &&
-        ' ' +
-          getExpressionFormatter(this.ctx._type, this.config)
-            .indent(this.indentCount)
-            .toString()
-            .trim()) ||
-      '';
-    let body =
-      (this.ctx.body() &&
-        ((type && ' ') || '') +
-          getBodyFormatter(this.ctx.body(), this.config).indent(this.indentCount)) ||
-      '';
-    return `${this.ctx._name.text}${typeSign}${type}${body}`;
+    const type = this.typeFormatter(this.ctx._type);
+    let body = this.bodyFormat(this.ctx.body(), this.ctx._type);
+
+    if (this.ctx._name) return `${this.ctx._name.text}${type}${body}`;
+
+    const parameters = getParametersFormatter(this.ctx.parameters(), this.config);
+    return `${parameters}${type}${body}`;
+  }
+
+  typeFormatter(type: ExpressionContext): String {
+    if (!type) return '';
+
+    return (
+      ': ' +
+      getExpressionFormatter(this.ctx._type, this.config).indent(this.indentCount).toString().trim()
+    );
+  }
+
+  bodyFormat(body: BodyContext, type: ExpressionContext): String {
+    if (!body) return '';
+
+    const bodyFormatter = getBodyFormatter(body, this.config).indent(this.indentCount);
+    if (body instanceof SingleBodyContext) return ' ' + bodyFormatter;
+
+    return bodyFormatter.toString();
   }
 }
