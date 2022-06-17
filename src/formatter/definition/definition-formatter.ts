b3c8d309d5e9a4ccc54640fd32cbc6c0ae40a19d
nizami <nizami.dev@gmail.com>
2022-06-17 19:29:05 +0300

    Update definition formatter

--- src/formatter/definition/definition-formatter.ts
+++ src/formatter/definition/definition-formatter.ts
@@ -1,8 +1,8 @@
-import { DefinitionContext, MethodExpressionContext } from '../../grammar/xon-parser';
+import { DefinitionContext } from '../../grammar/xon-parser';
+import { getBodyFormatter } from '../body/body-formatter-helper';
 import { getExpressionFormatter } from '../expression/expression-formatter-helper';
 import { Formatter } from '../formatter';
 import { FormatterConfig } from '../formatter-config';
-import { getParameterFormatter } from '../parameter/parameter-formatter-helper';
 import { getParametersFormatter } from '../parameters/parameters-formatter-helper';
 
 export class DefinitionFormatter extends Formatter {
@@ -18,37 +18,14 @@ export class DefinitionFormatter extends Formatter {
     const type = getExpressionFormatter(this.ctx.expression(), this.config)?.indent(
       this.indentCount,
     );
-    const attributeIndent = this.config.indent(this.indentCount + 1);
-    let attributes = this.ctx.parameter();
-    const properties = attributes
-      .filter((x) => !(x instanceof MethodExpressionContext))
-      .map(
-        (x) => attributeIndent + getParameterFormatter(x, this.config).indent(this.indentCount + 1),
-      )
-      .join(this.config.nl);
 
-    const methodsWithBody = attributes
-      .filter((x) => x instanceof MethodExpressionContext && x.body())
-      .map(
-        (x) => attributeIndent + getParameterFormatter(x, this.config).indent(this.indentCount + 1),
-      )
-      .join(this.config.nl2);
-
-    const methodsWithNoBody = attributes
-      .filter((x) => x instanceof MethodExpressionContext && !x.body())
-      .map(
-        (x) => attributeIndent + getParameterFormatter(x, this.config).indent(this.indentCount + 1),
-      )
-      .join(this.config.nl);
-
-    let attributesString = [properties, methodsWithBody, methodsWithNoBody]
-      .filter((x) => x)
-      .join(this.config.nl2);
-    attributesString = (attributesString && this.config.nl + attributesString) || '';
-
+    const body = getBodyFormatter(this.ctx.body(), this.config)?.indent(this.indentCount) || '';
+    let result = '';
     if (type) {
-      return `${modifier} ${name}${parameters} is ${type}${attributesString}`;
+      result = `${modifier} ${name}${parameters} is ${type}${body}`;
+    } else {
+      result = `${modifier} ${name}${parameters}${body}`;
     }
-    return `${modifier} ${name}${parameters}${attributesString}`;
+    return result;
   }
 }
