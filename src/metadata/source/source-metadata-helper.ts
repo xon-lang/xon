a8e07067e01d4377a947ed1477030f4955d0e7dd
nizami <nizami.dev@gmail.com>
2022-06-24 08:46:38 +0300

    Update source metadata helper

--- src/metadata/source/source-metadata-helper.ts
+++ src/metadata/source/source-metadata-helper.ts
@@ -1,12 +1,7 @@
-import { Issue } from '../../issue-service/issue';
 import { SourceTree } from '../../tree/source/source-tree';
 import { DeclarationScope } from '../declaration/scope/declaration-scope';
 import { SourceMetadata } from './source-metadata';
 
 export function getSourceMetadata(tree: SourceTree, scope: DeclarationScope): SourceMetadata {
-  try {
-    return new SourceMetadata(tree, scope);
-  } catch (error) {
-    Issue.errorFromTree(tree, error.toString());
-  }
+  return new SourceMetadata(tree, scope);
 }
