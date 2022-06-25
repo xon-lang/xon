a2b93b27db4bbde19d6e8ee15d3662811c222ca3
nizami <nizami.dev@gmail.com>
2022-06-25 09:39:09 +0300

    Add body property to definition tree

--- src/tree/definition/definition-tree.ts
+++ src/tree/definition/definition-tree.ts
@@ -1,8 +1,6 @@
 // this code was generated
 
-import {
-  DefinitionContext,
-} from '../../grammar/xon-parser';
+import { DefinitionContext } from '../../grammar/xon-parser';
 import { Issue } from '../../issue-service/issue';
 import { none, None, String } from '../../lib/core';
 import { DefinitionMetadata } from '../../metadata/declaration/definition/definition-metadata';
@@ -10,14 +8,14 @@ import { SourceRange } from '../../util/source-range';
 import { ExpressionTree } from '../expression/expression-tree';
 import { getExpressionTree } from '../expression/expression-tree-helper';
 import { MethodExpressionTree } from '../expression/method/method-expression-tree';
-import { getIdTree } from '../id/id-tree-helper';
 import { IdTree } from '../id/id-tree';
+import { getIdTree } from '../id/id-tree-helper';
 import { ParameterTree } from '../parameter/parameter-tree';
 import { getParameterTrees } from '../parameter/parameter-tree-helper';
-import { ParameterStatementTree } from '../statement/parameter/parameter-statement-tree';
-import { getStatementTrees } from '../statement/statement-tree-helper';
-import { Tree } from '../tree';
+import { SourceTree } from '../source/source-tree';
 import { getSourceTree } from '../source/source-tree-helper';
+import { ParameterStatementTree } from '../statement/parameter/parameter-statement-tree';
+import { Tree } from '../tree';
 
 export class DefinitionTree extends Tree {
   metadata: DefinitionMetadata;
@@ -27,6 +25,7 @@ export class DefinitionTree extends Tree {
   name: IdTree;
   parameters: ParameterTree[];
   base?: ExpressionTree | None;
+  body?: SourceTree;
   attributes: ParameterTree[];
 
   constructor(ctx: DefinitionContext) {
@@ -38,14 +37,15 @@ export class DefinitionTree extends Tree {
     this.parameters = getParameterTrees(ctx.parameters()?.parameter()) || none;
     this.base = getExpressionTree(ctx.expression()) || none;
 
-    const statements = getSourceTree(ctx.body()?.source())?.statements || [];
+    this.body = getSourceTree(ctx.body()?.source());
+    const statements = this.body?.statements || [];
     statements
       .filter((x) => !(x instanceof ParameterStatementTree))
       .forEach((x) => Issue.errorFromTree(x, 'Definition body should contain only parameters'));
     this.attributes = statements
       .filter((x) => x instanceof ParameterStatementTree)
       .map((x) => (x as ParameterStatementTree).parameter);
-    this.addChildren(this.modifier, this.name, ...this.parameters, this.base, ...this.attributes)
+    this.addChildren(this.modifier, this.name, ...this.parameters, this.base, ...this.attributes);
   }
 
   toString(): String {
