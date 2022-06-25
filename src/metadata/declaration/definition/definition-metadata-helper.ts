6168a7e55484cac553d14f0905adb48ce3727cf2
nizami <nizami.dev@gmail.com>
2022-06-25 09:36:44 +0300

    Update definition declaration metadata

--- src/metadata/declaration/definition/definition-metadata-helper.ts
+++ src/metadata/declaration/definition/definition-metadata-helper.ts
@@ -2,6 +2,7 @@ import { Issue } from '../../../issue-service/issue';
 import { None, none } from '../../../lib/core';
 import { DefinitionTree } from '../../../tree/definition/definition-tree';
 import { IdExpressionTree } from '../../../tree/expression/id/id-expression-tree';
+import { getSourceMetadata } from '../../source/source-metadata-helper';
 import { getParameterMetadata } from '../parameter/parameter-metadata-helper';
 import { DeclarationScope } from '../scope/declaration-scope';
 import { DefinitionMetadata } from './definition-metadata';
@@ -11,23 +12,27 @@ export function getDefinitionMetadata(
   scope: DeclarationScope,
 ): DefinitionMetadata {
   if (tree instanceof DefinitionTree) {
-    const parameters = () => tree.parameters.map((x) => getParameterMetadata(x, scope)).flat();
-    let baseDefinition: () => DefinitionMetadata | None = () => none;
+    const parameters = tree.parameters.map((x) => getParameterMetadata(x, scope)).flat();
+    let baseDefinition: DefinitionMetadata | None = none;
     if (tree.base instanceof IdExpressionTree) {
-      tree.base.metadata = () => scope.find(tree.base['name'].text) as DefinitionMetadata;
-      baseDefinition = () => scope.find(tree.base['name'].text) as DefinitionMetadata;
+      tree.base.metadata = scope.find(tree.base['name'].text) as DefinitionMetadata;
+      baseDefinition = scope.find(tree.base['name'].text) as DefinitionMetadata;
     } else if (tree.base) {
       throw new Error('Not implemented');
     }
-    const attributes = () => tree.attributes.map((x) => getParameterMetadata(x, scope)).flat();
-    const metadata = new DefinitionMetadata(
-      tree.sourceRange,
-      tree.modifier.text,
-      tree.name.text,
-      parameters,
-      baseDefinition,
-      attributes,
-    );
+
+    const metadata = scope.find(tree.name.text, (x) =>
+      x.sourceRange.equals(tree.sourceRange),
+    ) as DefinitionMetadata;
+    metadata.parameters = parameters;
+    metadata.baseDefinition = baseDefinition;
+
+    const innerScope = scope.create();
+    if (tree.body) {
+      tree.body.metadata = getSourceMetadata(tree.body, innerScope);
+      metadata.attributes = tree.attributes.map((x) => getParameterMetadata(x, innerScope)).flat();
+    }
+
     return metadata;
   }
 
--- src/metadata/declaration/definition/definition-metadata.ts
+++ src/metadata/declaration/definition/definition-metadata.ts
@@ -6,47 +6,40 @@ import { ParameterMetadata } from '../parameter/parameter-metadata';
 import { DeclarationScope } from '../scope/declaration-scope';
 
 export class DefinitionMetadata extends DeclarationMetadata {
+  type = new DefinitionTypeMetadata(this);
+
+  parameters: ParameterMetadata[];
+  baseDefinition: DefinitionMetadata | None;
+  attributes: ParameterMetadata[];
+
   private scope: DeclarationScope;
 
-  constructor(
-    public sourceRange: SourceRange,
-    public modifier: String,
-    public name: String,
-    public parameters: () => ParameterMetadata[],
-    public baseDefinition: () => DefinitionMetadata | None,
-    public attributes: () => ParameterMetadata[],
-  ) {
+  constructor(public sourceRange: SourceRange, public modifier: String, public name: String) {
     super();
   }
 
-  type(): DefinitionTypeMetadata {
-    return new DefinitionTypeMetadata(() => this);
-  }
-
   attributesScope(): DeclarationScope {
-    if (this.scope) return this.scope;
+    if (this.scope) {
+      return this.scope;
+    }
 
-    const baseDefinition = this.baseDefinition();
-    if (baseDefinition) {
-      this.scope = baseDefinition.attributesScope().create();
+    if (this.baseDefinition) {
+      this.scope = this.baseDefinition.attributesScope().create();
     } else {
       this.scope = new DeclarationScope();
     }
-    this.attributes().forEach((x) => this.scope.add(x));
+    this.attributes.forEach((x) => this.scope.add(x));
     return this.scope;
   }
 
   allAttributes(): ParameterMetadata[] {
-    const attributes = this.attributes();
-    const baseDefinition = this.baseDefinition();
-
-    return [...(baseDefinition?.allAttributes() || []), ...attributes];
+    return [...(this.baseDefinition?.allAttributes() || []), ...this.attributes];
   }
 
   is(definition: DefinitionMetadata): Boolean {
-    if (this.sourceRange.equals(definition.sourceRange)) return true;
-
-    const baseDefinition = this.baseDefinition();
-    return baseDefinition?.is(definition) || false;
+    if (this.sourceRange.equals(definition.sourceRange)) {
+      return true;
+    }
+    return this.baseDefinition?.is(definition) || false;
   }
 }
