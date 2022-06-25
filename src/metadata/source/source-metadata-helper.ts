9390309ed233aa96496833808f783cb1d5fe7895
nizami <nizami.dev@gmail.com>
2022-06-25 09:38:07 +0300

    Update source metadata

--- src/metadata/source/source-metadata-helper.ts
+++ src/metadata/source/source-metadata-helper.ts
@@ -2,6 +2,10 @@ import { SourceTree } from '../../tree/source/source-tree';
 import { DeclarationScope } from '../declaration/scope/declaration-scope';
 import { SourceMetadata } from './source-metadata';
 
-export function getSourceMetadata(tree: SourceTree, scope: DeclarationScope): SourceMetadata {
-  return new SourceMetadata(tree, scope);
+export function getSourceMetadata(
+  tree: SourceTree,
+  scope: DeclarationScope,
+  deepRun = true,
+): SourceMetadata {
+  return new SourceMetadata(tree, scope, deepRun);
 }
--- src/metadata/source/source-metadata.ts
+++ src/metadata/source/source-metadata.ts
@@ -1,13 +1,47 @@
 import { SourceTree } from '../../tree/source/source-tree';
+import { DefinitionStatementTree } from '../../tree/statement/definition/definition-statement-tree';
+import { OperatorStatementTree } from '../../tree/statement/operator/operator-statement-tree';
+import { ParameterStatementTree } from '../../tree/statement/parameter/parameter-statement-tree';
+import { DefinitionMetadata } from '../declaration/definition/definition-metadata';
+import { ParameterMetadata } from '../declaration/parameter/parameter-metadata';
 import { DeclarationScope } from '../declaration/scope/declaration-scope';
 import { getStatementMetadata } from '../statement/statement-metadata-helper';
 
 export class SourceMetadata {
-  constructor(tree: SourceTree, scope: DeclarationScope) {
-    // const innerScope = scope.create();
-
+  constructor(tree: SourceTree, scope: DeclarationScope, deepRun = true) {
     for (const statement of tree.statements) {
-      statement.metadata = getStatementMetadata(statement, scope);
+      if (statement instanceof ParameterStatementTree) {
+        if (statement.parameter.name) {
+          const metadata = new ParameterMetadata(
+            statement.parameter.sourceRange,
+            statement.parameter.name.text,
+          );
+          scope.add(metadata);
+        } else {
+          for (const parameter of statement.parameter.parameters) {
+            const metadata = new ParameterMetadata(parameter.sourceRange, parameter.name.text);
+            scope.add(metadata);
+          }
+        }
+      }
+      if (statement instanceof OperatorStatementTree) {
+        const metadata = new ParameterMetadata(statement.sourceRange, statement.name.text);
+        scope.add(metadata);
+      }
+      if (statement instanceof DefinitionStatementTree) {
+        const metadata = new DefinitionMetadata(
+          statement.definition.sourceRange,
+          statement.definition.modifier.text,
+          statement.definition.name.text,
+        );
+        scope.add(metadata);
+      }
+    }
+
+    if (deepRun) {
+      for (const statement of tree.statements) {
+        statement.metadata = getStatementMetadata(statement, scope);
+      }
     }
   }
 }
