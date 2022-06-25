ced221c55bda04788a53c1ccb4d2b210843ec40c
nizami <nizami.dev@gmail.com>
2022-06-25 09:39:17 +0300

    Refactoring

--- src/tree/expression/expression-tree.ts
+++ src/tree/expression/expression-tree.ts
@@ -4,7 +4,7 @@ import { Metadata } from '../../metadata/metadata';
 import { Tree } from '../tree';
 
 export abstract class ExpressionTree extends Tree {
-  metadata: () => Metadata;
+  metadata: Metadata;
 }
 
 // this code was generated
