6a4a258fcbdc1a8b45d5fca6adca6d4a1658e7dc
nizami <nizami.dev@gmail.com>
2022-07-02 13:47:11 +0300

    Update definition metadata test

--- src/metadata/declaration/definition/definition-metadata.test.ts
+++ src/metadata/declaration/definition/definition-metadata.test.ts
@@ -1,16 +1,16 @@
-import { parseStatement } from '../../../util/parse';
-import { DeclarationStatementMetadata } from '../../statement/declaration/declaration-statement-metadata';
-import { getStatementMetadata } from '../../statement/statement-metadata-helper';
+import { parseSource } from '../../../util/parse';
+import { SourceMetadata } from '../../source/source-metadata';
+import { getSourceMetadata } from '../../source/source-metadata-helper';
 import { TestDeclarationScope } from '../scope/test-declaration-scope';
 import { DefinitionMetadata } from './definition-metadata';
 
 test('definition', () => {
   const code = 'model A';
-  const tree = parseStatement(code);
+  const tree = parseSource(code);
   const scope = new TestDeclarationScope();
-  const metadata = getStatementMetadata(tree, scope);
+  const metadata = getSourceMetadata(tree, scope);
 
-  expect(metadata).toBeInstanceOf(DeclarationStatementMetadata);
+  expect(metadata).toBeInstanceOf(SourceMetadata);
   expect(scope.declarations.length).toBe(1);
   expect(scope.declarations[0]).toBeInstanceOf(DefinitionMetadata);
   expect(scope.declarations[0].name).toBe('A');
