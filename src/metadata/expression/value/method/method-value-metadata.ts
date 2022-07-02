c4ef9be82be0d451d2e73201a40b416f82b7aac6
nizami <nizami.dev@gmail.com>
2022-07-02 19:51:00 +0300

    Update method value metadata

--- src/metadata/expression/value/method/method-value-metadata.ts
+++ src/metadata/expression/value/method/method-value-metadata.ts
@@ -1,6 +1,9 @@
 import { Any } from '../../../../lib/core';
 import { MethodExpressionTree } from '../../../../tree/expression/method/method-expression-tree';
-import { getParameterMetadata } from '../../../declaration/parameter/parameter-metadata-helper';
+import {
+  fillParameterMetadata,
+  getParameterMetadata,
+} from '../../../declaration/parameter/parameter-metadata-helper';
 import { DeclarationScope } from '../../../declaration/scope/declaration-scope';
 import { MethodTypeMetadata } from '../../type/method/method-type-metadata';
 import { TypeMetadata } from '../../type/type-metadata';
@@ -10,9 +13,10 @@ import { getValueMetadata } from '../value-metadata-helper';
 export class MethodValueMetadata extends ValueMetadata {
   constructor(private tree: MethodExpressionTree, private scope: DeclarationScope) {
     super();
-    tree.parameters.forEach((x) => (x.metadata = getParameterMetadata(x, scope)));
     const innerScope = scope.create();
+    tree.parameters.forEach((x) => (x.metadata = getParameterMetadata(x, innerScope)));
     tree.parameters.forEach((x) => innerScope.add(x.metadata));
+    tree.parameters.forEach((x) => fillParameterMetadata(x));
     tree.value.metadata = getValueMetadata(tree.value, innerScope);
   }
 
