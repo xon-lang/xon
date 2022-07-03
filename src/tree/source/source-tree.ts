a8ef2bc5725132d7f039be315478c1908bb04f00
nizami <nizami.dev@gmail.com>
2022-07-03 10:09:50 +0300

    Update source tree

--- src/tree/source/source-tree.ts
+++ src/tree/source/source-tree.ts
@@ -1,32 +1,35 @@
 // this code was generated
 
-import { SourceContext } from '../../grammar/xon-parser'
-import { String } from '../../lib/core'
-import { SourceRange } from '../../util/source-range'
-import { getStatementTrees } from '../statement/statement-tree-helper'
-import { StatementTree } from '../statement/statement-tree'
-import { Tree } from '../tree'
-import { SourceMetadata } from '../../metadata/source/source-metadata'
+import { SourceContext } from '../../grammar/xon-parser';
+import { String } from '../../lib/core';
+import { SourceMetadata } from '../../metadata/source/source-metadata';
+import { SourceRange } from '../../util/source-range';
+import { StatementTree } from '../statement/statement-tree';
+import { getStatementTree, getStatementTrees } from '../statement/statement-tree-helper';
+import { Tree } from '../tree';
 
 export class SourceTree extends Tree {
-  metadata: SourceMetadata
-  ctx: SourceContext
-  sourceRange: SourceRange
-  statements: StatementTree[]
+  metadata: SourceMetadata;
+  ctx: SourceContext;
+  sourceRange: SourceRange;
+  statements: StatementTree[];
 
   constructor(ctx: SourceContext) {
-    super()
-    this.ctx = ctx
-    this.sourceRange = SourceRange.fromContext(ctx)
-    this.statements = getStatementTrees(ctx.statement())
-    this.addChildren(...this.statements)
+    super();
+    this.ctx = ctx;
+    this.sourceRange = SourceRange.fromContext(ctx);
+    this.statements = [
+      ...getStatementTrees(ctx.sourceItem().map((x) => x.statement())),
+      getStatementTree(ctx.statement()),
+    ];
+    this.addChildren(...this.statements);
   }
 
   toString(): String {
-    let statements, result
-    statements = this.statements.join('\n')
-    result = [statements].filter((x) => x).join('\n\n')
-    return (result && result + '\n') || ''
+    let statements, result;
+    statements = this.statements.join('\n');
+    result = [statements].filter((x) => x).join('\n\n');
+    return (result && result + '\n') || '';
   }
 }
 
