0e752a3138c9a5a5684bf3bfb91c749a5074efd8
nizami <nizami.dev@gmail.com>
2022-07-03 14:25:16 +0300

    Update declaration scope

--- src/metadata/declaration/scope/declaration-scope.ts
+++ src/metadata/declaration/scope/declaration-scope.ts
@@ -4,49 +4,12 @@ import { Boolean, none } from '../../../lib/core';
 import { DefinitionTree } from '../../../tree/definition/definition-tree';
 import { ParameterTree } from '../../../tree/parameter/parameter-tree';
 import { DeclarationMetadata } from '../declaration-metadata';
-import { DefinitionMetadata } from '../definition/definition-metadata';
-import { ParameterMetadata } from '../parameter/parameter-metadata';
 import { CoreDeclarationScope } from './core/core-declaration-scope';
 
 export class DeclarationScope {
   static _core: CoreDeclarationScope;
   get core() {
-    const self = this;
-    return {
-      get any(): DefinitionMetadata {
-        return self.find('Any') as DefinitionMetadata;
-      },
-      get boolean(): DefinitionMetadata {
-        return self.find('Boolean') as DefinitionMetadata;
-      },
-      get true(): ParameterMetadata {
-        return self.find('true') as ParameterMetadata;
-      },
-      get false(): ParameterMetadata {
-        return self.find('false') as ParameterMetadata;
-      },
-      get integer(): DefinitionMetadata {
-        return self.find('Integer') as DefinitionMetadata;
-      },
-      get float(): DefinitionMetadata {
-        return self.find('Float') as DefinitionMetadata;
-      },
-      get number(): DefinitionMetadata {
-        return self.find('Number') as DefinitionMetadata;
-      },
-      get none(): DefinitionMetadata {
-        return self.find('None') as DefinitionMetadata;
-      },
-      get char(): DefinitionMetadata {
-        return self.find('Char') as DefinitionMetadata;
-      },
-      get string(): DefinitionMetadata {
-        return self.find('String') as DefinitionMetadata;
-      },
-      get array(): DefinitionMetadata {
-        return self.find('Array') as DefinitionMetadata;
-      },
-    };
+    return DeclarationScope._core;
   }
 
   declarations: DeclarationMetadata[] = [];
--- src/metadata/import-provider.ts
+++ src/metadata/import-provider.ts
@@ -1,6 +1,8 @@
 import { lstatSync } from 'fs';
 import { glob } from 'glob';
+import os from 'os';
 import path from 'path';
+import { String } from '../lib/core';
 import { parseSourceFile } from '../util/parse';
 import { DeclarationScope } from './declaration/scope/declaration-scope';
 import { getSourceMetadata } from './source/source-metadata-helper';
@@ -9,9 +11,11 @@ export class ImportProvider {
   constructor(private importPath: string) {}
 
   scope(): DeclarationScope {
-    const fullImportPath = path.resolve(this.importPath);
+    const fullImportPath = resolvePath(this.importPath);
     const stats = lstatSync(fullImportPath);
-    if (!stats.isDirectory()) throw new Error('Should be a directory');
+    if (!stats.isDirectory()) {
+      throw new Error('Should be a directory');
+    }
 
     const globPath = path.resolve(fullImportPath, '*.xon');
     const files = glob.sync(globPath);
@@ -27,4 +31,11 @@ export class ImportProvider {
     }
     return scope;
   }
+}
+
+function resolvePath(importPath: String): String {
+  if (importPath[0] === '~') {
+    return path.join(os.homedir(), importPath.slice(1));
+  }
+  return importPath;
 }
