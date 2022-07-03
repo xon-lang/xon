9bed7ea1014aca48a372f514da07ff6dcae6e9c2
nizami <nizami.dev@gmail.com>
2022-07-03 15:34:36 +0300

    Update parameter metadata test

--- src/metadata/declaration/parameter/parameter-metadata.test.ts
+++ src/metadata/declaration/parameter/parameter-metadata.test.ts
@@ -1,12 +1,16 @@
+import { ParameterTree } from '../../../tree/parameter/parameter-tree';
+import { DeclarationStatementTree } from '../../../tree/statement/declaration/declaration-statement-tree';
 import { parseSource } from '../../../util/parse';
 import { LiteralTypeMetadata } from '../../expression/type/literal/literal-type-metadata';
+import { TypeMetadata } from '../../expression/type/type-metadata';
+import { ValueMetadata } from '../../expression/value/value-metadata';
 import { SourceMetadata } from '../../source/source-metadata';
 import { getSourceMetadata } from '../../source/source-metadata-helper';
 import { TestDeclarationScope } from '../scope/test-declaration-scope';
 import { ParameterMetadata } from './parameter-metadata';
 
 test('single parameter', () => {
-  const code = 'a: 132';
+  const code = 'a: Number =  132';
   const tree = parseSource(code);
   const scope = new TestDeclarationScope();
   const metadata = getSourceMetadata(tree, scope);
@@ -14,6 +18,12 @@ test('single parameter', () => {
   expect(metadata).toBeInstanceOf(SourceMetadata);
   expect(scope.declarations.length).toBe(1);
   expect(scope.find('a').name).toBe('a');
+
+  const parameter = (tree.statements[0] as DeclarationStatementTree).declaration as ParameterTree;
+  const valueType = (parameter.value.metadata as ValueMetadata).type() as LiteralTypeMetadata;
+  expect(valueType.definition.name).toBe('Integer');
+  expect(valueType.is(scope.core.number.type)).toBe(true);
+  expect((parameter.type.metadata as TypeMetadata).equals(scope.core.number.type)).toBe(true);
 });
 
 test('multiple parameters array value', () => {
