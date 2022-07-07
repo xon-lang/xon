d10fed38e9e6ffcbf4088aacc24aec28037f3e5d
nizami <nizami.dev@gmail.com>
2022-07-07 05:38:23 +0300

    Refactoring

--- src/metadata/expression/type/type-metadata-helper.ts
+++ src/metadata/expression/type/type-metadata-helper.ts
@@ -1,4 +1,6 @@
 import { Issue } from '../../../issue-service/issue';
+import { IssueLevel } from '../../../issue-service/issue-level';
+import { none } from '../../../lib/core';
 import { ArrayExpressionTree } from '../../../tree/expression/array/array-expression-tree';
 import { ExpressionTree } from '../../../tree/expression/expression-tree';
 import { GroupExpressionTree } from '../../../tree/expression/group/group-expression-tree';
@@ -32,22 +34,40 @@ export function getTypeMetadata(tree: ExpressionTree, scope: DeclarationScope): 
   if (tree instanceof GroupExpressionTree) return getTypeMetadata(tree.expression, scope);
   if (tree instanceof LiteralExpressionTree) {
     let definition: DefinitionMetadata;
-    if (tree.literal instanceof IntegerLiteralTree) definition = scope.core.integer;
-    else if (tree.literal instanceof FloatLiteralTree) definition = scope.core.float;
-    else if (tree.literal instanceof StringLiteralTree) definition = scope.core.string;
+    if (tree.literal instanceof IntegerLiteralTree) {
+      definition = scope.core.integer;
+    } else if (tree.literal instanceof FloatLiteralTree) {
+      definition = scope.core.float;
+    } else if (tree.literal instanceof StringLiteralTree) {
+      definition = scope.core.string;
+    }
     return new LiteralTypeMetadata(tree.literal.value, definition);
   }
 
   if (tree instanceof IdExpressionTree) {
-    const declaration = scope.find(tree.name.text);
-    if (declaration instanceof ParameterMetadata) return new ParameterTypeMetadata(declaration);
-    if (declaration instanceof DefinitionMetadata) return new DefinitionTypeMetadata(declaration);
+    const declarations = scope.filter(tree.name.text);
+    if (declarations.length === 1) {
+      if (declarations[0] instanceof ParameterMetadata) {
+        return new ParameterTypeMetadata(declarations[0]);
+      }
+      if (declarations[0] instanceof DefinitionMetadata) {
+        return new DefinitionTypeMetadata(declarations[0]);
+      }
+    } else if (declarations.length > 0) {
+      tree.name.addError('Too many declarations');
+    } else {
+      tree.name.addError('No declarations found');
+    }
   }
   if (tree instanceof InfixExpressionTree) {
     const left = getTypeMetadata(tree.left, scope);
     const right = getTypeMetadata(tree.right, scope);
-    if (tree.name.text === 'or') return new UnionTypeMetadata(left, right);
-    if (tree.name.text === 'and') return new IntersectionTypeMetadata(left, right);
+    if (tree.name.text === 'or') {
+      return new UnionTypeMetadata(left, right);
+    }
+    if (tree.name.text === 'and') {
+      return new IntersectionTypeMetadata(left, right);
+    }
 
     throw new Error('Not implemented');
   }
@@ -74,16 +94,18 @@ export function getTypeMetadata(tree: ExpressionTree, scope: DeclarationScope): 
 
   if (tree instanceof ArrayExpressionTree) {
     if (tree.ctx.arguments().OPEN_BRACE()) {
-      const noNameArgument = tree.arguments.find((x) => !x.name);
-      if (noNameArgument) Issue.errorFromTree(noNameArgument, 'No name argument');
       const objectScope = new DeclarationScope();
       const parameters = tree.arguments.map((x) => {
-        const metadata = new ParameterMetadata(x.name.text, x.sourceRange, scope);
+        if (!x.name) {
+          x.addIssue(IssueLevel.error, 'No name argument');
+          return none;
+        }
+        const metadata = new ParameterMetadata(x.name, scope);
         metadata.type = getValueMetadata(x.value, scope).type();
         metadata.value = getValueMetadata(x.value, scope);
         return metadata;
       });
-      parameters.forEach((x) => objectScope.add(x));
+      parameters.filter((x) => x).forEach((x) => objectScope.add(x));
       return new ObjectTypeMetadata(objectScope);
     }
     if (tree.ctx.arguments().OPEN_BRACKET()) {
