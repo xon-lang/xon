f81f18fd46392d94a3cde8458bbe81772456379d
nizami <nizami.dev@gmail.com>
2022-07-09 17:56:15 +0300

    Update source metadata

---
+++ src/metadata/source/source-metadata.test.ts
@@ -0,0 +1,33 @@
+import { ParameterTree } from '../../tree/parameter/parameter-tree';
+import { DeclarationStatementTree } from '../../tree/statement/declaration/declaration-statement-tree';
+import { parseSource, parseSourceFile } from '../../util/parse';
+import { TestDeclarationScope } from '../declaration/scope/test-declaration-scope';
+import { SourceMetadata } from './source-metadata';
+import { getSourceMetadata } from './source-metadata-helper';
+
+test('1-error.xon', () => {
+  const tree = parseSourceFile('src/metadata/source/test-files/1-error.xon');
+  const scope = new TestDeclarationScope();
+  const metadata = getSourceMetadata(tree, scope);
+
+  expect(metadata).toBeInstanceOf(SourceMetadata);
+  const issues = tree.allIssues();
+  expect(issues.length).toBe(2);
+  expect(issues[0].message).toBe('No declarations found');
+  expect(issues[1].message).toBe('Wrong import path');
+});
+
+test('2.xon', () => {
+  const tree = parseSource("{none} := import 'src/lib/@xon/core'");
+  const scope = new TestDeclarationScope();
+  const metadata = getSourceMetadata(tree, scope);
+
+  expect(metadata).toBeInstanceOf(SourceMetadata);
+  const declarations = scope.filter('none');
+  expect(declarations.length).toBe(1);
+  const declarationTree = (tree.statements[0] as DeclarationStatementTree)
+    .declaration as ParameterTree;
+  expect(declarationTree.destructure[0].metadata.sourceRange.sourceName).toBe(
+    '/Users/nizami/work/xon/core/src/lib/@xon/core/special.xon',
+  );
+});
--- src/metadata/source/source-metadata.ts
+++ src/metadata/source/source-metadata.ts
@@ -5,9 +5,9 @@ import { ParameterTree } from '../../tree/parameter/parameter-tree';
 import { SourceTree } from '../../tree/source/source-tree';
 import { DeclarationStatementTree } from '../../tree/statement/declaration/declaration-statement-tree';
 import { ExpressionStatementTree } from '../../tree/statement/expression/expression-statement-tree';
-import { getDefinitionMetadata } from '../declaration/definition/definition-metadata-helper';
+import { getShadowDefinitionMetadata } from '../declaration/definition/definition-metadata-helper';
 import { ParameterMetadata } from '../declaration/parameter/parameter-metadata';
-import { getParameterMetadata } from '../declaration/parameter/parameter-metadata-helper';
+import { getShadowParameterMetadata } from '../declaration/parameter/parameter-metadata-helper';
 import { DeclarationScope } from '../declaration/scope/declaration-scope';
 import { getStatementMetadata } from '../statement/statement-metadata-helper';
 
@@ -29,16 +29,20 @@ export class SourceMetadata {
       }
 
       if (statement.declaration instanceof DefinitionTree) {
-        statement.declaration.metadata = getDefinitionMetadata(
+        statement.declaration.metadata = getShadowDefinitionMetadata(
           statement.declaration,
           this.scope.create(),
         );
+        statement.declaration.name.metadata = statement.declaration.metadata;
         this.scope.add(statement.declaration.metadata);
       } else if (statement.declaration instanceof ParameterTree) {
-        statement.declaration.metadata = getParameterMetadata(
+        statement.declaration.metadata = getShadowParameterMetadata(
           statement.declaration,
           (statement.declaration.destructure && this.scope) || this.scope.create(),
         );
+        if (statement.declaration.name) {
+          statement.declaration.name.metadata = statement.declaration.metadata;
+        }
         if (statement.declaration.metadata.name) {
           this.scope.add(statement.declaration.metadata);
         }
