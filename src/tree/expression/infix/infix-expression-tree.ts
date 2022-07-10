298a1a83eb3ed08def24516e7c4ba9f04ba4fdc8
nizami <nizami.dev@gmail.com>
2022-07-10 09:10:42 +0300

    Update infix expression tree

--- src/tree/expression/infix/infix-expression-tree.ts
+++ src/tree/expression/infix/infix-expression-tree.ts
@@ -1,12 +1,13 @@
 // this code was generated
 
+import { ParserRuleContext } from 'antlr4ts';
 import { String } from '../../../lib/core';
-import { Metadata } from '../../../metadata/metadata';
 import { SourceRange } from '../../../util/source-range';
 import { IdTree } from '../../id/id-tree';
 import { ExpressionTree } from '../expression-tree';
 
 export class InfixExpressionTree extends ExpressionTree {
+  ctx: ParserRuleContext;
   name: IdTree;
   left: ExpressionTree;
   right: ExpressionTree;
@@ -14,6 +15,7 @@ export class InfixExpressionTree extends ExpressionTree {
 
   constructor(name: IdTree, left: ExpressionTree, right: ExpressionTree) {
     super();
+    this.ctx = left.ctx.parent;
     this.name = name;
     this.left = left;
     this.right = right;
