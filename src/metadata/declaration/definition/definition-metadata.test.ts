f1ab7c70d338d54602b523b478716effa1c45e7f
nizami <nizami.dev@gmail.com>
2022-07-30 10:25:53 +0300

    Fix tests

--- src/metadata/declaration/definition/definition-metadata.test.ts
+++ src/metadata/declaration/definition/definition-metadata.test.ts
@@ -1,19 +1,18 @@
+import { DeclarationStatementTree } from '../../../tree/statement/declaration/declaration-statement-tree';
 import { parseSource } from '../../../util/parse';
 import { SourceMetadata } from '../../source/source-metadata';
 import { getSourceMetadata } from '../../source/source-metadata-helper';
-import { getShadowSourceMetadata } from '../declaration-metadata-helper';
 import { TestDeclarationScope } from '../scope/test-declaration-scope';
 import { DefinitionMetadata } from './definition-metadata';
 
 test('definition', () => {
   const code = 'model A';
   const tree = parseSource(code);
+  const { declaration } = tree.statements[0] as DeclarationStatementTree;
   tree.scope.parent = new TestDeclarationScope();
   tree.metadata = getSourceMetadata(tree);
-  const declarations = getShadowSourceMetadata(tree);
 
   expect(tree.metadata).toBeInstanceOf(SourceMetadata);
-  expect(declarations.length).toBe(1);
-  expect(declarations[0]).toBeInstanceOf(DefinitionMetadata);
-  expect(declarations[0].name).toBe('A');
+  expect(declaration.metadata).toBeInstanceOf(DefinitionMetadata);
+  expect(declaration.metadata.name).toBe('A');
 });
