1106a39cf62fe5871fc1e2357ef50f153a270c1f
nizami <nizami.dev@gmail.com>
2022-09-10 14:51:21 +0300

    Update source metadata

--- src/metadata/source/source-metadata.ts
+++ src/metadata/source/source-metadata.ts
@@ -1,9 +1,11 @@
 import { SourceTree } from '../../tree/source/source-tree';
 import { getShadowSourceMetadata } from '../declaration/declaration-metadata-helper';
+import { Metadata } from '../metadata';
 import { getStatementMetadata } from '../statement/statement-metadata-helper';
 
-export class SourceMetadata {
+export class SourceMetadata extends Metadata {
   constructor(public tree: SourceTree) {
+    super();
     getShadowSourceMetadata(tree);
     for (const statement of tree.statements) {
       statement.metadata = getStatementMetadata(statement);
