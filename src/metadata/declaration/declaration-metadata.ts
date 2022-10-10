597d98608780941ac3cdacd4ae94a535c4a8574e
nizami <nizami.dev@gmail.com>
2022-10-10 18:07:14 +0300

    Update declaration metadata helper

--- src/metadata/declaration/declaration-metadata.ts
+++ src/metadata/declaration/declaration-metadata.ts
@@ -2,20 +2,18 @@ import { Boolean, String } from '../../lib/core';
 import { DeclarationTree } from '../../tree/declaration/declaration-tree';
 import { SourceRange } from '../../util/source-range';
 import { TypeMetadata } from '../expression/type/type-metadata';
-import { ValueMetadata } from '../expression/value/value-metadata';
 import { Metadata } from '../metadata';
 import { GenericMetadata } from './generic/generic-metadata';
 import { ParameterMetadata } from './parameter/parameter-metadata';
 
 export abstract class DeclarationMetadata extends Metadata {
-  sourceRange: SourceRange;
+  sourceRange?: SourceRange;
   modifier?: String | null;
   name?: String | null;
 
   generics: GenericMetadata[] = [];
   parameters: ParameterMetadata[] = [];
   type?: TypeMetadata | null = null;
-  value?: ValueMetadata | null;
 
   constructor(public tree: DeclarationTree | null) {
     super();
