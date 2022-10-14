c2fb15a7bdb90c62917ce64f3e6d3ab972504cf8
nizami <nizami.dev@gmail.com>
2022-10-14 20:55:43 +0300

    Update module tree helper

--- src/tree/module/module-tree-helper.ts
+++ src/tree/module/module-tree-helper.ts
@@ -2,6 +2,7 @@ import glob from 'glob';
 import path from 'path';
 import { String } from '../../lib/core';
 import { parseSourceFile } from '../../util/parse';
+import { SourceTree } from '../source/source-tree';
 import { ModuleTree } from './module-tree';
 
 export function getModuleTree(name: String, sourceName: String): ModuleTree {
@@ -9,20 +10,28 @@ export function getModuleTree(name: String, sourceName: String): ModuleTree {
 }
 
 export function getModuleTreeFromPath(modulePath: String): ModuleTree {
-  const normalizedModulePath = path.join(modulePath, '/');
-  const globPath = path.join(normalizedModulePath, '*.xon');
-  const sourceFiles = glob.sync(globPath, { nodir: true });
-  const sourceTrees = sourceFiles.map((x) => parseSourceFile(x));
-  const moduleName = path.basename(normalizedModulePath);
-  const moduleTree = getModuleTree(moduleName, normalizedModulePath);
+  const moduleName = path.basename(modulePath);
+  const moduleTree = getModuleTree(moduleName, modulePath);
+
+  const sourceTrees = getSourceTreesFromPath(modulePath);
   moduleTree.addChildren(...sourceTrees);
 
-  const innerModulesPathPattern = path.join(normalizedModulePath, '*/');
-  const modulesPaths = glob.sync(innerModulesPathPattern);
-  for (const innerModulePath of modulesPaths) {
-    const childModule = getModuleTreeFromPath(innerModulePath);
-    moduleTree.addChildren(childModule);
-  }
+  const innerModules = getInnerModules(modulePath);
+  moduleTree.addChildren(...innerModules);
 
   return moduleTree;
+}
+
+function getSourceTreesFromPath(modulePath: String): SourceTree[] {
+  const globPath = path.join(modulePath, '*.xon');
+  const sourceFiles = glob.sync(globPath, { nodir: true });
+  const sourceTrees = sourceFiles.map((x) => parseSourceFile(x));
+  return sourceTrees;
+}
+
+function getInnerModules(modulePath: String): ModuleTree[] {
+  const globPath = path.join(modulePath, '*/');
+  const modulesDirectories = glob.sync(globPath);
+  const modules = modulesDirectories.map(getModuleTreeFromPath);
+  return modules;
 }
