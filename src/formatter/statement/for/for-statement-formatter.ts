0c66289e4f6af102379ef4e03d9c79bd9f5f72de
nizami <nizami.dev@gmail.com>
2022-11-05 17:37:54 +0300

    Fix linter issues

--- src/formatter/statement/for/for-statement-formatter.ts
+++ src/formatter/statement/for/for-statement-formatter.ts
@@ -18,7 +18,7 @@ export class ForStatementFormatter extends StatementFormatter {
       .filter((x) => x)
       .map((x) => x?.toString().trim())
       .join(', ');
-    vars = vars && vars + ' in ';
+    vars &&= vars + ' in ';
     const expression = getExpressionFormatter(this.ctx.expression(), this.config)
       .indent(this.indentCount)
       .toString()
