10da73afcde82c3c6bd7ce770134b107d2f77519
nizami <nizami.dev@gmail.com>
2022-11-06 04:52:28 +0300

    Refactoring issue

--- src/issue/issue.ts
+++ src/issue/issue.ts
@@ -1,10 +1,10 @@
-import { IssueLevel } from '@/issue/issue-level';
-import { Never2, String2 } from '@/lib/core';
-import { Tree } from '@/tree/tree';
-import { SourceRange } from '@/util/source-range';
 import { ParserRuleContext, RecognitionException } from 'antlr4ts';
-import chalk from 'chalk';
-import fs from 'fs';
+import { cyan, gray, red, redBright } from 'chalk';
+import { readFileSync } from 'fs';
+import { IssueLevel } from '~/issue';
+import { Never2, String2 } from '~/lib';
+import { Tree } from '~/tree';
+import { SourceRange } from '~/util';
 
 export class Issue extends Error {
   antlrError?: RecognitionException | null;
@@ -16,19 +16,19 @@ export class Issue extends Error {
   toString(): String2 {
     let code = this.sourceRange.sourceText.split('\n')[this.sourceRange.start.line - 1];
     if (!code && this.sourceRange.sourceName && this.sourceRange.sourceName !== '<unknown>') {
-      code = fs.readFileSync(this.sourceRange.sourceName).toString().split('\n')[
+      code = readFileSync(this.sourceRange.sourceName).toString().split('\n')[
         this.sourceRange.start.line - 1
       ];
     }
-    const message = chalk.redBright(this.message);
-    const source = chalk.cyan(this.sourceRange.sourceName);
-    const line = chalk.cyan(':' + this.sourceRange.start.line);
-    const column = chalk.cyan(':' + this.sourceRange.start.column);
+    const message = redBright(this.message);
+    const source = cyan(this.sourceRange.sourceName);
+    const line = cyan(':' + this.sourceRange.start.line);
+    const column = cyan(':' + this.sourceRange.start.column);
     const lineNumberBeforeGrayed = `${this.sourceRange.start.line} | `;
-    const lineNumber = chalk.gray(lineNumberBeforeGrayed);
+    const lineNumber = gray(lineNumberBeforeGrayed);
     const caret =
       ' '.repeat(this.sourceRange.start.column + lineNumberBeforeGrayed.length - 1) +
-      chalk.red('~'.repeat(Math.min(this.sourceRange.rangeText.length, code.length)));
+      red('~'.repeat(Math.min(this.sourceRange.rangeText.length, code.length)));
 
     return `${message}\n${source}${line}${column}\n${lineNumber}${code}\n${caret}`;
   }
