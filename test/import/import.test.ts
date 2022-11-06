e61f755cbd89cb34a2a62f22aaaafabf56f92248
nizami <nizami.dev@gmail.com>
2022-11-06 22:46:36 +0300

    Fix import test imports

--- test/import/import.test.ts
+++ test/import/import.test.ts
@@ -1,11 +1,10 @@
-import {
-  DefinitionTypeMetadata,
-  getSourceMetadata,
-  SourceMetadata,
-  ValueMetadata,
-} from '~/metadata';
-import { DeclarationStatementTree, PrefixExpressionTree } from '~/tree';
-import { parseSourceFile } from '~/util';
+import { DefinitionTypeMetadata } from '~/metadata/expression/type/definition/definition-type-metadata';
+import { ValueMetadata } from '~/metadata/expression/value/value-metadata';
+import { SourceMetadata } from '~/metadata/source/source-metadata';
+import { getSourceMetadata } from '~/metadata/source/source-metadata-helper';
+import { PrefixExpressionTree } from '~/tree/expression/prefix/prefix-expression-tree';
+import { DeclarationStatementTree } from '~/tree/statement/declaration/declaration-statement-tree';
+import { parseSourceFile } from '~/util/parse';
 
 test('import', () => {
   const tree = parseSourceFile('test/import/b.xon');
