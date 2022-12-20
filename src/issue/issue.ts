76c42cfb7c9e4242522d6eaea4de2a229489509b
nizami <nizami.dev@gmail.com>
2022-12-20 22:07:29 +0300

    Add issue formatter

--- src/issue/issue.ts
+++ src/issue/issue.ts
@@ -1,5 +1,5 @@
 import { ParserRuleContext, RecognitionException } from 'antlr4ts';
-import { cyan, gray, red, redBright } from 'chalk';
+import { IssueFormatter } from '~/issue/issue-formatter';
 import { IssueLevel } from '~/issue/issue-level';
 import { Never, String2 } from '~/lib/core';
 import { SourceSpan } from '~/source/source-span';
@@ -12,28 +12,17 @@ export class Issue extends Error {
     super(message);
   }
 
-  toString(): String2 {
-    const code = this.sourceSpan.source.lineText(this.sourceSpan.start.line);
-    const message = redBright(this.message);
-    const location = cyan(this.sourceSpan.source.location);
-    const line = cyan(`:${this.sourceSpan.start.line}`);
-    const column = cyan(`:${this.sourceSpan.start.column}`);
-    const lineNumberBeforeGrayed = `${this.sourceSpan.start.line} | `;
-    const lineNumber = gray(lineNumberBeforeGrayed);
-    const caret =
-      ' '.repeat(this.sourceSpan.start.column + lineNumberBeforeGrayed.length - 1) +
-      red('~'.repeat(Math.min(this.sourceSpan.getText().length, code.length)));
-
-    return `${message}\n${location}${line}${column}\n${lineNumber}${code}\n${caret}`;
+  static errorFromSourceSpan(sourceSpan: SourceSpan, message: String2): Never {
+    const issue = new Issue(sourceSpan, IssueLevel.error, message);
+    const formatter = new IssueFormatter(issue);
+    throw new Error(formatter.toString());
   }
 
   static errorFromContext(ctx: ParserRuleContext, message: String2): Never {
-    const issue = new Issue(SourceSpan.fromContext(ctx), IssueLevel.error, message);
-    throw new Error(issue.toString());
+    Issue.errorFromSourceSpan(SourceSpan.fromContext(ctx), message);
   }
 
   static errorFromTree(tree: Tree, message: String2): Never {
-    const issue = new Issue(tree.sourceSpan, IssueLevel.error, message);
-    throw new Error(issue.toString());
+    Issue.errorFromSourceSpan(tree.sourceSpan, message);
   }
 }
