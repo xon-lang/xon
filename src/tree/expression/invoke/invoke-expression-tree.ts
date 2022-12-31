51ab5294943bee177dc9cfa77a3b2ff8aa3a2622
nizami <nizami.dev@gmail.com>
2022-12-31 11:55:46 +0300

    Update invoke expression tree

--- src/tree/expression/invoke/invoke-expression-tree.ts
+++ src/tree/expression/invoke/invoke-expression-tree.ts
@@ -4,18 +4,13 @@ import { ArgumentTree } from '~/tree/argument/argument-tree';
 import { getArgumentTree } from '~/tree/argument/argument-tree-helper';
 import { ExpressionTree } from '~/tree/expression/expression-tree';
 import { getExpressionTree } from '~/tree/expression/expression-tree-helper';
-import { Token } from '~/tree/token';
 
 export class InvokeExpressionTree extends ExpressionTree {
   ctx: InvokeExpressionContext;
   sourceSpan: SourceSpan;
   instance: ExpressionTree;
-  generics: ArgumentTree[];
+  generics: ArgumentTree[] | null = null;
   arguments: ArgumentTree[];
-  openGenerics: Token | null;
-  closeGenerics: Token | null;
-  openArguments: Token;
-  closeArguments: Token;
 
   constructor(ctx: InvokeExpressionContext) {
     super();
@@ -23,10 +18,11 @@ export class InvokeExpressionTree extends ExpressionTree {
     this.sourceSpan = SourceSpan.fromContext(ctx);
     this.instance = getExpressionTree(ctx.expression());
 
+    const generics = ctx.arguments().find((x) => x.OPEN().text === '{');
+    this.generics = generics?.argument().map(getArgumentTree) ?? [];
+
     const args = ctx.arguments().find((x) => x.OPEN().text === '[');
     this.arguments = args?.argument().map(getArgumentTree) ?? [];
-    this.openArguments = argsToken.from(args.OPEN());
-    this.closeArguments = argsToken.from(args.CLOSE());
 
     this.addChildren(this.instance, ...this.arguments);
   }
