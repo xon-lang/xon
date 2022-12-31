faeb69d27f0297702103d5502901259e6bb0b2a1
nizami <nizami.dev@gmail.com>
2022-12-31 12:41:40 +0300

    Refactoring

--- src/tree/declaration/declaration-tree-helper.ts
+++ src/tree/declaration/declaration-tree-helper.ts
@@ -1,26 +1,7 @@
-import {
-  DeclarationContext,
-  DefinitionDeclarationContext,
-  DestructureDeclarationContext,
-  OperatorDeclarationContext,
-  ParameterDeclarationContext,
-} from '~/grammar/xon-parser';
-import { Issue } from '~/issue/issue';
+import { DeclarationContext } from '~/grammar/xon-parser';
 import { DeclarationTree } from '~/tree/declaration/declaration-tree';
-import { MultipleDeclarationTree } from '~/tree/declaration/multiple/multiple-declaration-tree';
-import { SingleDeclarationTree } from '~/tree/declaration/single/single-declaration-tree';
 
 export function getDeclarationTree(ctx: DeclarationContext): DeclarationTree {
-  if (ctx instanceof DestructureDeclarationContext) return new MultipleDeclarationTree(ctx);
-  if (ctx instanceof ParameterDeclarationContext) return new SingleDeclarationTree(ctx);
-  if (ctx instanceof OperatorDeclarationContext) return new SingleDeclarationTree(ctx);
-  if (ctx instanceof DefinitionDeclarationContext) return new SingleDeclarationTree(ctx);
-
-  Issue.errorFromContext(ctx, `Declaration tree not found for "${ctx.constructor.name}"`);
+  return new DeclarationTree(ctx);
+  // Issue.errorFromContext(ctx, `Declaration tree not found for "${ctx.constructor.name}"`);
 }
-
-export const isSingleDeclaration = (declaration: DeclarationTree): declaration is SingleDeclarationTree =>
-  declaration instanceof SingleDeclarationTree;
-
-export const isMultipleDeclaration = (declaration: DeclarationTree): declaration is MultipleDeclarationTree =>
-  declaration instanceof MultipleDeclarationTree;
---
+++ src/tree/declaration/declaration-tree.test.ts
@@ -0,0 +1,65 @@
+import { DeclarationTree } from '~/tree/declaration/declaration-tree';
+import { IdExpressionTree } from '~/tree/expression/id/id-expression-tree';
+import { InvokeExpressionTree } from '~/tree/expression/invoke/invoke-expression-tree';
+import { parseDeclaration } from '~/util/parse';
+
+test('model animal', () => {
+  const code = 'model Animal';
+  const tree = parseDeclaration(code) as DeclarationTree;
+
+  expect(tree).toBeInstanceOf(DeclarationTree);
+  expect(tree.modifier?.text).toBe('model');
+  expect(tree.name?.text).toBe('Animal');
+  expect(tree.type).toBe(null);
+});
+
+test('object with parameters', () => {
+  const code = 'object Cat {T}: Animal';
+  const tree = parseDeclaration(code) as DeclarationTree;
+
+  expect(tree).toBeInstanceOf(DeclarationTree);
+  expect(tree.modifier?.text).toBe('object');
+  expect(tree.name?.text).toBe('Cat');
+  expect((tree.type as IdExpressionTree).name.text).toBe('Animal');
+  expect(tree.generics?.length).toBe(1);
+  expect(tree.generics?.at(0)?.name?.text).toBe('T');
+});
+
+test('model cat', () => {
+  const code = 'model Cat: Animal';
+  const tree = parseDeclaration(code) as DeclarationTree;
+
+  expect(tree).toBeInstanceOf(DeclarationTree);
+  expect(tree.modifier?.text).toBe('model');
+  expect(tree.name?.text).toBe('Cat');
+  expect((tree.type as IdExpressionTree).name.text).toBe('Animal');
+});
+
+test('model animal with only attribute', () => {
+  const code = 'model Animal\n   --def: Integer\n   abc: Integer';
+  const tree = parseDeclaration(code) as DeclarationTree;
+
+  expect(tree).toBeInstanceOf(DeclarationTree);
+  expect(tree.allIssues().length).toBe(0);
+  expect(tree.modifier?.text).toBe('model');
+  expect(tree.name?.text).toBe('Animal');
+  expect(tree.type).toBe(null);
+});
+
+test('model cat with generics', () => {
+  const code = 'model Cat{T: Number}: Animal{T, Integer}';
+  const tree = parseDeclaration(code) as DeclarationTree;
+
+  expect(tree).toBeInstanceOf(DeclarationTree);
+  expect(tree.modifier?.text).toBe('model');
+  expect(tree.name?.text).toBe('Cat');
+  expect(tree.generics?.length).toBe(1);
+  expect(tree.generics?.at(0)?.name?.text).toBe('T');
+  expect((tree.generics?.at(0)?.type as IdExpressionTree).name.text).toBe('Number');
+  const type = tree.type as InvokeExpressionTree;
+  expect(type).toBeInstanceOf(InvokeExpressionTree);
+  expect((type.instance as IdExpressionTree).name.text).toBe('Animal');
+  expect(type.generics?.length).toBe(2);
+  expect((type.generics?.at(0)?.value as IdExpressionTree).name.text).toBe('T');
+  expect((type.generics[1].value as IdExpressionTree).name.text).toBe('Integer');
+});
