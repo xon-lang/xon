22727872a9d92109849a89a210c61befa86b4110
nizami <nizami.dev@gmail.com>
2023-01-01 17:30:13 +0300

    Update import provider

--- src/metadata/import-provider.ts
+++ src/metadata/import-provider.ts
@@ -1,10 +1,8 @@
-import { lstatSync } from 'fs';
-import { homedir } from 'os';
-import { join, resolve } from 'path';
-import { Boolean2, String2 } from '~/lib/core';
+import { String2 } from '~/lib/core';
 import { DeclarationMetadata } from '~/metadata/declaration/declaration-metadata';
 import { getSourceDeclarationMetadata } from '~/metadata/declaration/declaration-metadata-helper';
 import { getModuleTreeFromPath } from '~/tree/module/module-tree-helper';
+import { PathFs } from '~/util/fs/path-fs';
 import { parseSourceFile } from '~/util/parse';
 
 export class ImportProvider {
@@ -12,51 +10,23 @@ export class ImportProvider {
   static instance = new ImportProvider();
 
   declarations(importPath: String2): DeclarationMetadata[] {
-    const location = resolvePath(importPath);
-    const cacheDeclarations = ImportProvider.cache.get(location);
+    const pathFs = new PathFs(importPath);
+    const cacheDeclarations = ImportProvider.cache.get(pathFs.fullPath);
     if (cacheDeclarations) {
       return cacheDeclarations;
     }
-    if (isFile(location)) {
-      const sourceTree = parseSourceFile(location);
+    if (pathFs.isFile()) {
+      const sourceTree = parseSourceFile(pathFs.fullPath);
 
       return getSourceDeclarationMetadata(sourceTree);
     }
-    if (isDirectory(location)) {
-      const moduleTree = getModuleTreeFromPath(location);
+    if (pathFs.isDirectory()) {
+      const moduleTree = getModuleTreeFromPath(pathFs.fullPath);
       const sourceTrees = moduleTree.sources;
 
       return sourceTrees.map(getSourceDeclarationMetadata).flat();
     }
 
     return [];
-  }
-}
-
-function resolvePath(importPath: String2): String2 {
-  if (importPath[0] === '~') {
-    return join(homedir(), importPath.slice(1));
-  }
-
-  return resolve(importPath);
-}
-
-function isDirectory(fullPath: String2): Boolean2 {
-  try {
-    const stats = lstatSync(fullPath);
-
-    return stats.isDirectory();
-  } catch (error) {
-    return false;
-  }
-}
-
-function isFile(fullPath): Boolean2 {
-  try {
-    const stats = lstatSync(fullPath);
-
-    return stats.isFile();
-  } catch (error) {
-    return false;
   }
 }
