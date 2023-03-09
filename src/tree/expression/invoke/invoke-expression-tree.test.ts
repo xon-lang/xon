7d64d9ec0a4d3595743b94eddc3771eb0ff778da
nizami <nizami.dev@gmail.com>
2023-03-09 20:32:57 +0300

    Update invoke expression tree

--- src/tree/expression/invoke/invoke-expression-tree.test.ts
+++ src/tree/expression/invoke/invoke-expression-tree.test.ts
@@ -1,8 +1,8 @@
 import { ArrayExpressionTree } from '~/tree/expression/array/array-expression-tree';
 import { IdExpressionTree } from '~/tree/expression/id/id-expression-tree';
+import { InfixExpressionTree } from '~/tree/expression/infix/infix-expression-tree';
 import { IntegerExpressionTree } from '~/tree/expression/integer/integer-expression-tree';
 import { InvokeExpressionTree } from '~/tree/expression/invoke/invoke-expression-tree';
-import { MemberExpressionTree } from '~/tree/expression/member/member-expression-tree';
 import { StringExpressionTree } from '~/tree/expression/string/string-expression-tree';
 import { parseExpression } from '~/util/parse';
 
@@ -11,11 +11,11 @@ test('method call', () => {
   const tree = parseExpression(code) as InvokeExpressionTree;
 
   expect(tree).toBeInstanceOf(InvokeExpressionTree);
-  expect(tree.parameters?.length).toBe(2);
-  expect(tree.parameters?.at(0) as IntegerExpressionTree).toBeInstanceOf(IntegerExpressionTree);
-  expect((tree.parameters?.at(0) as IntegerExpressionTree).value).toBe(3);
-  expect(tree.parameters?.at(1) as StringExpressionTree).toBeInstanceOf(StringExpressionTree);
-  expect((tree.parameters?.at(1) as StringExpressionTree).value).toBe('str');
+  expect(tree.array.parameters.length).toBe(2);
+  expect(tree.array.parameters.at(0) as IntegerExpressionTree).toBeInstanceOf(IntegerExpressionTree);
+  expect((tree.array.parameters.at(0) as IntegerExpressionTree).value).toBe(3);
+  expect(tree.array.parameters.at(1) as StringExpressionTree).toBeInstanceOf(StringExpressionTree);
+  expect((tree.array.parameters.at(1) as StringExpressionTree).value).toBe('str');
   expect(tree.instance).toBeInstanceOf(IdExpressionTree);
 });
 
@@ -26,9 +26,9 @@ test('method on several lines', () => {
   const tree = parseExpression(code) as InvokeExpressionTree;
 
   expect(tree).toBeInstanceOf(InvokeExpressionTree);
-  expect(tree.parameters?.length).toBe(4);
-  const indexer1 = tree.parameters?.at(0);
-  const indexer2 = tree.parameters?.at(1);
+  expect(tree.array.parameters.length).toBe(4);
+  const indexer1 = tree.array.parameters.at(0);
+  const indexer2 = tree.array.parameters.at(1);
   expect(indexer1).toBeInstanceOf(IntegerExpressionTree);
   expect(indexer2).toBeInstanceOf(StringExpressionTree);
   expect(tree.instance).toBeInstanceOf(IdExpressionTree);
@@ -39,10 +39,13 @@ test('can call with type parameter', () => {
   const tree = parseExpression(code) as InvokeExpressionTree;
 
   expect(tree).toBeInstanceOf(InvokeExpressionTree);
-  expect(tree.parameters?.length).toBe(1);
-  const indexer = tree.parameters?.at(0);
-  expect(indexer).toBeInstanceOf(IntegerExpressionTree);
-  expect(tree.instance).toBeInstanceOf(MemberExpressionTree);
+  expect(tree.array.parameters.length).toBe(1);
+  expect((tree.array.parameters[0] as IntegerExpressionTree).value).toBe(1);
+  expect(tree.instance).toBeInstanceOf(InfixExpressionTree);
+  const { operator, left, right } = tree.instance as InfixExpressionTree;
+  expect(operator.text).toBe('.');
+  expect((left as IdExpressionTree).name.text).toBe('a');
+  expect((right as IdExpressionTree).name.text).toBe('get');
 });
 
 test('object method', () => {
@@ -50,9 +53,15 @@ test('object method', () => {
   const tree = parseExpression(code) as InvokeExpressionTree;
 
   expect(tree).toBeInstanceOf(InvokeExpressionTree);
-  expect(tree.parameters?.length).toBe(0);
-  expect(tree.instance).toBeInstanceOf(MemberExpressionTree);
-  expect((tree.instance as MemberExpressionTree).instance).toBeInstanceOf(ArrayExpressionTree);
+  expect(tree.array.parameters.length).toBe(0);
+  expect(tree.instance).toBeInstanceOf(InfixExpressionTree);
+  const { operator, left, right } = tree.instance as InfixExpressionTree;
+  expect(operator.text).toBe('.');
+  const leftParameters = (left as ArrayExpressionTree).parameters;
+  expect(leftParameters.length).toBe(2);
+  expect((leftParameters[0] as IdExpressionTree).name.text).toBe('a');
+  expect((leftParameters[1] as IdExpressionTree).name.text).toBe('b');
+  expect((right as IdExpressionTree).name.text).toBe('call');
 });
 
 test('generics', () => {
@@ -60,7 +69,7 @@ test('generics', () => {
   const tree = parseExpression(code) as InvokeExpressionTree;
 
   expect(tree).toBeInstanceOf(InvokeExpressionTree);
-  expect(tree.parameters?.length).toBe(1);
+  expect(tree.array.parameters.length).toBe(1);
   expect(tree.instance).toBeInstanceOf(IdExpressionTree);
   expect((tree.instance as IdExpressionTree).name.text).toBe('Animal');
 });
--- src/tree/expression/invoke/invoke-expression-tree.ts
+++ src/tree/expression/invoke/invoke-expression-tree.ts
@@ -1,28 +1,10 @@
-import { InvokeExpressionContext } from '~/grammar/xon-parser';
 import { SourceSpan } from '~/source/source-span';
+import { ArrayExpressionTree } from '~/tree/expression/array/array-expression-tree';
 import { ExpressionTree } from '~/tree/expression/expression-tree';
-import { getExpressionTree } from '~/tree/expression/expression-tree-helper';
-import { Token } from '~/tree/token';
 
 export class InvokeExpressionTree extends ExpressionTree {
-  ctx: InvokeExpressionContext;
-  sourceSpan: SourceSpan;
-  openToken: Token;
-  closeToken: Token;
-  instance: ExpressionTree;
-  parameters: ExpressionTree[] | null = null;
-
-  constructor(ctx: InvokeExpressionContext) {
-    super();
-    this.ctx = ctx;
-    this.sourceSpan = SourceSpan.fromContext(ctx);
-
-    const args = ctx.parameters();
-    this.openToken = new Token(args.OPEN());
-    this.closeToken = new Token(args.CLOSE());
-    this.instance = getExpressionTree(ctx.expression());
-    this.parameters = args.expression().map(getExpressionTree);
-
-    this.addChildren(this.instance, this.openToken, ...this.parameters, this.closeToken);
+  constructor(public instance: ExpressionTree, public array: ArrayExpressionTree) {
+    super(SourceSpan.fromTwoTrees(instance, array));
+    this.addChildren(instance, array);
   }
 }
