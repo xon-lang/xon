2dbe40bd2299ed27e8018121917907ccccef220a
nizami <nizami.dev@gmail.com>
2023-03-09 20:29:42 +0300

    Remove body tree

--- src/tree/body/body-tree-helper.ts
+++
@@ -1,6 +0,0 @@
-import { BodyContext } from '~/grammar/xon-parser';
-import { BodyTree } from '~/tree/body/body-tree';
-
-export function getBodyTree(ctx: BodyContext): BodyTree {
-  return new BodyTree(ctx);
-}
--- src/tree/body/body-tree.test.ts
+++
@@ -1,9 +0,0 @@
-import { BodyTree } from '~/tree/body/body-tree';
-import { parseBody } from '~/util/parse';
-
-test('id value', () => {
-  const code = '\n  a = 1';
-  const tree = parseBody(code);
-
-  expect(tree).toBeInstanceOf(BodyTree);
-});
