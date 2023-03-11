cdb1611371d8501f0040745685a26b47efee9b5c
nizami <nizami.dev@gmail.com>
2023-03-11 14:24:14 +0300

    Add char alias

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -2,6 +2,7 @@
 export type Any = any;
 export type Unknown2 = unknown;
 export type Never = never;
+export type Char = string;
 export type String2 = string;
 export type Integer = number;
 export type Float = number;
