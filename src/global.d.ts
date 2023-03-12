c7d3120d21b61276acbdb68b28e170a7150f5eef
nizami <nizami.dev@gmail.com>
2023-03-12 22:03:42 +0300

    Refactoring

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-declare global {
-  interface Array<T> {
-    // eslint-disable-next-line @typescript-eslint/method-signature-style
-    findLastIndex(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: unknown): number;
-  }
-}
