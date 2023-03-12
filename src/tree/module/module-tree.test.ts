392dced1d950d5d8f5c9573ba67687f75d0751e3
nizami <nizami.dev@gmail.com>
2023-03-12 16:46:19 +0300

    Fix module tests

--- src/tree/module/module-tree.test.ts
+++ src/tree/module/module-tree.test.ts
@@ -3,8 +3,8 @@ import { getModuleFromLocation } from '~/tree/module/module-tree-helper';
 test('module from path', () => {
   const tree = getModuleFromLocation('src/lib/@xon/core');
 
-  expect(tree.modules.length).toBe(5);
-  expect(tree.sources.length).toBe(5);
+  expect(tree.modules.length).toBe(2);
+  expect(tree.sources.length).toBe(3);
 
   const testModule = tree.modules.find((x) => x.name === 'test');
   expect(testModule?.name).toBe('test');
