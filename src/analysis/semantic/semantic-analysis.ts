a2d673dbed8b07c4b64d5054a6d2e9d4bb9a8b90
nizami <nizami.dev@gmail.com>
2023-03-18 10:15:10 +0300

    Refactoring

--- src/analysis/semantic/semantic-analysis.ts
+++ src/analysis/semantic/semantic-analysis.ts
@@ -1,5 +1,14 @@
+import { Analysis } from '~/analysis/analysis';
 import { Node } from '~/analysis/node';
+import { SemanticNode } from '~/analysis/semantic/semantic-node';
 
-export class SemanticAnalysis {
-  public constructor(public node: Node) {}
+export class SemanticAnalysis implements Analysis {
+  public constructor(public syntaxNodes: Node[]) {}
+
+  nodes(): SemanticNode[] {
+    return [];
+    // if ((operator.text === '.' || operator.text === '::') && is<IdNode>(right, NodeType.ID)) {
+    //   return memberNode(operator, left, right);
+    // }
+  }
 }
