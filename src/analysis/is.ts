cc65a7addeff623db390cd8edd52405c398117f8
nizami <nizami.dev@gmail.com>
2023-06-03 09:05:02 +0300

    Update node and is function

--- src/analysis/is.ts
+++ src/analysis/is.ts
@@ -2,11 +2,11 @@ import { Node, NodeType } from '~/analysis/node';
 import { String2 } from '~/lib/core';
 
 export function is<T extends Node = Node>(node: Node | null | undefined, nodeType: NodeType | String2): node is T {
-  const suffix = node?.$.split('-')[1];
+  // const suffix = node?.$.split('-')[1];
 
-  if (suffix === nodeType) {
-    return true;
-  }
+  // if (suffix === nodeType) {
+  //   return true;
+  // }
 
   return node?.$ === nodeType;
 }
