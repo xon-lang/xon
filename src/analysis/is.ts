85448a92126aca2089368a32278160a6ce5b88e7
nizami <nizami.dev@gmail.com>
2023-06-11 15:45:29 +0300

    Update node types

--- src/analysis/is.ts
+++ src/analysis/is.ts
@@ -2,11 +2,11 @@ import { Node, NodeType } from '~/analysis/node';
 import { String2 } from '~/lib/core';
 
 export function is<T extends Node = Node>(node: Node | null | undefined, nodeType: NodeType | String2): node is T {
-  // const suffix = node?.$.split('-')[1];
+  const suffixes = node?.$.split(' ');
 
-  // if (suffix === nodeType) {
-  //   return true;
-  // }
+  if (suffixes?.includes(nodeType)) {
+    return true;
+  }
 
   return node?.$ === nodeType;
 }
