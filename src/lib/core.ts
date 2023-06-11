477e0550dc08f81a49b1fbe5594feb0daac528ec
nizami <nizami.dev@gmail.com>
2023-06-11 15:41:36 +0300

    Add core number

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -4,6 +4,7 @@ export type Unknown2 = unknown;
 export type Never = never;
 export type Char = string;
 export type String2 = string;
+export type Number2 = number;
 export type Integer = number;
 export type Float = number;
 export type Boolean2 = boolean;
