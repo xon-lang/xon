977378e13ffb1d19d3b6b3fbc2e800d05f973edd
nizami <nizami.dev@gmail.com>
2023-08-18 10:11:15 +0300

    Fix evaluate

--- src/util/evaluate.ts
+++ src/util/evaluate.ts
@@ -1,24 +1,23 @@
 import { is } from '~/analysis/is';
+import { GroupNode } from '~/analysis/lexical/node/group/group-node';
 import { IdNode } from '~/analysis/lexical/node/id/id-node';
+import { InfixNode } from '~/analysis/lexical/node/infix/infix-node';
 import { IntegerNode } from '~/analysis/lexical/node/integer/integer-node';
+import { PrefixNode } from '~/analysis/lexical/node/prefix/prefix-node';
 import { StringNode } from '~/analysis/lexical/node/string/string-node';
-import { NodeType, Token } from '~/analysis/node';
-import { ArrayNode } from '~/analysis/syntax/node/array/array-node';
-import { InfixNode } from '~/analysis/syntax/node/infix/infix-node';
-import { PrefixNode } from '~/analysis/syntax/node/prefix/prefix-node';
+import { Node, NodeType } from '~/analysis/node';
 import { String2, Unknown2 } from '~/lib/core';
-import { Source } from '~/source/source';
 
 export function escapeToString<T>(value: T): String2 {
   return (typeof value === 'string' && `\`${value}\``) || String(value);
 }
 
-export function evaluate(source: Source, node: Token | null, argsMap = {}): Unknown2 {
+export function evaluate(node: Node | null, argsMap = {}): Unknown2 {
   if (!node) {
     return null;
   }
-  if (is<ArrayNode>(node, NodeType.ARRAY)) {
-    return node.items.map((x) => evaluate(source, x ?? null));
+  if (is<GroupNode>(node, NodeType.ARRAY)) {
+    return node.items.map((x) => evaluate(x ?? null));
   }
   if (is<IntegerNode>(node, NodeType.INTEGER)) {
     return Number(node.text);
@@ -27,15 +26,17 @@ export function evaluate(source: Source, node: Token | null, argsMap = {}): Unkn
     return node.text.slice(1, -1);
   }
   if (is<InfixNode>(node, NodeType.INFIX)) {
-    const a = evaluate(source, node.left, argsMap);
-    const b = evaluate(source, node.right, argsMap);
+    const a = evaluate(node.left, argsMap);
+    const b = evaluate(node.right, argsMap);
     const operator = (node.operator.text === '^' && '**') || node.operator.text;
 
+    // eslint-disable-next-line no-eval
     return eval(`${escapeToString(a)} ${operator} ${escapeToString(b)}`);
   }
   if (is<PrefixNode>(node, NodeType.PREFIX)) {
-    const a = evaluate(source, node.value, argsMap);
+    const a = evaluate(node.value, argsMap);
 
+    // eslint-disable-next-line no-eval
     return eval(`${node.operator.text}${escapeToString(a)}`);
   }
   if (is<IdNode>(node, NodeType.ID)) {
