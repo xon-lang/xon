7c2013d97e56c3f4b2c61d36c98be200261da3e7
nizami <nizami.dev@gmail.com>
2023-09-02 16:21:30 +0300

    Fix operator test

--- src/analysis/lexical/node/operator/operator-node.test.ts
+++ src/analysis/lexical/node/operator/operator-node.test.ts
@@ -1,27 +1,29 @@
 import { LexicalAnalysis } from '~/analysis/lexical/lexical-analysis';
-import { NodeType, Token } from '~/analysis/node';
+import { PostfixNode } from '~/analysis/lexical/node/postfix/postfix-node';
+import { NodeType } from '~/analysis/node';
 import { Source } from '~/source/source';
 
-test('single operator', () => {
-  const text = '!';
+// test('single operator', () => {
+//   const text = '!';
+//   const source = Source.fromText(text, null);
+//   const lexer = new LexicalAnalysis(source.text);
+//   const nodes = lexer.body().statements[0].nodes as Token[];
+
+//   expect(nodes.length).toBe(1);
+//   expect(nodes[0].text).toBe('!');
+//   expect(nodes[0].$).toBe(NodeType.OPERATOR);
+// });
+
+test('after integer', () => {
+  const text = '1!';
   const source = Source.fromText(text, null);
   const lexer = new LexicalAnalysis(source.text);
-  const nodes = lexer.body().statements[0].nodes as Token[];
+  const nodes = lexer.body().statements[0].nodes;
+  const tree = nodes[0] as PostfixNode;
 
-  expect(nodes.length).toBe(1);
-  expect(nodes[0].text).toBe('!');
-  expect(nodes[0].$).toBe(NodeType.OPERATOR);
+  expect(tree.$).toBe(NodeType.POSTFIX);
+  expect(tree.operator.text).toBe('!');
 });
-
-// test('after integer', () => {
-//   const code = '1!';
-//   const source = Source.fromText(code);
-//   const tree = syntaxNode(source) as PostfixNode;
-
-//   expect(tree.$).toBe(NodeType.POSTFIX);
-//   expect(tree.operator.text).toBe('!');
-//   expect(evaluate(source, tree.value)).toBe(1);
-// });
 
 // test('after invoke', () => {
 //   const code = 'ctx.parameters[]!';
@@ -31,16 +33,3 @@ test('single operator', () => {
 //   expect(tree.$).toBe(NodeType.POSTFIX);
 //   expect(tree.operator.text).toBe('!');
 // });
-
-test('infix operator', () => {
-  const text = 'infix +: (a: Number, b: Number) = Number';
-  const source = Source.fromText(text, null);
-  const lexer = new LexicalAnalysis(source.text);
-  const nodes = lexer.body().statements[0].nodes as Token[];
-
-  expect(nodes.length).toBe(10);
-  expect(nodes[0].$).toBe(NodeType.MODIFIER);
-  expect(nodes[0].text).toBe('infix');
-  expect(nodes[nodes.length - 1].$).toBe(NodeType.ID);
-  expect(nodes[nodes.length - 1].text).toBe('Number');
-});
