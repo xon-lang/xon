3eba0c2a05b94f0061c3d9f3aabdc47d6e05f1a8
nizami <nizami.dev@gmail.com>
2023-12-01 19:06:50 +0300

    Update invoke node

--- src/parser/node/invoke/invoke-node.ts
+++ src/parser/node/invoke/invoke-node.ts
@@ -1,17 +1,15 @@
-import { ArrayNode } from '~/parser/node/array/array-node';
-import { GroupNode } from '~/parser/node/group/group-node';
+import { Group } from '~/parser/node/declaration/declaration-node';
 import { Node } from '~/parser/node/node';
-import { ObjectNode } from '~/parser/node/object/object-node';
 import { rangeFromNodes } from '../../../source/source-range';
 import { NodeType } from '../node-type';
 
 export interface InvokeNode extends Node {
   $: NodeType.INVOKE;
   instance: Node;
-  group: GroupNode | ObjectNode | ArrayNode;
+  group: Group;
 }
 
-export function invokeNode(instance: Node, group: GroupNode | ObjectNode | ArrayNode): InvokeNode {
+export function invokeNode(instance: Node, group: Group): InvokeNode {
   return {
     $: NodeType.INVOKE,
     range: rangeFromNodes(instance, group),
