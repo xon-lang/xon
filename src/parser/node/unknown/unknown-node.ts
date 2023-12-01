cbdbd63527b6b188eb7e9682f6ebb25ad17465cb
nizami <nizami.dev@gmail.com>
2023-12-01 19:07:42 +0300

    Update unknown node

--- src/parser/node/unknown/unknown-node.ts
+++ src/parser/node/unknown/unknown-node.ts
@@ -1,5 +1,6 @@
 import { String2 } from '~/lib/core';
 import { ParserContext } from '~/parser/parser-context';
+import { SourceRange } from '~/source/source-range';
 import { NodeType } from '../node-type';
 import { TokenNode } from '../token-node';
 
@@ -7,13 +8,17 @@ export interface UnknownNode extends TokenNode {
   $: NodeType.UNKNOWN;
 }
 
-export function unknownNode(text: String2): Partial<UnknownNode> {
+export function unknownNode(range: SourceRange, text: String2): UnknownNode {
   return {
     $: NodeType.UNKNOWN,
+    range,
     text,
   };
 }
 
-export function scanUnknownNode({ index, source }: ParserContext): Partial<UnknownNode> | null {
-  return unknownNode(source.text[index]);
+export function scanUnknownNode(context: ParserContext): UnknownNode | null {
+  const text = context.source.text[context.index];
+  const range = context.getRange(text.length);
+
+  return unknownNode(range, text);
 }
