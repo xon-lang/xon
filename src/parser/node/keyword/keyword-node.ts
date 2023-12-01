7e7af5a5356d0d5caebb779a360a74a58099ae22
nizami <nizami.dev@gmail.com>
2023-12-01 19:07:03 +0300

    Update keyword node

--- src/parser/node/keyword/keyword-node.ts
+++ src/parser/node/keyword/keyword-node.ts
@@ -1,4 +1,5 @@
 import { String2 } from '~/lib/core';
+import { SourceRange } from '~/source/source-range';
 import { NodeType } from '../node-type';
 import { TokenNode } from '../token-node';
 
@@ -6,9 +7,10 @@ export interface KeywordNode extends TokenNode {
   $: NodeType.KEYWORD;
 }
 
-export function keywordNode(text: String2): Partial<KeywordNode> {
+export function keywordNode(range: SourceRange, text: String2): KeywordNode {
   return {
     $: NodeType.KEYWORD,
+    range,
     text,
   };
 }
