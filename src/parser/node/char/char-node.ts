79f4571729a18be29600b88d24438963b5571ac3
nizami <nizami.dev@gmail.com>
2023-12-01 19:05:44 +0300

    Update char node

--- src/parser/node/char/char-node.ts
+++ src/parser/node/char/char-node.ts
@@ -1,31 +1,31 @@
 import { String2 } from '~/lib/core';
 import { ParserContext } from '~/parser/parser-context';
 import { CHAR_QUOTE } from '~/parser/util/operators';
-import { Type } from '~/type/type';
+import { SourceRange } from '~/source/source-range';
 import { NodeType } from '../node-type';
 import { TokenNode } from '../token-node';
 
 export interface CharNode extends TokenNode {
   $: NodeType.CHAR;
-  type: Type;
 }
 
-export function charNode(text: String2): Partial<CharNode> {
+export function charNode(range: SourceRange, text: String2): CharNode {
   return {
     $: NodeType.CHAR,
+    range,
     text,
   };
 }
 
-export function scanCharNode({ index, source }: ParserContext): Partial<CharNode> | null {
-  if (source.text[index] === CHAR_QUOTE) {
-    const nextQuoteIndex = source.text.indexOf(CHAR_QUOTE, index + 1);
+export function scanCharNode(context: ParserContext): CharNode | null {
+  if (context.source.text[context.index] === CHAR_QUOTE) {
+    const nextQuoteIndex = context.source.text.indexOf(CHAR_QUOTE, context.index + 1);
 
-    if (nextQuoteIndex < 0) {
-      return charNode(source.text.slice(index, source.text.length));
-    }
+    const endSlice = nextQuoteIndex < 0 ? context.source.text.length : nextQuoteIndex + 1;
+    const text = context.source.text.slice(context.index, endSlice);
+    const range = context.getRange(text.length);
 
-    return charNode(source.text.slice(index, nextQuoteIndex + 1));
+    return charNode(range, text);
   }
 
   return null;
