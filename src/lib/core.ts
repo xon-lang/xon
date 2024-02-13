ca80d9bd6e88febfe93c497a8234c26f286a9ec0
nizami <nizami.dev@gmail.com>
2024-02-13 16:37:16 +0300

    Update types

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Any = any;
+// export type Dynamic = any;
 
 export type Unknown2 = unknown;
 
--- src/parser/type/type.ts
+++ src/parser/type/type.ts
@@ -1,10 +1,34 @@
-import { Boolean2, String2 } from '../../lib/core';
+import { Boolean2, Integer, String2, Unknown2 } from '../../lib/core';
 
 export interface Type {
   name: String2;
-  isIndexer: Boolean2;
-  isInvoke: Boolean2;
-  attributes: Type[];
+  base: Type | null;
+  literalValue: Unknown2 | null ;
+  parameters: Type[];
+  attributes: Attribute[];
+
+  is: (type: Type) => Boolean2;
+  eq: (type: Type) => Boolean2;
+}
+
+export interface Attribute {
+  name: String2;
+  type: Type;
+}
+
+export function numberType(): Type {
+  return {
+    name: 'Integer',
+    base: null,
+    literalValue: null
+  }
+}
+
+export function integerType(literalValue: Integer | null): Type {
+  return {
+    name: 'Integer',
+    base: 
+  }
 }
 
 export const StringType: Type = {
@@ -14,8 +38,6 @@ export const StringType: Type = {
   attributes: [
     {
       name: 'length',
-      isIndexer: false,
-      isInvoke: false,
       attributes: [],
     },
   ],
