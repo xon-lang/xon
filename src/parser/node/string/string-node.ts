8f5e74530981bddebc7722b58afdc8d34c62c4e3
nizami <nizami.dev@gmail.com>
2024-02-14 08:13:18 +0300

    Fix tests

--- src/parser/node/string/string-node.ts
+++ src/parser/node/string/string-node.ts
@@ -2,13 +2,13 @@ import { String2 } from '../../../lib/core';
 import { ParserContext } from '../../../parser/parser-context';
 import { SourceRange } from '../../../source/source-range';
 import { STRING_QUOTE } from '../../parser-config';
-import { AnythingType, stringLiteralType } from '../../type/type';
+import { StringLiteralType, stringLiteralType } from '../../type/string/string-type';
 import { TokenNode } from '../node';
 import { NodeType } from '../node-type';
 
 export interface StringNode extends TokenNode {
   $: NodeType.STRING;
-  type: AnythingType;
+  type: StringLiteralType;
 }
 
 export function stringNode(range: SourceRange, text: String2): StringNode {
