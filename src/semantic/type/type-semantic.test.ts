57f20320fb0f26a59e9f343f7a4c45653ed56d4a
nizami <nizami.dev@gmail.com>
2024-02-21 14:31:18 +0300

    Update semantic test

--- src/semantic/type/type-semantic.test.ts
+++ src/semantic/type/type-semantic.test.ts
@@ -2,11 +2,14 @@ import { parseSyntax } from '../../syntax/syntax';
 import { $Semantic, parseSemantic } from '../semantic';
 
 test('a is integer', () => {
-  const text = 'const a: Integer = 1';
+  const text = `
+model Integer
+const a: Integer = 1
+`;
   const syntax = parseSyntax(text);
   const semantic = parseSemantic(syntax);
 
-  expect(Object.keys(semantic.declarations).length).toBe(1);
+  expect(Object.keys(semantic.declarations).length).toBe(2);
   expect(semantic.declarations.a[0].$).toBe($Semantic.CONST);
-  expect(semantic.declarations.a[0].name).toBe('A');
+  expect(semantic.declarations.a[0].name).toBe('a');
 });
