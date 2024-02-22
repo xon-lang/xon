91479afe486ea571eee31b4313756886018b5c2f
nizami <nizami.dev@gmail.com>
2024-02-22 14:22:05 +0300

    Refactoring

--- src/semantic/usage/type/declaration/declaration-type-semantic.ts
+++ src/semantic/usage/type/declaration/declaration-type-semantic.ts
@@ -1,4 +1,4 @@
-import { Boolean2, String2 } from '../../../../lib/core';
+import { Boolean2, Nothing, String2 } from '../../../../lib/core';
 import { SourceReference } from '../../../../source/source-reference';
 import { DeclarationSemantic } from '../../../declaration/declaration-semantic';
 import { $Semantic } from '../../../semantic';
@@ -7,16 +7,22 @@ import { TypeSemantic } from '../type-semantic';
 export interface DeclarationTypeSemantic extends TypeSemantic {
   $: $Semantic.DECLARATION_TYPE;
   declaration: DeclarationSemantic;
+  generics: TypeSemantic[] | Nothing;
+  arguments: TypeSemantic[] | Nothing;
 }
 
 export function declarationTypeSemantic(
   reference: SourceReference,
   declaration: DeclarationSemantic,
+  generics: TypeSemantic[] | Nothing,
+  args: TypeSemantic[] | Nothing,
 ): DeclarationTypeSemantic {
   const semantic: DeclarationTypeSemantic = {
     $: $Semantic.DECLARATION_TYPE,
     reference,
     declaration,
+    generics,
+    arguments: args,
 
     eq(type: TypeSemantic): Boolean2 {
       return false;
