06306bf8ff509c1252f241fb31335bcb049e7803
nizami <nizami.dev@gmail.com>
2024-02-24 10:49:45 +0300

    Refactoring

--- src/parser/node/syntax/syntax-node.ts
+++ src/parser/node/syntax/syntax-node.ts
@@ -1,12 +1,12 @@
 import { Nothing } from '../../../lib/core';
-import { SourceRange, rangeFromNodes } from '../../../source/source-range';
+import { rangeFromNodes } from '../../../source/source-range';
 import { Node } from '../node';
 
 export interface SyntaxNode extends Node {
   readonly children: Node[];
 }
 
-export function getRangeAndChildren(...nodes: (Node | Nothing)[]): { range: SourceRange; children: Node[] } {
+export function getRangeAndChildren(...nodes: (Node | Nothing)[]): Pick<SyntaxNode, 'range' | 'children'> {
   const children = nodes.filter((x): x is Node => !!x);
   const range = rangeFromNodes(children.first(), children.last());
 
