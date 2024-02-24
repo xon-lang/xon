24346497f3c4526f6cb7f8c613df12f1bf49c818
nizami <nizami.dev@gmail.com>
2024-02-24 10:18:35 +0300

    Update formatter

--- src/formatter/formatter.ts
+++ src/formatter/formatter.ts
@@ -1,35 +1,95 @@
-import { String2 } from '../lib/core';
+import { Nothing, String2 } from '../lib/core';
 import { ArrayNode } from '../syntax/node/array/array-node';
-import { $Node, TokenNode } from '../syntax/node/node';
+import { CloseNode } from '../syntax/node/close/close-node';
+import { GroupNode } from '../syntax/node/group/group-node';
+import { InfixNode } from '../syntax/node/infix/infix-node';
+import { $Node, Node, TokenNode } from '../syntax/node/node';
+import { ObjectNode } from '../syntax/node/object/object-node';
+import { OpenNode } from '../syntax/node/open/open-node';
+import { PostfixNode } from '../syntax/node/postfix/postfix-node';
+import { PrefixNode } from '../syntax/node/prefix/prefix-node';
 import { StatementNode } from '../syntax/node/statement/statement-node';
 import { NL } from '../syntax/syntax-config';
 import { SyntaxResult } from '../syntax/syntax-result';
 import { is } from '../syntax/util/is';
 
-function syntaxFormat(syntax: SyntaxResult) {
+export function syntaxFormat(syntax: SyntaxResult) {
   const nodes = syntax.statements.map((x) => statementFormat(x));
   return nodes.join(NL);
 }
 
 function statementFormat(statement: StatementNode): String2 {
   const node = statement.item;
-  const tokens = [$Node.CHAR, $Node.COMMA, $Node.ID, $Node.INTEGER, $Node.OPERATOR, $Node.STRING];
 
-  if (tokens.some((x) => is(node, x))) {
-    return tokenFormat(node as TokenNode);
+  return nodeFormat(node);
+}
+
+function nodeFormat(node: Node) {
+  if (isToken(node)) {
+    return node.text;
   }
 
-  if (is<ArrayNode>(node, $Node.ARRAY)) {
-    return arrayFormat(node);
+  if (is<PrefixNode>(node, $Node.PREFIX)) {
+    return prefixFormat(node);
+  }
+
+  if (is<PostfixNode>(node, $Node.POSTFIX)) {
+    return postfixFormat(node);
+  }
+
+  if (is<InfixNode>(node, $Node.INFIX)) {
+    return infixFormat(node);
+  }
+
+  if (is<GroupNode>(node, $Node.GROUP) || is<ArrayNode>(node, $Node.ARRAY) || is<ObjectNode>(node, $Node.OBJECT)) {
+    return groupFormat(node);
   }
 
   throw new Error(`Not implemented for ${node.$}`);
 }
 
-function tokenFormat(node: TokenNode) {
-  return node.text;
+function prefixFormat(node: PrefixNode) {
+  const operator = node.operator.text;
+  const value = node.value ? nodeFormat(node.value) : '';
+
+  if (operator.some((x) => x.isDigitOrLetter(0))) {
+    return `${operator} ${value}`;
+  }
+
+  return `${operator}${value}`;
 }
 
-function arrayFormat(node: ArrayNode) {
-  return node.text;
+function postfixFormat(node: PostfixNode) {
+  const operator = node.operator.text;
+  const value = node.value ? nodeFormat(node.value) : '';
+
+  if (operator.some((x) => x.isDigitOrLetter(0))) {
+    return `${value} ${operator}`;
+  }
+
+  return `${value}${operator}`;
+}
+
+function infixFormat(node: InfixNode) {
+  const operator = node.operator.text;
+  const left = node.left ? nodeFormat(node.left) : '';
+  const right = node.right ? nodeFormat(node.right) : '';
+
+  if (operator.some((x) => x === '.')) {
+    return `${left}${operator}${right}`;
+  }
+
+  return `${left} ${operator} ${right}`;
+}
+
+function groupFormat(node: { open: OpenNode; close: CloseNode | Nothing; items: (Node | Nothing)[] }): String2 {
+  const open = node.open.text;
+  const close = node.close?.text ?? '';
+
+  const items = node.items.map((x) => (x ? nodeFormat(x) : '')).join(', ');
+  return `${open}${items}${close}`;
+}
+
+function isToken(node: Node): node is TokenNode {
+  return 'text' in node;
 }
