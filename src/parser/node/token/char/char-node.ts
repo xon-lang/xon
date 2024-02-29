900f1111fd86c0d0864b513db11246e77b99a85a
nizami <nizami.dev@gmail.com>
2024-02-29 14:27:31 +0300

    Refactoring

--- src/parser/node/token/char/char-node.ts
+++ src/parser/node/token/char/char-node.ts
@@ -1,9 +1,9 @@
-import { String2 } from '../../../../lib/core';
-import { SourceRange } from '../../../../source/source-range';
-import { CHAR_QUOTE } from '../../../syntax-config';
-import { SyntaxContext } from '../../../syntax-context';
-import { $Node } from '../../node';
-import { TokenNode, tokenNode } from '../token-node';
+import {Nothing, String2, nothing} from '../../../../lib/core';
+import {SourceRange} from '../../../../source/source-range';
+import {CHAR_QUOTE} from '../../../syntax-config';
+import {SyntaxContext} from '../../../syntax-context';
+import {$Node} from '../../node';
+import {TokenNode, tokenNode} from '../token-node';
 
 export interface CharNode extends TokenNode {
   $: $Node.CHAR;
@@ -13,16 +13,16 @@ export function charNode(range: SourceRange, text: String2): CharNode {
   return tokenNode($Node.CHAR, range, text);
 }
 
-export function scanCharNode(context: SyntaxContext): CharNode | null {
-  if (context.source.text[context.position.index] === CHAR_QUOTE) {
-    const nextQuoteIndex = context.source.text.indexOf(CHAR_QUOTE, context.position.index + 1);
-
-    const endSlice = nextQuoteIndex < 0 ? context.source.text.length : nextQuoteIndex + 1;
-    const text = context.source.text.slice(context.position.index, endSlice);
-    const range = context.getRange(text.length, false);
-
-    return charNode(range, text);
+export function scanCharNode(context: SyntaxContext): CharNode | Nothing {
+  if (context.source.text[context.position.index] !== CHAR_QUOTE) {
+    return nothing;
   }
 
-  return null;
+  const nextQuoteIndex = context.source.text.indexOf(CHAR_QUOTE, context.position.index + 1);
+
+  const endSlice = nextQuoteIndex < 0 ? context.source.text.length : nextQuoteIndex + 1;
+  const text = context.source.text.slice(context.position.index, endSlice);
+  const range = context.getRange(text.length, false);
+
+  return charNode(range, text);
 }
