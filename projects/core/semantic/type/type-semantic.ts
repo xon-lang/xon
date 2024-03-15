e1311a0091a0895116f4c568578577398c9a5874
nizami <nizami.dev@gmail.com>
2024-03-15 13:58:31 +0300

    Update type semantic

--- projects/core/semantic/type/type-semantic.ts
+++ projects/core/semantic/type/type-semantic.ts
@@ -2,12 +2,13 @@ import {Boolean2, String2} from '../../lib/core';
 import {Semantic} from '../semantic';
 
 export interface TypeSemantic extends Semantic {
-  attributes(): Record<String2, TypeSemantic[]>;
+  attributes: Record<String2, TypeSemantic[]>;
+
   eq(semantic: TypeSemantic): Boolean2;
   is(semantic: TypeSemantic): Boolean2;
 }
 
-// todo remove this hack
 export function isTypeSemantic(semantic: Semantic): semantic is TypeSemantic {
+  // todo remove this hack
   return semantic.$.endsWith('_TYPE');
 }
