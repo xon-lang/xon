74b10b29c6166cb922a04d8fa848019c4aa8cf58
nizami <nizami.dev@gmail.com>
2024-03-29 18:20:46 +0300

    Add function type semantic

---
+++ projects/core/semantic/type/function/function-type-semantic-parser.ts
@@ -0,0 +1,36 @@
+import {ISSUE_MESSAGE} from '../../../issue/issue-message';
+import {Nothing, nothing} from '../../../lib/core';
+import {$Node, Node, is} from '../../../parser/node/node';
+import {StringNode} from '../../../parser/node/token/string/string-node';
+import {DeclarationKind} from '../../declaration-manager';
+import {TypeDeclarationSemantic} from '../../declaration/type/type-declaration-semantic';
+import {$Semantic, semanticIs} from '../../semantic';
+import {SemanticContext} from '../../semantic-context';
+import {FunctionTypeSemantic, functionTypeSemantic} from './function-type-semantic';
+
+export function functionTypeSemanticTryParse(context: SemanticContext, node: Node): FunctionTypeSemantic | Nothing {
+  if (!is<StringNode>(node, $Node.STRING)) {
+    return nothing;
+  }
+
+  const declaration = context.declarationManager.single(
+    DeclarationKind.TYPE,
+    context.config.literalTypeNames.stringTypeName,
+    nothing,
+    nothing,
+  );
+
+  if (!declaration || !semanticIs<TypeDeclarationSemantic>(declaration, $Semantic.TYPE_DECLARATION)) {
+    context.issueManager.addError(
+      node.range,
+      ISSUE_MESSAGE.declarationNotFound(context.config.literalTypeNames.stringTypeName),
+    );
+
+    return nothing;
+  }
+
+  const reference = context.createReference(node);
+  const semantic = functionTypeSemantic(reference, declaration, node.value);
+
+  return semantic;
+}
---
+++ projects/core/semantic/type/function/function-type-semantic.ts
@@ -0,0 +1,47 @@
+import {Array2, Boolean2, String2} from '../../../lib/core';
+import {TextResourceReference} from '../../../util/resource/resource-reference';
+import {DeclarationSemantic} from '../../declaration/declaration-semantic';
+import {$Semantic} from '../../semantic';
+import {isInSet, isSetOperatorTypeSemantic} from '../set/set';
+import {TypeSemantic} from '../type-semantic';
+
+export interface FunctionTypeSemantic extends TypeSemantic {
+  $: $Semantic.FUNCTION_TYPE;
+  parameters: DeclarationSemantic[];
+  result: TypeSemantic;
+}
+
+export function functionTypeSemantic(
+  reference: TextResourceReference,
+  parameters: DeclarationSemantic[],
+  result: TypeSemantic,
+): FunctionTypeSemantic {
+  const semantic: FunctionTypeSemantic = {
+    $: $Semantic.FUNCTION_TYPE,
+    reference,
+    parameters,
+    result,
+
+    is(other: TypeSemantic): Boolean2 {
+      if (isSetOperatorTypeSemantic(other)) {
+        return isInSet(this, other);
+      }
+
+      if (this.eq(other)) {
+        return true;
+      }
+
+      return false;
+    },
+
+    eq(other: TypeSemantic): Boolean2 {
+      return false;
+    },
+
+    attributes(): Record<String2, Array2<TypeSemantic>> {
+      return {};
+    },
+  };
+
+  return semantic;
+}
