a47947a1aeab0acca4f3cf77bff3746400fa4c77
nizami <nizami.dev@gmail.com>
2024-05-11 09:08:20 +0300

    Update formatter

--- projects/core/formatter/formatter.ts
+++ projects/core/formatter/formatter.ts
@@ -1,14 +1,13 @@
 import {Array2, Boolean2, Nothing, String2, nothing} from '../lib/core';
 import {$Node, Node, is} from '../parser/node/node';
 import {StatementNode} from '../parser/node/statement/statement-node';
-import {CommentBlockNode} from '../parser/node/token/comment-block/comment-block-node';
-import {CommentLineNode} from '../parser/node/token/comment-line/comment-line-node';
 import {NlNode} from '../parser/node/token/nl/nl-node';
 import {TokenNode} from '../parser/node/token/token-node';
 import {WhitespaceNode} from '../parser/node/token/whitespace/whitespace-node';
 import {NL} from '../parser/parser-config';
 import {SyntaxContext} from '../parser/syntax-context';
-import {TextRange, rangeFromNodes, rangeFromPosition} from '../util/resource/text/text-range';
+import {TextRange, cloneRange, rangeFromNodes, rangeFromPosition} from '../util/resource/text/text-range';
+import {TextResource} from '../util/resource/text/text-resource';
 
 export interface Formatter {
   range: TextRange;
@@ -46,9 +45,30 @@ export function formatAfterHiddenNodes(context: SyntaxContext, node: Node, keepS
 }
 
 export function formatStatement(context: SyntaxContext, statement: StatementNode): Nothing {
-  // if (statement.hiddenNodes?.length === 0) {
+  // if (!statement.hiddenNodes || statement.hiddenNodes.length === 0) {
   //   return;
   // }
+
+  // const indentNlIndex = statement.hiddenNodes.findLast<NlNode>((x): x is NlNode => is<NlNode>(x, $Node.NL));
+
+  // if (!indentNlNode) {
+  //   const indentText = '  '.repeat(statement.indentLevel);
+  //   const nonWhitespaceNodes = statement.hiddenNodes.filter((x) => !is(x, $Node.WHITESPACE));
+  //   const text = indentText + nonWhitespaceNodes.map((x) => x.text).join(' ');
+
+  //   if (isSameContent(context.resource, statement.hiddenNodes, text)) {
+  //     return nothing;
+  //   }
+  
+  //     context.formatterManager.addFormatter({
+  //       range: rangeFromNodes( statement.hiddenNodes),
+  //       text,
+  //     });
+  // } 
+
+
+
+
   // const ifFirstStatement = context.statements.first() === statement;
   // const beforeIndentHiddenNodes = statement.beforeIndentHiddenNodes;
   // if (beforeIndentHiddenNodes.length > 0) {
@@ -97,73 +117,28 @@ export function formatStatement(context: SyntaxContext, statement: StatementNode
   // }
 }
 
-export function formatLastContextHiddenNodes(context: SyntaxContext): Formatter | Nothing {
-  const {statements, hiddenNodesBuffer: hiddenNodes} = context;
+export function formatRemainingContextHiddenNodes(context: SyntaxContext): Formatter | Nothing {
+  const {statements, hiddenNodes} = context;
 
-  if (statements.length === 0 && hiddenNodes.length === 0) {
-    return nothing;
-  }
-
-  if (hiddenNodes.length > 0) {
-    const nonWhitespaceNodes = hiddenNodes.filter((x) => !is(x, $Node.WHITESPACE));
-    const splittedByNl = nonWhitespaceNodes.splitBy((x) => is(x, $Node.NL));
-    const range = rangeFromNodes(hiddenNodes);
-
-    let text = splittedByNl
-      .map((x) => format(context, x.splitter) + x.items.map((z) => format(context, z)).join(' '))
-      .join('');
-
-    if (statements.length === 0) {
-      text = text.trim();
-      text = text.length > 0 ? text + NL : '';
-
-      return compareAndCreateFormatter(context, hiddenNodes, range, text);
+  if (hiddenNodes.length === 0) {
+    if (!context.lastStatement) {
+      return nothing;
     }
 
-    return compareAndCreateFormatter(context, hiddenNodes, range, text.trimEnd() + NL);
+    return {
+      range: rangeFromPosition(context.lastStatement.range.stop),
+      text: NL,
+    };
   }
 
-  const lastStatementNode = context.nodes.last();
-  const lastNode = lastStatementNode?.hiddenNodes?.last() ?? lastStatementNode;
+  const formattedHiddenNodesText = formatHiddenNodes(hiddenNodes, nothing).trimEnd();
 
-  if (!lastNode) {
-    return nothing;
-  }
+  const text =
+    statements.length > 0 || formattedHiddenNodesText.length > 0
+      ? formattedHiddenNodesText + NL
+      : formattedHiddenNodesText;
 
-  return {
-    range: rangeFromPosition(lastNode.range.stop),
-    text: NL,
-  };
-}
-
-export function getFormatterForHiddenNodes(
-  context: SyntaxContext,
-  range: TextRange,
-  hiddenNodes: Array2<TokenNode>,
-  formattingType: FormattingType | Nothing,
-): Formatter | Nothing {
-  const nonWhitespaceNodes = hiddenNodes.filter((x) => !is(x, $Node.WHITESPACE));
-  const splittedByNl = nonWhitespaceNodes.splitBy((x) => is(x, $Node.NL));
-
-  let text = splittedByNl
-    .map((x) => format(context, x.splitter) + x.items.map((z) => format(context, z)).join(' '))
-    .join('');
-
-  if (text.length > 0 && formattingType) {
-    if (formattingType === FormattingType.BEFORE) {
-      text = `${text} `;
-    }
-
-    if (formattingType === FormattingType.BETWEEN) {
-      text = ` ${text} `;
-    }
-
-    if (formattingType === FormattingType.AFTER && !text.startsWith(NL)) {
-      text = ` ${text}`;
-    }
-  }
-
-  if (isSameContent(context, hiddenNodes, text)) {
+  if (isSameContent(context.resource, hiddenNodes, text)) {
     return nothing;
   }
 
@@ -179,69 +154,101 @@ function getFormatterForHiddenNodesWithSpaceKeeping(
   keepSingleSpace: Boolean2,
   formattingType: FormattingType,
 ): Formatter | Nothing {
-  const hiddenNodes = node.hiddenNodes ?? [];
-  const spaceText = keepSingleSpace ? ' ' : '';
+  if (!node.hiddenNodes || node.hiddenNodes.length === 0) {
+    if (keepSingleSpace) {
+      return {
+        range: rangeFromPosition(node.range.start),
+        text: ' ',
+      };
+    }
 
-  if (hiddenNodes.length === 0) {
-    return compareAndCreateFormatter(context, hiddenNodes, rangeFromPosition(node.range.stop), spaceText);
+    return nothing;
   }
 
-  if (hiddenNodes.length === 1 && is<WhitespaceNode>(hiddenNodes[0], $Node.WHITESPACE)) {
-    return compareAndCreateFormatter(context, hiddenNodes, rangeFromNodes(hiddenNodes), spaceText);
+  if (node.hiddenNodes.length === 1 && is<WhitespaceNode>(node.hiddenNodes[0], $Node.WHITESPACE)) {
+    const whitespace = node.hiddenNodes[0];
+
+    if (!keepSingleSpace) {
+      return {
+        range: cloneRange(whitespace.range),
+        text: '',
+      };
+    }
+
+    if (whitespace.text === ' ') {
+      return nothing;
+    }
+
+    return {
+      range: cloneRange(whitespace.range),
+      text: ' ',
+    };
   }
 
-  return getFormatterForHiddenNodes(
-    context,
-    hiddenNodes.length > 0 ? rangeFromNodes(hiddenNodes) : rangeFromPosition(node.range.stop),
-    hiddenNodes,
-    formattingType,
-  );
+  return getFormatterForHiddenNodes(context.resource, node.hiddenNodes, formattingType);
 }
 
-function format(context: SyntaxContext, node: TokenNode | Nothing): String2 {
-  if (is<NlNode>(node, $Node.NL)) {
-    const nlCount = node.text.count((x) => x === NL);
-    const text = NL.repeat(Math.min(nlCount, 2));
-
-    return text;
-  }
-
-  if (is<CommentLineNode>(node, $Node.COMMENT_LINE)) {
-    return node.text;
-  }
-
-  if (is<CommentBlockNode>(node, $Node.COMMENT_BLOCK)) {
-    return node.text;
-  }
-
-  return '';
-}
-
-function compareAndCreateFormatter(
-  context: SyntaxContext,
-  nodes: Array2<TokenNode>,
-  range: TextRange,
-  text: String2,
+export function getFormatterForHiddenNodes(
+  resource: TextResource,
+  hiddenNodes: Array2<TokenNode>,
+  formattingType: FormattingType,
 ): Formatter | Nothing {
-  if (isSameContent(context, nodes, text)) {
+  const text = formatHiddenNodes(hiddenNodes, formattingType);
+
+  if (isSameContent(resource, hiddenNodes, text)) {
     return nothing;
   }
 
   return {
-    range,
+    range: rangeFromNodes(hiddenNodes),
     text,
   };
 }
 
-function isSameContent(context: SyntaxContext, nodes: Array2<TokenNode>, text: String2): Boolean2 {
-  const first = nodes.first();
-  const last = nodes.last();
+export function formatHiddenNodes(hiddenNodes: Array2<TokenNode>, formattingType: FormattingType | Nothing): String2 {
+  const splittedByNl = hiddenNodes
+    .filter((x) => !is(x, $Node.WHITESPACE))
+    .splitBy<NlNode>((x): x is NlNode => is<NlNode>(x, $Node.NL));
+
+  const text = splittedByNl.map((x) => formatNlNode(x.splitter) + x.items.map((z) => z.text).join(' ')).join('');
+
+  if (text.length > 0 && formattingType) {
+    if (formattingType === FormattingType.BEFORE) {
+      return `${text} `;
+    }
+    if (formattingType === FormattingType.BETWEEN) {
+      return ` ${text} `;
+    }
+    // todo remove branch with FormattingType.AFTER
+    if (formattingType === FormattingType.AFTER && !text.startsWith(NL)) {
+      return ` ${text}`;
+    }
+  }
+
+  return text;
+}
+
+function formatNlNode(node: NlNode | Nothing): String2 {
+  if (!node) {
+    return '';
+  }
+
+  // todo replace with node.range.stop.line - node.range.start.line
+  const nlCount = node.text.count((x) => x === NL);
+  const text = NL.repeat(Math.min(nlCount, 2));
+
+  return text;
+}
+
+function isSameContent(resource: TextResource, hiddenNodes: Array2<TokenNode>, text: String2): Boolean2 {
+  const first = hiddenNodes.first();
+  const last = hiddenNodes.last();
 
   if (!first || !last) {
     return text === '';
   }
 
-  const sliced = context.resource.data.slice(first.range.start.index, last.range.stop.index);
+  const sliced = resource.data.slice(first.range.start.index, last.range.stop.index);
 
   return text === sliced;
 }
