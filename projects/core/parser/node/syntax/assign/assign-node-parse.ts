c659aa00237e34b7207d3ad3d92a1288111f75d6
nizami <nizami.dev@gmail.com>
2024-05-25 20:12:51 +0300

    Refactoring

--- projects/core/parser/node/syntax/assign/assign-node-parse.ts
+++ projects/core/parser/node/syntax/assign/assign-node-parse.ts
@@ -1,29 +1,29 @@
-import {Integer, nothing} from '../../../../../lib/types';
-import {ASSIGN} from '../../../parser-config';
-import {SyntaxContext} from '../../../syntax-context';
-import {$Node, is, nodeFindMap} from '../../node';
-import {SyntaxParseFn} from '../../statement/statement-node-collapse';
-import {OperatorNode} from '../../token/operator/operator-node';
-import {assignNode} from './assign-node';
+// import {Integer, nothing} from '../../../../../lib/types';
+// import {ASSIGN} from '../../../parser-config';
+// import {SyntaxContext} from '../../../syntax-context';
+// import {$Node, is, isNonOperatorExpression, nodeFindMap} from '../../node';
+// import {SyntaxParseFn} from '../../statement/statement-node-collapse';
+// import {OperatorNode} from '../../token/operator/operator-node';
+// import {assignNode} from './assign-node';
 
-function assignNodeParse(): SyntaxParseFn {
-  return (context: SyntaxContext, startIndex: Integer) => {
-    return nodeFindMap(context.nodes, startIndex, false, (node, index, nodes) => {
-      if (
-        !is<OperatorNode>(node, $Node.OPERATOR) ||
-        node.text !== ASSIGN ||
-        (index !== 0 && !is<OperatorNode>(nodes[index - 1], $Node.OPERATOR))
-      ) {
-        return nothing;
-      }
+// function assignNodeParse(): SyntaxParseFn {
+//   return (context: SyntaxContext, startIndex: Integer) => {
+//     return nodeFindMap(context.nodes, startIndex, false, (node, index, nodes) => {
+//       if (
+//         !is<OperatorNode>(node, $Node.OPERATOR) ||
+//         node.text !== ASSIGN ||
+//         (index !== 0 && !is<OperatorNode>(nodes[index - 1], $Node.OPERATOR))
+//       ) {
+//         return nothing;
+//       }
 
-      const value = nodes[index + 1];
+//       const value = nodes[index + 1];
 
-      if (!is(value, $Node.e)) {
-        return nothing;
-      }
+//       if (!isNonOperatorExpression(value)) {
+//         return nothing;
+//       }
 
-      return {node: assignNode(context, node, value), index: index};
-    });
-  };
-}
+//       return {node: assignNode(context, node, value), index: index};
+//     });
+//   };
+// }
