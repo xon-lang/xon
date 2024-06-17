c5c5ecf9214bbda80b488664582da64e9df00775
nizami <nizami.dev@gmail.com>
2024-06-17 19:14:34 +0300

    Refactoring

--- projects/core/analyzer/syntax/token/id/id-node-parse.ts
+++ projects/core/analyzer/syntax/token/id/id-node-parse.ts
@@ -4,7 +4,7 @@ import {SyntaxContext} from '../../../syntax-context';
 import {IdNode, idNode} from './id-node';
 
 export function idNodeParse(context: SyntaxContext, index: Integer): IdNode | Nothing {
-  if (context.resource.data[index] !== UNDERSCORE && !context.resource.data.isLetter(index)) {
+  if (!context.checkLexemeAtIndex(UNDERSCORE, index) && !context.resource.data.isLetter(index)) {
     return nothing;
   }
 
