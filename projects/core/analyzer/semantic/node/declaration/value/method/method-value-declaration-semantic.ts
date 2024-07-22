bb2a8bee7b9a20ce8b8ec6063ca5087262f764ad
nizami <nizami.dev@gmail.com>
2024-07-22 23:11:15 +0300

    Add method value declaration semantic

---
+++ projects/core/analyzer/semantic/node/declaration/value/method/method-value-declaration-semantic.ts
@@ -0,0 +1,38 @@
+import {$} from '../../../../../../$';
+import {Array2, Boolean2, Nothing, String2} from '../../../../../../../lib/types';
+import {TextResourceRange} from '../../../../../../util/resource/text/text-resource-range';
+import {TypeSemantic} from '../../../type/type-semantic';
+import {DeclarationSemantic} from '../../declaration-semantic';
+import {ValueDeclarationSemantic} from '../value-declaration-semantic';
+
+export type MethodValueDeclarationSemantic = ValueDeclarationSemantic & {
+  $: $.MethodValueDeclarationSemantic;
+  parameters?: Array2<DeclarationSemantic | Nothing> | Nothing;
+
+  eq(other: DeclarationSemantic): Boolean2;
+};
+
+export function methodValueDeclarationSemantic(
+  reference: TextResourceRange,
+  documentation: String2 | Nothing,
+  modifier: String2 | Nothing,
+  name: String2,
+  parameters: Array2<DeclarationSemantic | Nothing> | Nothing,
+  type: TypeSemantic | Nothing,
+): MethodValueDeclarationSemantic {
+  return {
+    $: $.MethodValueDeclarationSemantic,
+    reference,
+    usages: [],
+
+    documentation,
+    modifier,
+    name,
+    parameters,
+    type,
+
+    eq(other: DeclarationSemantic): Boolean2 {
+      return this.reference.equals(other.reference);
+    },
+  };
+}
