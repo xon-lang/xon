9fad459acff2ae9f89d7b8d55e423aea6c2acb00
nizami <nizami.dev@gmail.com>
2024-07-22 23:12:04 +0300

    Add structural type declaration semantic

---
+++ projects/core/analyzer/semantic/node/declaration/type/structural/structural-type-declaration-semantic.ts
@@ -0,0 +1,36 @@
+import {$} from '../../../../../../$';
+import {Boolean2, Nothing, String2} from '../../../../../../../lib/types';
+import {TextResourceRange} from '../../../../../../util/resource/text/text-resource-range';
+import {TypeSemantic} from '../../../type/type-semantic';
+import {DeclarationSemantic} from '../../declaration-semantic';
+import {TypeDeclarationSemantic} from '../type-declaration-semantic';
+
+export type StructuralTypeDeclarationSemantic = TypeDeclarationSemantic & {
+  $: $.StructuralTypeDeclarationSemantic;
+  value?: TypeSemantic | Nothing;
+
+  eq(other: DeclarationSemantic): Boolean2;
+};
+
+export function structuralTypeDeclarationSemantic(
+  reference: TextResourceRange,
+  documentation: String2 | Nothing,
+  modifier: String2 | Nothing,
+  name: String2,
+  value: TypeSemantic | Nothing,
+): StructuralTypeDeclarationSemantic {
+  return {
+    $: $.StructuralTypeDeclarationSemantic,
+    reference,
+    usages: [],
+    documentation,
+
+    modifier,
+    name,
+    value,
+
+    eq(other: DeclarationSemantic): Boolean2 {
+      return this.reference.equals(other.reference);
+    },
+  };
+}
