6fa280fa50e5e28c493f1f43dcfcab50347b7c38
nizami <nizami.dev@gmail.com>
2024-07-28 19:09:54 +0300

    Update string value semantic

--- projects/core/analyzer/semantic/node/value/string/string-value-semantic-parser.ts
+++ projects/core/analyzer/semantic/node/value/string/string-value-semantic-parser.ts
@@ -1,18 +1,27 @@
+import {$, is} from '../../../../../$';
 import {Nothing, nothing} from '../../../../../../lib/types';
 import {Node} from '../../../../node';
+import {StringNode} from '../../../../syntax/node/string/string-node';
 import {SemanticAnalyzer} from '../../../semantic-analyzer';
-import {stringTypeSemanticTryParse} from '../../type/string/string-type-semantic-parser';
+import {stringTypeSemanticParse} from '../../type/string/string-type-semantic-parser';
 import {StringValueSemantic, stringValueSemantic} from './string-value-semantic';
 
 export function stringValueSemanticTryParse(
   analyzer: SemanticAnalyzer,
   node: Node,
 ): StringValueSemantic | Nothing {
-  const type = stringTypeSemanticTryParse(analyzer, node);
-
-  if (!type) {
+  if (!is(node, $.StringNode)) {
     return nothing;
   }
+
+  return stringValueSemanticParse(analyzer, node);
+}
+
+export function stringValueSemanticParse(
+  analyzer: SemanticAnalyzer,
+  node: StringNode,
+): StringValueSemantic | Nothing {
+  const type = stringTypeSemanticParse(analyzer, node);
 
   return stringValueSemantic(analyzer.createReference(node), type);
 }
--- projects/core/analyzer/semantic/node/value/string/string-value-semantic.ts
+++ projects/core/analyzer/semantic/node/value/string/string-value-semantic.ts
@@ -5,6 +5,8 @@ import {ValueSemantic} from '../value-semantic';
 
 export type StringValueSemantic = ValueSemantic<$.StringValueSemantic, StringTypeSemantic>;
 
+// todo should we use 'value: Integer' or always can get from 'type' ???
+
 export function stringValueSemantic(
   reference: TextResourceRange,
   type: StringTypeSemantic,
