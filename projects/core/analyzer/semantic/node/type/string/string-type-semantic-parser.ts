3c5f0630f2c582e508f746ba256a491aae25e632
nizami <nizami.dev@gmail.com>
2024-07-28 19:08:31 +0300

    Update string type semantic

--- projects/core/analyzer/semantic/node/type/string/string-type-semantic-parser.ts
+++ projects/core/analyzer/semantic/node/type/string/string-type-semantic-parser.ts
@@ -1,6 +1,7 @@
 import {$, is} from '../../../../../$';
 import {Nothing, nothing} from '../../../../../../lib/types';
 import {Node} from '../../../../node';
+import {StringNode} from '../../../../syntax/node/string/string-node';
 import {SemanticAnalyzer} from '../../../semantic-analyzer';
 import {StringTypeSemantic, stringTypeSemantic} from './string-type-semantic';
 
@@ -12,6 +13,10 @@ export function stringTypeSemanticTryParse(
     return nothing;
   }
 
+  return stringTypeSemanticParse(analyzer, node);
+}
+
+export function stringTypeSemanticParse(analyzer: SemanticAnalyzer, node: StringNode): StringTypeSemantic {
   const declaration = analyzer.declarationManager.single(
     $.NominalTypeDeclarationSemantic,
     analyzer.config.literalTypeNames.stringTypeName,
