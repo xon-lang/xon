58cb06bfd5aa4bd5e8283e8c4f770f2cdfb10925
nizami <nizami.dev@gmail.com>
2024-08-09 21:12:40 +0300

    Add structural type declaration deep handle

---
+++ projects/core/analyzer/semantic/node/declaration/type/structural/structural-type-declaration-semantic-handle.ts
@@ -0,0 +1,30 @@
+import {DeclarationNode} from '../../../../../syntax/node/declaration/declaration-node';
+import {SemanticAnalyzer} from '../../../../semantic-analyzer';
+import {functionTypeSemantic} from '../../../type/function/function-type-semantic';
+import {parametersOrGenericsParse} from '../../../type/function/function-type-semantic-parser';
+import {typeSemanticParse} from '../../../type/type-semantic-parser';
+import {unknownTypeFromNode} from '../../../type/unknown/unknown-type-semantic';
+import {StructuralTypeDeclarationSemantic} from './structural-type-declaration-semantic';
+
+export function structuralTypeDeclarationSemanticHandle(
+  analyzer: SemanticAnalyzer,
+  semantic: StructuralTypeDeclarationSemantic,
+  node: DeclarationNode,
+): void {
+  const resultType = node.assign
+    ? typeSemanticParse(analyzer, node.assign.value)
+    : unknownTypeFromNode(analyzer, node);
+
+  if (node.generics) {
+    const generics = parametersOrGenericsParse(analyzer, node, node.generics);
+    semantic.type = functionTypeSemantic(analyzer.reference(node.generics), generics, resultType);
+
+    return;
+  }
+
+  semantic.type = resultType;
+
+  if (node.parameters) {
+    throw new Error('Not implemented');
+  }
+}
