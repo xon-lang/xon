e444085f04c5f83e0ae2a5a6b6185d541396cb28
nizami <nizami.dev@gmail.com>
2024-08-19 23:41:48 +0300

    Add parameter type declaration semantic

---
+++ projects/core/analyzer/semantic/node/declaration/type/parameter/parameter-type-declaration-semantic-handle.ts
@@ -0,0 +1,30 @@
+import {DeclarationNode} from '../../../../../syntax/node/declaration/declaration-node';
+import {SemanticAnalyzer} from '../../../../semantic-analyzer';
+import {functionTypeSemantic} from '../../../type/function/function-type-semantic';
+import {parametersParse} from '../../../type/function/function-type-semantic-parser';
+import {typeSemanticParse} from '../../../type/type-semantic-parser';
+import {unknownTypeSemantic} from '../../../type/unknown/unknown-type-semantic';
+import {ParameterTypeDeclarationSemantic} from './parameter-type-declaration-semantic';
+
+export function parameterTypeDeclarationSemanticHandle(
+  analyzer: SemanticAnalyzer,
+  semantic: ParameterTypeDeclarationSemantic,
+  node: DeclarationNode,
+): void {
+  const resultType = node.assign
+    ? typeSemanticParse(analyzer, node.assign.value)
+    : unknownTypeSemantic(analyzer, node);
+
+  if (node.generics) {
+    const generics = parametersParse(analyzer, node, node.generics);
+    semantic.type = functionTypeSemantic(analyzer, node.generics, generics, resultType);
+
+    return;
+  }
+
+  semantic.type = resultType;
+
+  if (node.parameters) {
+    throw new Error('Not implemented');
+  }
+}
