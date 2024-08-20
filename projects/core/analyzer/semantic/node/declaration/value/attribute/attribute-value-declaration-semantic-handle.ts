78684ba036c4467bd26c6a1ace7af139742383d5
nizami <nizami.dev@gmail.com>
2024-08-20 21:43:14 +0300

    Refactoring

--- projects/core/analyzer/semantic/node/declaration/value/attribute/attribute-value-declaration-semantic-handle.ts
+++ projects/core/analyzer/semantic/node/declaration/value/attribute/attribute-value-declaration-semantic-handle.ts
@@ -1,7 +1,9 @@
+import {nothing, Nothing} from '../../../../../../../lib/types';
 import {DeclarationNode} from '../../../../../syntax/node/declaration/declaration-node';
 import {SemanticAnalyzer} from '../../../../semantic-analyzer';
 import {functionTypeSemantic} from '../../../type/function/function-type-semantic';
 import {parametersParse} from '../../../type/function/function-type-semantic-parser';
+import {TypeSemantic} from '../../../type/type-semantic';
 import {typeSemanticParse} from '../../../type/type-semantic-parser';
 import {unknownTypeSemantic} from '../../../type/unknown/unknown-type-semantic';
 import {AttributeValueDeclarationSemantic} from './attribute-value-declaration-semantic';
@@ -11,23 +13,37 @@ export function attributeValueDeclarationSemanticHandle(
   semantic: AttributeValueDeclarationSemantic,
   node: DeclarationNode,
 ): void {
-  const valueType = node.assign
-    ? typeSemanticParse(analyzer, node.assign.value)
-    : unknownTypeSemantic(analyzer, node);
+  let generics: ReturnType<typeof parametersParse> | Nothing = nothing;
+  let parameters: ReturnType<typeof parametersParse> | Nothing = nothing;
+  let type: TypeSemantic = unknownTypeSemantic(analyzer, node.type?.value ?? node.assign?.value ?? node);
 
-  let type = node.type ? typeSemanticParse(analyzer, node.type.value) : valueType;
-
-  if (node.assign?.value && !valueType.is(type)) {
-    analyzer.diagnosticManager.addPredefinedDiagnostic(node.assign.value.reference, (x) => x.wrongType());
+  if (node.generics) {
+    generics = parametersParse(analyzer, node, node.generics);
   }
 
   if (node.parameters) {
-    const parameters = parametersParse(analyzer, node, node.parameters);
+    parameters = parametersParse(analyzer, node, node.parameters);
+  }
+
+  if (node.type) {
+    type = typeSemanticParse(analyzer, node.type.value);
+  }
+
+  if (node.assign) {
+    const valueType = typeSemanticParse(analyzer, node.assign.value);
+
+    if (!node.type) {
+      type = valueType;
+    } else if (!valueType.is(type)) {
+      analyzer.diagnosticManager.addPredefinedDiagnostic(node.assign.value.reference, (x) => x.wrongType());
+    }
+  }
+
+  if (node.parameters && parameters) {
     type = functionTypeSemantic(analyzer, node.parameters, parameters, type);
   }
 
-  if (node.generics) {
-    const generics = parametersParse(analyzer, node, node.generics);
+  if (node.generics && generics) {
     type = functionTypeSemantic(analyzer, node.generics, generics, type);
   }
 
