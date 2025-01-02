1bc546d006f7058617883e3c5de1ae3ac6e5de7a
nizami <nizami.dev@gmail.com>
2025-01-02 18:31:32 +0300

    Add number model

---
+++ packages/common/src/number/number.factory.ts
@@ -0,0 +1,12 @@
+// import {$Number, Boolean2, Number2} from '#common';
+
+// export function newNumber2(number: number): Number2 {
+//   return {
+//     $: $Number,
+//     _number: number,
+
+//     equals(other: Number2): Boolean2 {
+//       return this._number === other._number;
+//     },
+//   };
+// }
---
+++ packages/common/src/number/number.model.ts
@@ -0,0 +1,10 @@
+import {Boolean2, commonPackageType} from '#common';
+import {Model} from '#typing';
+
+// export interface Number2 extends Model {
+//   _number: number;
+
+//   equals(other: Number2): Boolean2;
+// }
+
+// export const $Number = commonPackageType<Number2>('Number');
