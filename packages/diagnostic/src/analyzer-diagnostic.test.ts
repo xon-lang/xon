eb2606c4584722e468ebef46d0094b24cfaf595a
nizami <nizami.dev@gmail.com>
2025-01-07 22:34:29 +0300

    Add diagnostic tests

---
+++ packages/diagnostic/src/analyzer-diagnostic.test.ts
@@ -0,0 +1,13 @@
+// test('diagnostic', () => {
+//   expect(syntax.diagnosticManager.diagnostics.count()).toBe(2);
+
+//   const diagnosticMessage = predefinedDiagnostics(node.reference).unexpectedExpression().message;
+
+//   expect(syntax.diagnosticManager.diagnostics.at(0)?.message.actual.toNativeString()).toBe(
+//     diagnosticMessage.actual.toNativeString(),
+//   );
+//   expect(syntax.diagnosticManager.diagnostics.at(1)?.message.actual.toNativeString()).toBe(
+//     diagnosticMessage.actual.toNativeString(),
+//   );
+
+// });
