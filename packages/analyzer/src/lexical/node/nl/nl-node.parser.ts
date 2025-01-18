e45a40e9d65c1a2eedb016aca11c0dc1f7e27c77
nizami <nizami.dev@gmail.com>
2025-01-18 16:55:08 +0300

    Update nl node

--- packages/analyzer/src/lexical/node/nl/nl-node.parser.ts
+++ packages/analyzer/src/lexical/node/nl/nl-node.parser.ts
@@ -1,11 +1,9 @@
-import {$NlNode, CR, LF, NlNode, SPACE} from '#analyzer';
+import {$NlNode, CR, CRLF, LF, NlNode} from '#analyzer';
 import {CharStream, Nothing} from '#common';
 
 export function parseNlNode(source: CharStream): NlNode | Nothing {
-  const node = source.takeWhile(
-    $NlNode,
-    (x, i) => (i > 0 && x.equals(SPACE)) || x.equals(CR) || x.equals(LF),
-  );
+  const crlfNode = source.takeWhile($NlNode, (x, i) => CRLF.at2(i).equals(x), CRLF.count());
+  const node = crlfNode ?? source.takeWhile($NlNode, (x, i) => x.equals(CR) || x.equals(LF), 1);
 
   if (node) {
     node.isHidden = true;
--- packages/analyzer/src/lexical/node/nl/nl-node.test.ts
+++ packages/analyzer/src/lexical/node/nl/nl-node.test.ts
@@ -4,17 +4,17 @@ import {is} from '#typing';
 import {expect, test} from 'vitest';
 
 test('cr lf', () => {
-  const text = newText('\n\r\nabc');
+  const text = newText('\r\nabc');
   const source = charStreamFromText(text);
   const node = parseNlNode(source);
 
   expect(node).toBeTruthy();
   expect(is(node, $NlNode)).toBe(true);
-  expect(node?.text.toNativeString()).toBe('\n\r\n');
+  expect(node?.text.toNativeString()).toBe('\r\n');
   expect(node?.range.start.index).toBe(0);
   expect(node?.range.start.line).toBe(0);
-  expect(node?.range.stop.index).toBe(3);
-  expect(node?.range.stop.line).toBe(2);
+  expect(node?.range.stop.index).toBe(2);
+  expect(node?.range.stop.line).toBe(1);
 });
 
 test('cr lf whitespace', () => {
@@ -24,11 +24,11 @@ test('cr lf whitespace', () => {
 
   expect(node).toBeTruthy();
   expect(is(node, $NlNode)).toBe(true);
-  expect(node?.text.toNativeString()).toBe('\n   \r\n\n   ');
+  expect(node?.text.toNativeString()).toBe('\n');
   expect(node?.range.start.index).toBe(0);
   expect(node?.range.start.line).toBe(0);
-  expect(node?.range.stop.index).toBe(10);
-  expect(node?.range.stop.line).toBe(3);
+  expect(node?.range.stop.index).toBe(1);
+  expect(node?.range.stop.line).toBe(1);
 });
 
 test('starts with whitespace', () => {
