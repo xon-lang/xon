b51fa97b40be550a249537a5eb983794388dc6a7
nizami <nizami.dev@gmail.com>
2025-01-20 22:26:02 +0300

    Refactoring

--- packages/analyzer/src/lexical/nodes/number/radix-point-operator/radix-point-node.parser.ts
+++ packages/analyzer/src/lexical/nodes/number/radix-point-operator/radix-point-node.parser.ts
@@ -1,10 +1,6 @@
-import {$RadixPointNode, AnalyzerContext, RADIX_POINT, RadixPointNode} from '#analyzer';
+import {$RadixPointNode, AnalyzerContext, POINT, RadixPointNode} from '#analyzer';
 import {Nothing} from '#common';
 
 export function parseRadixPointNode(context: AnalyzerContext): RadixPointNode | Nothing {
-  return context.source.takeWhile(
-    $RadixPointNode,
-    (x, i) => RADIX_POINT.at2(i).equals(x),
-    RADIX_POINT.count(),
-  );
+  return context.source.takeWhile($RadixPointNode, (x, i) => POINT.at2(i).equals(x), POINT.count());
 }
