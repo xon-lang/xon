413996fbc8e68415e1e295dee4891eac90eed24c
nizami <nizami.dev@gmail.com>
2025-02-19 00:53:38 +0300

    Add barrels script

---
+++ scripts/barrels.script.ts
@@ -0,0 +1,55 @@
+import {promises as fs} from 'fs';
+import {extname, join} from 'path';
+
+const directories = [
+  'packages/analyzer',
+  'packages/common',
+  'packages/diagnostic',
+  'packages/interpreter',
+  'packages/translator',
+  'packages/typing',
+  'packages/vscode',
+  'packages/xon-lib',
+];
+
+const excludeDirectories = ['dist'];
+const excludeFiles = ['index.ts'];
+const excludeExtensions = ['.spec.ts', '.test.ts'];
+
+async function createBarrel(directory: string) {
+  const files = await fs.readdir(directory, {withFileTypes: true, recursive: true});
+  const exports = files
+    .filter((file) => {
+      const relativePath = join(directory, file.name).replace(/\\/g, '/');
+      const dirName = relativePath.split('/')[1];
+
+      if (!file.isFile()) {
+        return false;
+      }
+
+      const ext = extname(file.name);
+
+      return (
+        ext === '.ts' &&
+        !excludeDirectories.includes(dirName) &&
+        !excludeFiles.includes(file.name) &&
+        !excludeExtensions.some((x) => file.name.endsWith(x))
+      );
+    })
+    .map((file) => {
+      const relativePath = join(file.parentPath, file.name).replace(directory, '').replace(/\\/g, '/');
+      return `export * from '.${relativePath.replace('.ts', '')}';`;
+    })
+    .sort((a, b) => a.localeCompare(b))
+    .join('\n');
+
+  const barrelPath = join(directory, 'index.ts');
+  await fs.writeFile(barrelPath, exports);
+}
+
+async function main() {
+  await Promise.all(directories.map((directory) => createBarrel(directory)));
+  console.log('\x1b[32m%s\x1b[0m', 'Done!');
+}
+
+main().catch(console.error);
