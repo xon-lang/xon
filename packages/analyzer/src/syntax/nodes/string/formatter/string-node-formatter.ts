15994ed8cf55ee878e569522cab906a34759e15c
nizami <nizami.dev@gmail.com>
2025-02-26 23:20:36 +0300

    Update string formatter

--- packages/analyzer/src/syntax/nodes/string/formatter/string-node-formatter.ts
+++ packages/analyzer/src/syntax/nodes/string/formatter/string-node-formatter.ts
@@ -1,6 +1,6 @@
-import {FormatterItem, StringNode} from '#analyzer';
+import {AnalyzerFormatter, StringNode} from '#analyzer';
 import {ArrayData, newArrayData} from '#common';
 
-export function formatStringNode(this: StringNode): ArrayData<FormatterItem> {
+export function formatStringNode(this: StringNode): ArrayData<AnalyzerFormatter> {
   return newArrayData();
 }
