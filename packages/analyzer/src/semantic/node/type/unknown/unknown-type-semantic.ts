b015ee84667c6145643e39e8272d30a75f6fc691
nizami <nizami.dev@gmail.com>
2025-03-01 21:52:19 +0300

    Refactor unknown type semantic

--- packages/analyzer/src/semantic/node/type/unknown/unknown-type-semantic.ts
+++ packages/analyzer/src/semantic/node/type/unknown/unknown-type-semantic.ts
@@ -1,79 +1,26 @@
-import {
-  $AnalyzerType,
-  $IdTypeSemantic,
-  $NominalTypeDeclarationSemantic,
-  $SetTypeSemantic,
-  $TypeSemantic,
-  AttributeValueDeclarationSemantic,
-  DeclarationScope,
-  isInSet,
-  newDeclarationScope,
-  Node,
-  NominalTypeDeclarationSemantic,
-  SemanticAnalyzer,
-  TypeSemantic,
-} from '#analyzer';
-import {Boolean2, Nothing} from '#common';
-import {is} from '#typing';
+import {$AnalyzerType, $TypeSemantic, AttributeTypeDeclarationSemantic, TypeSemantic} from '#analyzer';
+import {Boolean2, Nothing, nothing, Text} from '#common';
+import {Brand} from '#typing';
 
-// todo use something instead of unknown ???
-// todo one Unknown doesn't equals other unknown
-export type UnknownTypeSemantic = TypeSemantic & {
-  declaration?: NominalTypeDeclarationSemantic | Nothing;
-};
+export type UnknownTypeSemantic = TypeSemantic & Brand<'Analyzer.MemberNode'>;
 
 export const $UnknownTypeSemantic = () =>
   $AnalyzerType<UnknownTypeSemantic>('UnknownTypeSemantic', $TypeSemantic());
 
-// todo use 'Anything' type as unknown
-export function unknownTypeSemantic(analyzer: SemanticAnalyzer, nodeLink: Node): UnknownTypeSemantic {
-  const {unknownTypeName} = analyzer.config.literalTypeNames;
-  const declaration = analyzer.declarationManager.find($NominalTypeDeclarationSemantic(), unknownTypeName);
-
-  if (declaration) {
-    declaration.usages.addLastItem(nodeLink.reference);
-  }
-
+export function newUnknownTypeSemantic(): UnknownTypeSemantic {
   return {
     $: $UnknownTypeSemantic(),
-    nodeLink,
-    declaration,
+
+    getAttribute(name: Text): AttributeTypeDeclarationSemantic | Nothing {
+      return nothing;
+    },
 
     is(other: TypeSemantic): Boolean2 {
-      if (is(other, $SetTypeSemantic())) {
-        return isInSet(this, other);
-      }
-
-      if (this.equals(other)) {
-        return true;
-      }
-
-      if (is(this.declaration, $NominalTypeDeclarationSemantic())) {
-        return this.declaration.type?.is(other) ?? false;
-      }
-
       return false;
     },
 
     equals(other: TypeSemantic): Boolean2 {
-      if (
-        is(this.declaration, $NominalTypeDeclarationSemantic()) &&
-        is(other, $IdTypeSemantic()) &&
-        is(other.declaration, $NominalTypeDeclarationSemantic())
-      ) {
-        return this.declaration.equals(other.declaration);
-      }
-
       return false;
-    },
-
-    attributes(): DeclarationScope<AttributeValueDeclarationSemantic> {
-      // todo review body of this function
-      if (this.declaration) {
-        return this.declaration.attributes?.clone() ?? newDeclarationScope();
-      }
-
-      return newDeclarationScope();
     },
   };
 }
