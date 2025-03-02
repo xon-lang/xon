971d3806fc297299f28ee7f693bb639829191fe5
nizami <nizami.dev@gmail.com>
2025-03-02 14:32:43 +0300

    Update usage semantic

--- packages/analyzer/src/semantic/node/usage/usage-semantic.ts
+++ packages/analyzer/src/semantic/node/usage/usage-semantic.ts
@@ -1,10 +1,11 @@
 import {$AnalyzerType, $Semantic, DeclarationSemantic, Semantic, TypeSemantic} from '#analyzer';
-import {Nothing, Text} from '#common';
+import {Nothing, Text, TextReference} from '#common';
 
 export type UsageSemantic = Semantic & {
   name: Text;
   type?: TypeSemantic | Nothing;
   declaration?: DeclarationSemantic | Nothing;
+  reference?: TextReference | Nothing;
 };
 
 export const $UsageSemantic = () => $AnalyzerType<UsageSemantic>('UsageSemantic', $Semantic());
@@ -13,15 +14,19 @@ export function newUsageSemantic(
   name: Text,
   type?: TypeSemantic | Nothing,
   declaration?: DeclarationSemantic | Nothing,
+  reference?: TextReference | Nothing,
 ): UsageSemantic {
-  // if (declaration) {
-  //   declaration.usages.addLastItem(nodeLink.reference);
-  // }
-
-  return {
+  const usage: UsageSemantic = {
     $: $UsageSemantic(),
     name,
     type,
     declaration,
+    reference,
   };
+
+  if (declaration) {
+    declaration.usages.addLastItem(usage);
+  }
+
+  return usage;
 }
