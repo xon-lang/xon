a7ea778bc1b800bcb2f835b44322d2582b849045
nizami <nizami.dev@gmail.com>
2025-03-02 15:15:45 +0300

    Refactor type semantic

--- packages/analyzer/src/semantic/attribute-list.model.ts
+++ packages/analyzer/src/semantic/attribute-list.model.ts
@@ -1,23 +1,53 @@
-import {$AnalyzerType, DeclarationSemantic} from '#analyzer';
-import {ArrayData, newArrayData, Nothing, Text} from '#common';
+import {$AnalyzerType, AttributeDeclarationSemantic} from '#analyzer';
+import {ArrayData, Dictionary, newArrayData, newDictionary, Nothing, Text} from '#common';
 import {Brand, Model} from '#typing';
 
+// todo can we merge it with 'SemanticScope' ???
 export type AttributeList = Model &
   Brand<'Analyzer.AttributeList'> & {
-    _items: ArrayData<DeclarationSemantic>;
+    _declarations: Dictionary<Text, ArrayData<AttributeDeclarationSemantic>>;
 
-    get(name: Text): DeclarationSemantic | Nothing;
+    add(declaration: AttributeDeclarationSemantic): void;
+    get(name: Text): AttributeDeclarationSemantic | Nothing;
   };
 
 export const $AttributeList = () => $AnalyzerType<AttributeList>('AttributeList');
 
-export function newAttributeList(): AttributeList {
+export function newAttributeList(
+  declarations?: ArrayData<AttributeDeclarationSemantic> | Nothing,
+): AttributeList {
+  const _declarations = newDictionary<Text, ArrayData<AttributeDeclarationSemantic>>();
+
+  if (declarations) {
+    for (const declaration of declarations) {
+      const overloads = _declarations.get(declaration.name);
+
+      if (overloads) {
+        overloads.addFirstItem(declaration);
+      } else {
+        _declarations.set(declaration.name, newArrayData([declaration]));
+      }
+    }
+  }
+
   return {
     $: $AttributeList(),
-    _items: newArrayData(),
+    _declarations,
 
-    get(name: Text): DeclarationSemantic | Nothing {
-      return;
+    add(declaration: AttributeDeclarationSemantic): void {
+      this._declarations ??= newDictionary();
+
+      const overloads = this._declarations.get(declaration.name);
+
+      if (overloads) {
+        overloads.addFirstItem(declaration);
+      } else {
+        this._declarations.set(declaration.name, newArrayData([declaration]));
+      }
+    },
+
+    get(name: Text): AttributeDeclarationSemantic | Nothing {
+      return this._declarations.get(name)?.first();
     },
   };
 }
--- packages/analyzer/src/semantic/node/type/type-semantic.ts
+++ packages/analyzer/src/semantic/node/type/type-semantic.ts
@@ -1,19 +1,11 @@
-import {
-  $AnalyzerType,
-  $Semantic,
-  AttributeTypeDeclarationSemantic,
-  Semantic,
-  TypeDeclarationSemantic,
-} from '#analyzer';
-import {ArrayData, Boolean2, Nothing, Text} from '#common';
+import {$AnalyzerType, $Semantic, AttributeList, Semantic} from '#analyzer';
+import {Boolean2, Nothing} from '#common';
 
 export type TypeSemantic = Semantic & {
-  declaration?: TypeDeclarationSemantic | Nothing;
+  attributes: AttributeList | Nothing;
 
-  getAttribute(name: Text): AttributeTypeDeclarationSemantic | Nothing;
-  getAttributes(): ArrayData<AttributeTypeDeclarationSemantic> | Nothing;
-  is(other: TypeSemantic): Boolean2;
   equals(other: TypeSemantic): Boolean2;
+  is(other: TypeSemantic): Boolean2;
 };
 
 export const $TypeSemantic = () => $AnalyzerType<TypeSemantic>('TypeSemantic', $Semantic());
