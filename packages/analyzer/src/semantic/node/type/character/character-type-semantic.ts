687df624abbcbd2cda0c201008844aaa9d144873
nizami <nizami.dev@gmail.com>
2025-03-02 21:56:36 +0300

    Refactor character type semantic

---
+++ packages/analyzer/src/semantic/node/type/character/character-type-semantic.ts
@@ -0,0 +1,55 @@
+import {
+  $AnalyzerType,
+  $IdTypeSemantic,
+  $SetTypeSemantic,
+  $TypeSemantic,
+  isInSet,
+  NominalTypeDeclarationSemantic,
+  TypeSemantic,
+} from '#analyzer';
+import {Boolean2, Nothing, Text} from '#common';
+import {Brand, is} from '#typing';
+
+export type CharacterTypeSemantic = TypeSemantic &
+  Brand<'Analyzer.CharacterTypeSemantic'> & {
+    value: Text;
+    declaration?: NominalTypeDeclarationSemantic | Nothing;
+  };
+
+export const $CharacterTypeSemantic = () =>
+  $AnalyzerType<CharacterTypeSemantic>('CharacterTypeSemantic', $TypeSemantic());
+
+export function newCharacterTypeSemantic(
+  value: Text,
+  declaration?: NominalTypeDeclarationSemantic | Nothing,
+): CharacterTypeSemantic {
+  return {
+    $: $CharacterTypeSemantic(),
+    declaration,
+    value,
+
+    is(other: TypeSemantic): Boolean2 {
+      if (is(other, $SetTypeSemantic())) {
+        return isInSet(this, other);
+      }
+
+      if (this.equals(other)) {
+        return true;
+      }
+
+      if (is(other, $IdTypeSemantic()) && other.declaration) {
+        // return this.declaration.equals(other.declaration) || (this.declaration.type?.is(other) ?? false);
+      }
+
+      return false;
+    },
+
+    equals(other: TypeSemantic): Boolean2 {
+      if (is(other, $CharacterTypeSemantic())) {
+        return this.value === other.value;
+      }
+
+      return false;
+    },
+  };
+}
