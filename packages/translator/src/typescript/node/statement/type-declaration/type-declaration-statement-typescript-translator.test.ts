c932518833264805d1c1576950ebffd6380e189b
nizami <nizami.dev@gmail.com>
2025-03-13 22:56:20 +0300

    Update type declaration translator tests

--- packages/translator/src/typescript/node/statement/type-declaration/type-declaration-statement-typescript-translator.test.ts
+++ packages/translator/src/typescript/node/statement/type-declaration/type-declaration-statement-typescript-translator.test.ts
@@ -1,40 +1,30 @@
 import {
-  $ReturnStatementNode,
   newAnalyzerContext,
   newCharacterStreamFromText,
-  nonHiddenNodeGenerator,
-  parseReturnStatementNode,
-  ReturnStatementNode,
+  NominalTypeDeclarationNode,
+  parseStatements,
 } from '#analyzer';
-import {newArrayData, newText, Text} from '#common';
-import {translateTypescriptReturnStatement} from '#translator';
-import {is} from '#typing';
+import {newText, Text} from '#common';
+import {translateTypescriptTypeDeclarationStatement} from '#translator';
 import {expect, test} from 'vitest';
 
-test('Return statement with expression', () => {
-  const text = newText('return 7 17 37');
-  const node = getReturnStatementNode(text);
-  const translated = translateTypescriptReturnStatement(node);
+test('Nominal type statement', () => {
+  const text = newText('type A');
+  const node = getNominalTypeDeclarationNode(text);
+  const translated = translateTypescriptTypeDeclarationStatement(node);
 
-  expect(translated.toNativeString()).toBe('return 7;');
+  expect(translated.toNativeString()).toBe('type A = {}');
 });
 
-test('Return statement without expression', () => {
-  const text = newText('return');
-  const node = getReturnStatementNode(text);
-  const translated = translateTypescriptReturnStatement(node);
-
-  expect(translated.toNativeString()).toBe('return;');
-});
-
-function getReturnStatementNode(text: Text): ReturnStatementNode {
+function getNominalTypeDeclarationNode(text: Text): NominalTypeDeclarationNode {
   const source = newCharacterStreamFromText(text);
   const context = newAnalyzerContext(source);
-  const nodes = newArrayData(nonHiddenNodeGenerator(context));
-  const node = parseReturnStatementNode(0, nodes) as ReturnStatementNode;
+  const {statements} = parseStatements(context);
+  const node = statements.first() as NominalTypeDeclarationNode;
+  // const nodes = newArrayData(nonHiddenNodeGenerator(context));
+  // const node = parseTypeDeclarationNode(0, nodes) as NominalTypeDeclarationNode;
 
   expect(node).toBeTruthy();
-  expect(is(node, $ReturnStatementNode())).toBe(true);
 
   return node;
 }
