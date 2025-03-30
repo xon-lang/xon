9cadb2c933352bd28eb428b8b2eb261333f42c9e
nizami <nizami.dev@gmail.com>
2025-03-30 18:04:03 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
-import {join} from 'node:path';
+import {resolve} from 'node:path';
 import {defineConfig} from 'vitest/config';
 
-const PROJECT_ROOT = __dirname;
+const WORKSPACE_ROOT = __dirname;
 
 export default defineConfig({
-  root: PROJECT_ROOT,
-  envDir: PROJECT_ROOT,
+  root: WORKSPACE_ROOT,
+  envDir: WORKSPACE_ROOT,
   resolve: {
-    alias: {
-      '#typing': join(PROJECT_ROOT, 'packages/typing/index.ts'),
-      '#common': join(PROJECT_ROOT, 'packages/common/index.ts'),
-      '#diagnostic': join(PROJECT_ROOT, 'packages/diagnostic/index.ts'),
-      '#analyzer': join(PROJECT_ROOT, 'packages/analyzer/index.ts'),
-      '#interpreter': join(PROJECT_ROOT, 'packages/interpreter/index.ts'),
-      '#translator': join(PROJECT_ROOT, 'packages/translator/index.ts'),
-      '#vscode': join(PROJECT_ROOT, 'packages/vscode/index.ts'),
-      '#xon-lib': join(PROJECT_ROOT, 'packages/xon-lib/index.ts'),
-    },
+    alias: [{find: /#(.+)/g, replacement: resolve(WORKSPACE_ROOT, 'packages/$1/index.ts')}],
   },
   test: {
-    // ...
+    environment: 'node',
+    alias: {
+      vscode: resolve(WORKSPACE_ROOT, 'packages/vscode/vscode.mock.ts'),
+    },
   },
 });
