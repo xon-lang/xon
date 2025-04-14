f1f4129368ac31bcdcb8f515ecca156de3968ae1
nizami <nizami.dev@gmail.com>
2025-04-14 19:54:15 +0300

    Add json file import provider

---
+++ packages/analyzer/src/import-provider/file/json/json-file-import-provider.ts
@@ -0,0 +1,99 @@
+import {
+  $AnalyzerType,
+  $ImportProvider,
+  DeclarationScope,
+  FileImportProvider,
+  newAttributeDeclarationSemantic,
+  newDeclarationScope,
+  newStringTypeSemantic,
+} from '#analyzer';
+import {
+  Boolean2,
+  newText,
+  newTextPosition,
+  newTextRange,
+  newTextReference,
+  nothing,
+  TextPosition,
+  TextRange,
+  Uri,
+} from '#common';
+import {JsonGrammarLexer, JsonGrammarParser} from '#grammar';
+import {Brand} from '#typing';
+import {CharStream, CommonTokenStream, ParserRuleContext, Token} from 'antlr4';
+import {readFileSync} from 'node:fs';
+
+export type JsonFileImportProvider = FileImportProvider & Brand<'Analyzer.JsonFileImportProvider'>;
+
+export const $JsonFileImportProvider = () =>
+  $AnalyzerType<JsonFileImportProvider>('JsonFileImportProvider', $ImportProvider());
+
+export function newJsonFileImportProvider(): JsonFileImportProvider {
+  return {
+    $: $JsonFileImportProvider(),
+
+    canProvide(uri: Uri): Boolean2 {
+      return uri.value.lowerCase().endsWith(newText('.json'));
+    },
+
+    provideDeclarationScope(uri: Uri): DeclarationScope {
+      return getDeclarationsFromUri(uri);
+    },
+  };
+}
+
+function getDeclarationsFromUri(uri: Uri): DeclarationScope {
+  // todo use async version of 'readFileSync'
+  const buffer = readFileSync(uri.value.toNativeString());
+  const object = JSON.parse(buffer.toString());
+  const declarationScope = newDeclarationScope();
+
+  return jsonToObjectTypeSemantic(uri);
+}
+
+function jsonToObjectTypeSemantic(uri: Uri): DeclarationScope {
+  const buffer = readFileSync(uri.value.toNativeString());
+  const input = buffer.toString();
+  const chars = new CharStream(input); // replace this with a FileStream as required
+  const lexer = new JsonGrammarLexer(chars);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new JsonGrammarParser(tokens);
+  const tree = parser.obj();
+
+  const scope = newDeclarationScope();
+
+  for (const pair of tree.pair_list()) {
+    const key = pair.key();
+    const value = pair.value();
+
+    // todo should we use non string keys ??? (json5 ???)
+    if (!key.getText().startsWith('"')) {
+      continue;
+    }
+
+    const range = antrlRangeToXonRange(key);
+    const reference = newTextReference(uri, range);
+
+    const declaration = newAttributeDeclarationSemantic(
+      reference,
+      nothing,
+      newText(key.getText().replace(/^\"/, '').replace(/\"$/, '')),
+      newStringTypeSemantic(newText(value.getText())),
+    );
+
+    scope.add(declaration);
+  }
+
+  return scope;
+}
+
+function antrlPositionToXonPosition(token: Token): TextPosition {
+  return newTextPosition(token.start, token.line - 1, token.column);
+}
+
+function antrlRangeToXonRange(ruleContext: ParserRuleContext): TextRange {
+  const start = antrlPositionToXonPosition(ruleContext.start);
+  const stop = ruleContext.stop ? antrlPositionToXonPosition(ruleContext.stop) : start.clone();
+
+  return newTextRange(start, stop);
+}
--- packages/analyzer/src/import-provider/get-declaration-scope-from-uri.ts
+++ packages/analyzer/src/import-provider/get-declaration-scope-from-uri.ts
@@ -1,7 +1,9 @@
-import {DeclarationScope, newFileImportProvider} from '#analyzer';
+import {DeclarationScope, newJsonFileImportProvider, newXonFileImportProvider} from '#analyzer';
 import {memoize, newArrayData, Nothing, Uri} from '#common';
 
-const declarationScopeProviders = memoize(() => newArrayData([newFileImportProvider()]));
+const declarationScopeProviders = memoize(() =>
+  newArrayData([newXonFileImportProvider(), newJsonFileImportProvider()]),
+);
 
 export function getDeclarationScopeFromUri(uri: Uri): DeclarationScope | Nothing {
   const provider = declarationScopeProviders().first((x) => x.canProvide(uri));
