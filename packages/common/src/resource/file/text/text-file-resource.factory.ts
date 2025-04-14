93a141bffff5fe8527313e189fe11b075a1ba745
nizami <nizami.dev@gmail.com>
2025-04-14 19:57:00 +0300

    Refactoring

--- packages/common/src/resource/file/text/text-file-resource.factory.ts
+++ packages/common/src/resource/file/text/text-file-resource.factory.ts
@@ -1,7 +1,11 @@
-import {$TextFileResource, TextEncodingType, TextFileResource, Uri} from '#common';
+import {$TextFileResource, newFileResource, TextEncodingType, TextFileResource, Uri} from '#common';
 
 export function newTextFileResource(uri: Uri): TextFileResource {
+  const file = newFileResource(uri);
+
   return {
+    ...file,
+
     $: $TextFileResource(),
     uri,
     encoding: TextEncodingType.UTF8,
