1683065ae6dab7dbdcbf120143f87067dead1ec2
nizami <nizami.dev@gmail.com>
2025-06-07 15:22:15 +0300

    Refactor common models

--- packages/common/src/key-value/key-value.model.ts
+++ packages/common/src/key-value/key-value.model.ts
@@ -1,13 +1,14 @@
 import {$CommonType, Anything, Boolean2} from '#common';
-import {$Model, Model} from '#typing';
+import {$Model, Brand, Model} from '#typing';
 
-export type KeyValue<K = Anything, V = Anything> = Model & {
-  key: K;
-  value: V;
+export type KeyValue<K = Anything, V = Anything> = Model &
+  Brand<'Common.KeyValue'> & {
+    key: K;
+    value: V;
 
-  equals(other: KeyValue<K, V>): Boolean2;
-  equals(other: KeyValue<K, V>, onlyKeyCompare: Boolean2): Boolean2;
-};
+    equals(other: KeyValue<K, V>): Boolean2;
+    equals(other: KeyValue<K, V>, onlyKeyCompare: Boolean2): Boolean2;
+  };
 
 export const $KeyValue = <T extends Model, V extends Model>($T = $Model(), $V = $Model()) =>
   $CommonType<KeyValue<T, V>>('KeyValue', undefined, [$T, $V]);
--- packages/typing/src/equals.ts
+++ packages/typing/src/equals.ts
@@ -1,9 +1,13 @@
 import {$Model, is} from '#typing';
 
 export function modelEquals(a: any, b: any): boolean {
+  if(a === b){
+    return true
+  }
+
   if (is(a, $Model()) && is(b, $Model()) && a.equals) {
     return a.equals(b);
   }
 
-  return a === b;
+  return false;
 }
